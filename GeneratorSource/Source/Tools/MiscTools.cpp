5f599438e4f39f3e380e9520ad67a93326226d1a
Mysticial <a-yee@u.northwestern.edu>
2021-03-06 23:20:00 -0600

    HexGenerator for the native programs.

---
+++ GeneratorSource/Source/Tools/MiscTools.cpp
@@ -0,0 +1,33 @@
+/*  Json Helpers
+ *
+ *  From: https://github.com/Mysticial/Pokemon-Automation-SwSh-Arduino-Scripts
+ *
+ */
+
+#include <QString>
+#include "Common/Qt/StringException.h"
+#include "Common/Qt/QtJsonTools.h"
+#include "Tools.h"
+
+namespace PokemonAutomation{
+
+const QString STRING_POKEMON = QString("Pok") + QChar(0xe9) + "mon";
+
+
+
+
+//QJsonObject json_get_object(const QJsonObject& obj, const QString& key);
+
+
+
+//#include <iostream>
+//using namespace std;
+
+
+
+
+}
+
+
+
+
---
+++ GeneratorSource/Source/UI/BoardList.h
@@ -0,0 +1,24 @@
+/*  Board List
+ *
+ *  From: https://github.com/Mysticial/Pokemon-Automation-SwSh-Arduino-Scripts
+ *
+ */
+
+#ifndef PokemonAutomation_BoardList_H
+#define PokemonAutomation_BoardList_H
+
+#include <QComboBox>
+
+namespace PokemonAutomation{
+
+
+class BoardList : public QComboBox{
+public:
+    BoardList(QWidget& parent);
+    const std::string& get_board() const;
+private:
+};
+
+
+}
+#endif
