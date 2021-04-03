9d1243dfcab4592e31502ecfb6c61ccaba38f1d1
Mysticial <a-yee@u.northwestern.edu>
2021-04-03 15:43:36 -0500

    Fix Clang take 2.

--- Common/Qt/Options/SimpleIntegerOption.cpp
+++ Common/Qt/Options/SimpleIntegerOption.cpp
@@ -14,11 +14,15 @@ namespace PokemonAutomation{
 
 template class SimpleIntegerOption<uint8_t>;
 template class SimpleIntegerOption<uint16_t>;
-template class SimpleIntegerOption<uint32_t>;
 
 template class SimpleIntegerOptionUI<uint8_t>;
 template class SimpleIntegerOptionUI<uint16_t>;
+
+//  COMPILER-BUG-CLANG: unsigned in vs. uint32_t linking
+#ifndef __clang__
+template class SimpleIntegerOption<uint32_t>;
 template class SimpleIntegerOptionUI<uint32_t>;
+#endif
 
 
 template <typename Type>
