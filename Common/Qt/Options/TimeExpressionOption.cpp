4ec4e734c85df1dd6cca13ba641be59aa52f5886
Mysticial <a-yee@u.northwestern.edu>
2021-04-03 16:12:37 -0500

    Fix Clang take 4.

--- Common/Qt/Options/TimeExpressionOption.cpp
+++ Common/Qt/Options/TimeExpressionOption.cpp
@@ -14,11 +14,6 @@
 namespace PokemonAutomation{
 
 
-template class TimeExpressionOption<uint16_t>;
-template class TimeExpressionOption<uint32_t>;
-
-template class TimeExpressionOptionUI<uint16_t>;
-template class TimeExpressionOptionUI<uint32_t>;
 
 
 template <typename Type>
@@ -166,5 +161,15 @@ void TimeExpressionOptionUI<Type>::restore_defaults(){
     m_box->setText(m_value.m_current);
 }
 
+
+
+
+
+
+template class TimeExpressionOption<uint16_t>;
+template class TimeExpressionOption<uint32_t>;
+
+template class TimeExpressionOptionUI<uint16_t>;
+template class TimeExpressionOptionUI<uint32_t>;
 
 }
