94a2e2a2de5e6e507033a2df53bc2d50791caf67
Alexander Yee <a-yee@u.northwestern.edu>
2022-03-26 13:10:51 -0500

    Update Globals.cpp

--- SerialPrograms/Source/CommonFramework/Globals.cpp
+++ SerialPrograms/Source/CommonFramework/Globals.cpp
@@ -10,7 +10,7 @@
 
 namespace PokemonAutomation{
 
-const QString PROGRAM_VERSION_BASE = "v0.12.6";
+const QString PROGRAM_VERSION_BASE = "v0.12.7";
 
 #ifdef PA_OFFICIAL
 const QString PROGRAM_VERSION = PROGRAM_VERSION_BASE;
