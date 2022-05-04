b550341a1e7d117ce7a69bcc8426546ef89f446c
Gin <>
2022-05-03 20:42:54 -0700

    Fix Ingo grinder A button detecting too early

--- SerialPrograms/Source/PokemonLA/Programs/Farming/PokemonLA_IngoBattleGrinder.cpp
+++ SerialPrograms/Source/PokemonLA/Programs/Farming/PokemonLA_IngoBattleGrinder.cpp
@@ -137,6 +137,10 @@ std::unique_ptr<StatsTracker> IngoBattleGrinder::make_stats() const{
 }
 
 bool IngoBattleGrinder::start_dialog(ConsoleHandle& console, BotBaseContext& context){
+    // First press A to start talking with Ingo.
+    pbf_press_button(context, BUTTON_A, 20, 150);
+    context.wait_for_all_requests();
+
     {
         ButtonDetector button0(console, console, ButtonType::ButtonA, {0.50, 0.408, 0.40, 0.042}, std::chrono::milliseconds(100), true);
         ButtonDetector button1(console, console, ButtonType::ButtonA, {0.50, 0.450, 0.40, 0.042}, std::chrono::milliseconds(100), true);
