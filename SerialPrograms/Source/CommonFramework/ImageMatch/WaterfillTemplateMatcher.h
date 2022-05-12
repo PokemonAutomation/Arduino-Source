fb98a3804bd11c524bccbfd0ed2c3e217fb3e494
Gin <>
2022-05-11 20:41:00 -0700

    More comments

--- SerialPrograms/Source/CommonFramework/ImageMatch/WaterfillTemplateMatcher.h
+++ SerialPrograms/Source/CommonFramework/ImageMatch/WaterfillTemplateMatcher.h
@@ -27,19 +27,36 @@ public:
 
 
 public:
-    // Load a template image from disk, min_color and max_color denote the
-    // color range of the object displayed in the image file while min_area
-    // is the minimum number of pixels required for an object.
+    // Load a template image from disk, min_color and max_color denote the color range of the object
+    // displayed in the image file while min_area is the minimum number of pixels required for the
+    // object in the template.
+    // Throw FileException when there is no object meeting the requirement in the template.
+    // If there are more than one objects meeting the requirement in the template, pick the one
+    // with the largest area (largest number of pixels).
     WaterfillTemplateMatcher(
         const char* path,
         Color min_color, Color max_color,
         size_t min_area
     );
 
-    //  Compute RMSD of current object against the template as-is.
-    double rmsd(const ConstImageRef& object) const;
+    //  Compute RMSD of the current image against the template as-is.
+    //  In case the image is invalid, return a large value.
+    //  It also calls the virtual function `check_image()` on the image.
+    //  If it returns false, then return a large value. 
+    double rmsd(const ConstImageRef& image) const;
 
+    //  Compute RMSD of the object on the image against the template.
+    //  The cropped image is compared against the template as-is.
+    //  The waterfill object's aspect ratio and area ratio are checked against template's. Return a large value 
+    //  if the check fails.
+    //  See `double rmsd(const ConstImageRef& image) const` on the details of comparing the image against the template.
     virtual double rmsd_precropped(const ConstImageRef& cropped_image, const WaterfillObject& object) const;
+    //  Compute RMSD of the object on the image against the template.
+    //  It will crop the original image using the bounding box of the waterfill object, then compare the cropped
+    //  image against the template as-is.
+    //  The waterfill object's aspect ratio and area ratio are checked against template's. Return a large value 
+    //  if the check fails.
+    //  See `double rmsd(const ConstImageRef& image) const` on the details of comparing the image against the template.
     virtual double rmsd_original(const ConstImageRef& original_image, const WaterfillObject& object) const;
 
 protected:
