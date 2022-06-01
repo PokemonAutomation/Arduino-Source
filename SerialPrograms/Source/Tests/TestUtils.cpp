b849b899dd50a132a294ed8fe7ccbf82e6709983
Gin <>
2022-06-01 04:07:17 -0700

    Add missing file

---
+++ SerialPrograms/Source/Tests/TestUtils.cpp
@@ -0,0 +1,93 @@
+/*  Test Utils
+ *
+ *  From: https://github.com/PokemonAutomation/Arduino-Source
+ *  
+ *  
+ */
+
+#include "TestUtils.h"
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+using std::cout;
+using std::cerr;
+using std::endl;
+
+namespace PokemonAutomation{
+
+
+std::vector<std::string> parse_words(const std::string& filename_base){
+    std::vector<std::string> words;
+    std::istringstream is(filename_base);
+    std::string keyword;
+    while (getline(is, keyword, '-')){
+        words.push_back(keyword);
+    }
+
+    return words;
+}
+
+
+bool parse_int(const std::string& word, int& number){
+    std::istringstream iss(word);
+    iss >> number;
+    return iss.eof() && !iss.fail();
+}
+
+bool parse_float(const std::string& word, float& number){
+    std::istringstream iss(word);
+    iss >> number;
+    return iss.eof() && !iss.fail();
+}
+
+bool load_sprite_count(const std::string& txt_path, std::map<std::string, int>& sprites){
+    std::ifstream fin(txt_path.c_str());
+    std::string line;
+    while(std::getline(fin, line)){
+        std::istringstream is(line);
+        std::string word;
+        is >> word;
+        if (is.fail()){
+            continue; // empty line, skip
+        }
+
+        int count = 0;
+        is >> count;
+        if (is.fail()){
+            count = 1;
+        } else {
+            if (count <= 0){
+                cerr << "Error: in file " << txt_path << " sprite count must be larger than zero, but got " << count << endl;
+                return false;
+            }
+        }
+        sprites.emplace(std::move(word), count);
+    }
+
+    return true;
+}
+
+bool load_slug_list(const std::string& filepath, std::vector<std::string>& sprites){
+    std::ifstream fin(filepath.c_str());
+    std::string line;
+    if (!fin){
+        return false;
+    }
+
+    while(std::getline(fin, line)){
+        std::istringstream is(line);
+        std::string word;
+        is >> word;
+        if (is.fail()){
+            continue; // empty line, skip
+        }
+
+        sprites.emplace_back(std::move(word));
+    }
+
+    return true;
+}
+
+
+}
