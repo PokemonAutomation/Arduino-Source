b5286bf26f5cf928653636f8091e1427d34f69b4
Gin <>
2022-07-30 19:47:12 -0700

    Fix star symbol check

--- SerialPrograms/Source/PokemonLA/Inference/Map/PokemonLA_MMOSpriteStarSymbolDetector.cpp
+++ SerialPrograms/Source/PokemonLA/Inference/Map/PokemonLA_MMOSpriteStarSymbolDetector.cpp
@@ -74,7 +74,7 @@ bool MMOSpriteStarSymbolDetector::process_frame(const ImageViewRGB32& frame, Wal
             continue;
         }
 
-        if (m_symbol_colors[i].r <= m_symbol_colors[i].g * 1.2){
+        if (m_symbol_colors[i].r <= m_symbol_colors[i].g * 1.2 && m_symbol_colors[i].g > 200 && m_symbol_colors[i].r > 220) {
             // It's not very red, so it's not a berry symbol
             m_is_star[i] = true;
         }
