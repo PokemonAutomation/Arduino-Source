abdc6e1be78e7636d4673f79ec4d53246056e893
Gin <>
2022-07-31 10:42:45 -0700

    Remove unused headers

--- SerialPrograms/Source/PokemonLA/Programs/General/PokemonLA_ApplyGrits.cpp
+++ SerialPrograms/Source/PokemonLA/Programs/General/PokemonLA_ApplyGrits.cpp
@@ -6,12 +6,8 @@
 
 #include "Common/Cpp/Exceptions.h"
 #include "CommonFramework/Notifications/ProgramNotifications.h"
-#include "CommonFramework/VideoPipeline/VideoFeed.h"
-#include "CommonFramework/InferenceInfra/InferenceRoutines.h"
 #include "NintendoSwitch/Commands/NintendoSwitch_Commands_PushButtons.h"
 #include "Pokemon/Pokemon_Strings.h"
-#include "PokemonLA/Inference/Objects/PokemonLA_ArcPhoneDetector.h"
-#include "PokemonLA/Inference/PokemonLA_ItemCompatibilityDetector.h"
 #include "PokemonLA_ApplyGrits.h"
 
 
