9688f586b7c332f413f180a904e171ac3edb0842
fyex <33452346+fyex@users.noreply.github.com>
2022-08-18 20:07:01 +0200

    Add a SwSh BerryFarmer which uses sound.

---
+++ SerialPrograms/Source/PokemonSwSh/Programs/DateSpamFarmers/PokemonSwSh_DateSpam-BerryFarmer2.h
@@ -0,0 +1,68 @@
+/*  Berry Farmer
+ *
+ *  From: https://github.com/PokemonAutomation/Arduino-Source
+ *
+ */
+
+#ifndef PokemonAutomation_PokemonSwSh_BerryFarmer2_H
+#define PokemonAutomation_PokemonSwSh_BerryFarmer2_H
+
+#include "Common/Cpp/Options/SimpleIntegerOption.h"
+#include "Common/Cpp/Options/StaticTextOption.h"
+#include "NintendoSwitch/Options/NintendoSwitch_StartInGripMenuOption.h"
+#include "Common/Cpp/Options/TimeExpressionOption.h"
+#include "NintendoSwitch/NintendoSwitch_SingleSwitchProgram.h"
+
+namespace PokemonAutomation{
+namespace NintendoSwitch{
+namespace PokemonSwSh{
+
+
+class BerryFarmer2_Descriptor : public SingleSwitchProgramDescriptor {
+public:
+    BerryFarmer2_Descriptor();
+};
+
+
+
+class BerryFarmer2 : public SingleSwitchProgramInstance{
+public:
+    BerryFarmer2();
+
+    virtual void program(SingleSwitchProgramEnvironment& env, BotBaseContext& context) override;
+
+    enum Rustling
+    {
+        No,
+        Slow,
+        Fast,
+        Battle
+    };
+
+private:
+    Rustling check_rustling(SingleSwitchProgramEnvironment& env, BotBaseContext& context);
+    uint32_t do_secondary_attempts(SingleSwitchProgramEnvironment& env, BotBaseContext& context, Rustling last_rustling);
+
+private:
+    StaticTextOption REQUIRES_AUDIO;
+    StartInGripOrGameOption START_IN_GRIP_MENU;
+
+    SimpleIntegerOption<uint32_t> FETCH_ATTEMPTS;
+    SimpleIntegerOption<uint16_t> SAVE_ITERATIONS;
+
+    SectionDividerOption m_advanced_options;
+    TimeExpressionOption<uint16_t> EXIT_BATTLE_TIMEOUT;
+    TimeExpressionOption<uint16_t> START_BATTLE_DELAY;
+    SimpleIntegerOption<uint16_t> RUSTLING_INTERVAL;
+    TimeExpressionOption<uint16_t> RUSTLING_TIMEOUT;
+    TimeExpressionOption<uint16_t> SECONDARY_ATTEMPT_MASH_TIME;
+};
+
+
+}
+}
+}
+#endif
+
+
+
