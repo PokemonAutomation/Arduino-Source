68655f20331e9722492889b48712a1653463aba8
Alexander Yee <a-yee@u.northwestern.edu>
2022-08-18 19:19:56 -0500

    Update PokemonSwSh_BattleMenuDetector.cpp
    Fix SwSh den battle menu detector.

--- SerialPrograms/Source/PokemonSwSh/Inference/Battles/PokemonSwSh_BattleMenuDetector.cpp
+++ SerialPrograms/Source/PokemonSwSh/Inference/Battles/PokemonSwSh_BattleMenuDetector.cpp
@@ -39,7 +39,7 @@ StandardBattleMenuDetector::StandardBattleMenuDetector(
     , m_text_bag    (0.830, 0.576 + 2 * 0.1075, 0.08, 0.080)
     , m_text_run    (0.830, 0.576 + 3 * 0.1075, 0.08, 0.080)
 //    , m_status0     (0.280, 0.870, 0.015, 0.030)
-    , m_status1     (0.165, 0.945, 0.100, 0.020)
+    , m_status1     (0.165, 0.945, 0.100, 0.015)
 {}
 void StandardBattleMenuDetector::make_overlays(VideoOverlaySet& items) const{
     if (!m_den){
