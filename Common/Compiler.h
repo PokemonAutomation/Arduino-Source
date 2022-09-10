3976aaa56d62fbef3872963d99d1328d0a24686d
pifopi <dottel.gael@gmail.com>
2022-09-10 18:10:33 +0200

    move warnings to cmake

--- Common/Compiler.h
+++ Common/Compiler.h
@@ -27,14 +27,6 @@ template <typename type> using r_ref  = type &__restrict;
 template <typename type> using c_ref  = type const&__restrict;
 template <typename type> using r_rref = type &&__restrict;
 
-//using ssize_t = ptrdiff_t;
-
-#pragma warning(disable:4100)   //  Unreferenced Formal Parameter
-#pragma warning(disable:4127)   //  Conditional expresstion is constant
-#pragma warning(disable:4324)   //  structure was padded due to alignment specifier
-#pragma warning(disable:4458)   //  Hiding of class members
-#pragma warning(disable:4996)   //  Unsafe function
-
 #define PA_CURRENT_FUNCTION __FUNCSIG__
 
 
@@ -47,11 +39,6 @@ template <typename type> using c_ptr  = type const*__restrict__;
 template <typename type> using r_ref  = type &__restrict__;
 template <typename type> using c_ref  = type const&__restrict__;
 template <typename type> using r_rref = type &&__restrict__;
-
-#pragma GCC diagnostic ignored "-Wunused-parameter"
-#pragma GCC diagnostic ignored "-Wunused-function"
-#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
-#pragma GCC diagnostic ignored "-Woverloaded-virtual"
 
 #define PA_CURRENT_FUNCTION __PRETTY_FUNCTION__
 
