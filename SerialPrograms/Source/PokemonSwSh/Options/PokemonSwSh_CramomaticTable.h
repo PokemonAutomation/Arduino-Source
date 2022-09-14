f3c1a21c49370f79df7ab5a973d03f058f624319
fyex <33452346+fyex@users.noreply.github.com>
2022-09-15 00:34:24 +0200

    more user-friendly options for Cram-o-matic RNG

---
+++ SerialPrograms/Source/PokemonSwSh/Options/PokemonSwSh_CramomaticTable.h
@@ -0,0 +1,67 @@
+/*  Cram-o-matic Table
+ *
+ *  From: https://github.com/PokemonAutomation/Arduino-Source
+ *
+ */
+
+#ifndef PokemonAutomation_PokemonSwSh_CramomaticTable_H
+#define PokemonAutomation_PokemonSwSh_CramomaticTable_H
+
+#include "Common/Cpp/Options/EditableTableOption.h"
+#include "CommonFramework/Options/StringSelectOption.h"
+#include "Common/Cpp/Options/BooleanCheckBoxOption.h"
+#include "Common/Cpp/Options/EnumDropdownOption.h"
+#include "PokemonSwSh_CramomaticTable.h"
+
+namespace PokemonAutomation{
+namespace NintendoSwitch{
+namespace PokemonSwSh{
+
+enum class CramomaticBallType
+{
+    Poke,
+    Great,
+    Shop1,
+    Shop2,
+    Apricorn,
+    Safari,
+    Sport
+};
+
+struct CramomaticSelection
+{
+    CramomaticBallType ball_type;
+    bool is_bonus;
+};
+
+class CramomaticRow : public EditableTableRow{
+public:
+    CramomaticRow();
+    virtual std::unique_ptr<EditableTableRow> clone() const;
+
+public:
+    EnumDropdownCell<CramomaticBallType> ball_type;
+    BooleanCheckBoxCell is_bonus;
+};
+
+
+
+class CramomaticTable : public EditableTableOption_t<CramomaticRow>{
+public:
+    CramomaticTable(std::string label);
+
+    std::vector<CramomaticSelection> selected_balls() const;
+
+    virtual std::vector<std::string> make_header() const;
+
+    static std::vector<std::unique_ptr<EditableTableRow>> make_defaults();
+};
+
+
+
+
+
+}
+}
+}
+#endif
--- SerialPrograms/Source/PokemonSwSh/Programs/RNG/PokemonSwSh_CramomaticRNG.h
+++ SerialPrograms/Source/PokemonSwSh/Programs/RNG/PokemonSwSh_CramomaticRNG.h
@@ -12,6 +12,7 @@
 #include "Common/Cpp/Options/StaticTextOption.h"
 #include "NintendoSwitch/Options/NintendoSwitch_StartInGripMenuOption.h"
 #include "NintendoSwitch/NintendoSwitch_SingleSwitchProgram.h"
+#include "PokemonSwSh/Options/PokemonSwSh_CramomaticTable.h"
 #include "PokemonSwSh/Programs/RNG/PokemonSwSh_Xoroshiro128Plus.h"
 
 namespace PokemonAutomation {
@@ -23,7 +24,11 @@ public:
     CramomaticRNG_Descriptor();
 };
 
-
+struct CramomaticTarget {
+    CramomaticBallType ball_type;
+    bool is_bonus;
+    size_t needed_advances;
+};
 
 class CramomaticRNG : public SingleSwitchProgramInstance {
 public:
@@ -32,31 +37,21 @@ public:
     virtual void program(SingleSwitchProgramEnvironment& env, BotBaseContext& context) override;
 
 private:
-    enum BallType
-    {
-        Poke,
-        Great,
-        Shop1,
-        Shop2,
-        Apricorn,
-        Safari,
-        Sport
-    };
 
     StartInGripOrGameOption START_LOCATION;
-    SimpleIntegerOption<uint32_t> NUM_ITERATIONS;
+    SimpleIntegerOption<uint32_t> NUM_APRICORN_ONE;
+    SimpleIntegerOption<uint32_t> NUM_APRICORN_TWO;
     SimpleIntegerOption<uint32_t> NUM_NPCS;
-    EnumDropdownOption<BallType> BALL_TYPE;
+    CramomaticTable BALL_TABLE;
 
 
     SectionDividerOption m_advanced_options;
     SimpleIntegerOption<uint16_t> MAX_UNKNOWN_ADVANCES;
-    BooleanCheckBoxOption ONLY_BONUS;
     BooleanCheckBoxOption SAVE_SCREENSHOTS;
     BooleanCheckBoxOption LOG_VALUES;
 
     void navigate_to_party(SingleSwitchProgramEnvironment& env, BotBaseContext& context);
-    size_t needed_advances(SingleSwitchProgramEnvironment& env, Xoroshiro128PlusState state, BallType wanted_type);
+    CramomaticTarget needed_advances(SingleSwitchProgramEnvironment& env, Xoroshiro128PlusState state, std::vector<CramomaticSelection> wanted_balls);
     void leave_to_overworld_and_interact(SingleSwitchProgramEnvironment& env, BotBaseContext& context);
     void choose_apricorn(SingleSwitchProgramEnvironment& env, BotBaseContext& context, bool sport);
     bool receive_ball(SingleSwitchProgramEnvironment& env, BotBaseContext& context);
