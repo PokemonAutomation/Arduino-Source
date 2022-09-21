8ac9f4ad77ea7f311a1442ec842536f94b95dbcc
pifopi <dottel.gael@gmail.com>
2022-09-21 13:52:26 +0200

    add reading capabilities to home sorter (shiny, gmax, ball)

---
+++ SerialPrograms/Source/PokemonHome/Inference/PokemonHome_BallReader.h
@@ -0,0 +1,42 @@
+/*  Ball Reader
+ *
+ *  From: https://github.com/PokemonAutomation/Arduino-Source
+ *
+ */
+
+#ifndef PokemonAutomation_PokemonHome_BallReader_H
+#define PokemonAutomation_PokemonHome_BallReader_H
+
+#include <string>
+#include "CommonFramework/VideoPipeline/VideoOverlay.h"
+#include "CommonFramework/Tools/ConsoleHandle.h"
+#include "CommonFramework/ImageMatch/CroppedImageDictionaryMatcher.h"
+
+namespace PokemonAutomation{
+namespace NintendoSwitch{
+namespace PokemonHome{
+
+
+class BallReader{
+    static const double MAX_ALPHA;
+    static const double ALPHA_SPREAD;
+
+public:
+    BallReader(ConsoleHandle& console);
+
+public:
+    std::string read_ball(const ImageViewRGB32& screen) const;
+
+private:
+    const ImageMatch::CroppedImageDictionaryMatcher& m_matcher;
+    ConsoleHandle& m_console;
+    InferenceBoxScope m_box_sprite;
+};
+
+
+
+
+}
+}
+}
+#endif
