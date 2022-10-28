8aab5d2f2f6316a93a6abb1a25e834702670a935
Mysticial <a-yee@u.northwestern.edu>
2022-10-28 00:51:53 -0700

    Unlock static text when running.

--- Common/Cpp/Options/StaticTextOption.cpp
+++ Common/Cpp/Options/StaticTextOption.cpp
@@ -27,7 +27,8 @@ struct StaticTextOption::Data{
 
 StaticTextOption::~StaticTextOption() = default;
 StaticTextOption::StaticTextOption(std::string label, bool text_wrapping)
-    : m_data(CONSTRUCT_TOKEN, std::move(label), text_wrapping)
+    : ConfigOption(LockWhileRunning::UNLOCKED)
+    , m_data(CONSTRUCT_TOKEN, std::move(label), text_wrapping)
 {}
 #if 0
 std::unique_ptr<ConfigOption> StaticTextOption::clone() const{
