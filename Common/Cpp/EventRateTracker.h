90de6263ab3e73b45c16f9457026903497861be0
Mysticial <a-yee@u.northwestern.edu>
2022-11-06 23:46:02 -0800

    Suppress spikes in utilization after a long idle period.

--- Common/Cpp/EventRateTracker.h
+++ Common/Cpp/EventRateTracker.h
@@ -55,14 +55,15 @@ private:
 
 class UtilizationTracker{
 public:
-    UtilizationTracker(std::chrono::milliseconds window = std::chrono::milliseconds(1000))
+    UtilizationTracker(WallClock::duration window = std::chrono::milliseconds(1000))
         : m_window(window)
+        , m_min_window(window / 2)
         , m_running_usage(0)
     {}
 
     void push_event(WallClock::duration usage_since_last, WallClock timestamp = current_time()){
         WallClock threshold = timestamp - m_window;
-        while (!m_history.empty()){
+        while (m_history.size() > 2){
             if (m_history.front().timestamp >= threshold){
                 break;
             }
@@ -84,6 +85,7 @@ public:
             return 0;
         }
         auto delta_time = m_history.back().timestamp - m_history.front().timestamp;
+        delta_time = std::max(delta_time, m_min_window);
         auto usage = m_running_usage - m_history.front().usage;
         std::chrono::microseconds time_usec = std::chrono::duration_cast<std::chrono::microseconds>(delta_time);
         std::chrono::microseconds usage_usec = std::chrono::duration_cast<std::chrono::microseconds>(usage);
@@ -96,7 +98,10 @@ private:
         WallClock timestamp;
         WallClock::duration usage;
     };
-    std::chrono::milliseconds m_window;
+
+    WallClock::duration m_window;
+    WallClock::duration m_min_window;
+
     std::deque<Entry> m_history;
     WallClock::duration m_running_usage;
 };
