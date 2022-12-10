49793d2a7ac18b05a43590443ec9b7966d0dfc24
Mysticial <a-yee@u.northwestern.edu>
2022-12-10 04:50:12 -0800

    Version #.

--- SerialPrograms/Source/CommonFramework/Globals.cpp
+++ SerialPrograms/Source/CommonFramework/Globals.cpp
@@ -10,7 +10,7 @@
 
 namespace PokemonAutomation{
 
-const std::string PROGRAM_VERSION_BASE = "v0.23.1";
+const std::string PROGRAM_VERSION_BASE = "v0.23.2";
 
 #ifdef PA_OFFICIAL
 const std::string PROGRAM_VERSION = PROGRAM_VERSION_BASE;
