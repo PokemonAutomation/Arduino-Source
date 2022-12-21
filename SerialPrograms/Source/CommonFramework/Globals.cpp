3dd9a69cde37e8a24a27c72461421bbdf7fc813d
Mysticial <a-yee@u.northwestern.edu>
2022-12-21 00:16:37 -0800

    Prepare v0.23 branch cut.

--- SerialPrograms/Source/CommonFramework/Globals.cpp
+++ SerialPrograms/Source/CommonFramework/Globals.cpp
@@ -10,7 +10,7 @@
 
 namespace PokemonAutomation{
 
-const std::string PROGRAM_VERSION_BASE = "v0.23.19h";
+const std::string PROGRAM_VERSION_BASE = "v0.23.21";
 
 #ifdef PA_OFFICIAL
 const std::string PROGRAM_VERSION = PROGRAM_VERSION_BASE;
