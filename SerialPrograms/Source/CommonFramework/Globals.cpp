25e7ce9577c08391deae8b37c487649559346ee1
Mysticial <a-yee@u.northwestern.edu>
2022-12-27 20:56:58 -0800

    Disable non-working egg stats for mass release.

--- SerialPrograms/Source/CommonFramework/Globals.cpp
+++ SerialPrograms/Source/CommonFramework/Globals.cpp
@@ -10,7 +10,7 @@
 
 namespace PokemonAutomation{
 
-const std::string PROGRAM_VERSION_BASE = "v0.24.6";
+const std::string PROGRAM_VERSION_BASE = "v0.25.1";
 
 #ifdef PA_OFFICIAL
 const std::string PROGRAM_VERSION = PROGRAM_VERSION_BASE;
