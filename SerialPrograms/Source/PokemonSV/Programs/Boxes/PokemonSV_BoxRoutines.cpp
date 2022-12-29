523c87faa7613508aa515bb10a947bfd3f814b21
Mysticial <a-yee@u.northwestern.edu>
2022-12-28 22:11:03 -0800

    Additional wait to help avoid A button drop when selecting entire party.

--- SerialPrograms/Source/PokemonSV/Programs/Boxes/PokemonSV_BoxRoutines.cpp
+++ SerialPrograms/Source/PokemonSV/Programs/Boxes/PokemonSV_BoxRoutines.cpp
@@ -4,7 +4,6 @@
  *
  */
 
-
 #include <cmath>
 #include <algorithm>
 #include <sstream>
@@ -34,7 +33,8 @@ void hold_one_column(BotBaseContext& context){
     for(int i = 0; i < 20; i++){
         pbf_press_dpad(context, DPAD_DOWN, 5, 3);
     }
-    // Hold rest of the pary
+    // Hold rest of the party
+    pbf_wait(context, 60);
     pbf_press_button(context, BUTTON_A, 30, 50);
 }
 
@@ -51,7 +51,6 @@ bool release_one_pokemon(
     const ProgramInfo& info, ConsoleHandle& console, BotBaseContext& context,
     size_t& errors
 ){
-#if 1
     errors = 0;
 
     bool release_attempted = false;
@@ -158,100 +157,6 @@ bool release_one_pokemon(
             );
         }
     }
-
-#else
-
-    context.wait_for_all_requests();
-    BoxSelectWatcher selected(COLOR_RED);
-    // If no pokemon in the slot:
-    if (!selected.exists(console.video().snapshot())){
-        console.log("Slot is empty.");
-        return false;
-    }
-    {
-        int ret = run_until(
-            console, context,
-            [](BotBaseContext& context){
-                pbf_press_button(context, BUTTON_A, 20, 230);
-            },
-            {selected}
-        );
-        if (ret < 0){
-            dump_image_and_throw_recoverable_exception(
-                info, console, "SelectionArrowNotFound", "Unable to detect selection arrow."
-            );
-        }
-    }
-    context.wait_for(std::chrono::milliseconds(100));
-
-    pbf_press_dpad(context, DPAD_UP, 20, 30);
-    pbf_press_dpad(context, DPAD_UP, 20, 30);
-    context.wait_for_all_requests();
-
-    console.log("Releasing...");
-    console.overlay().add_log("Release pokemon", COLOR_PURPLE);
-    {
-        PromptDialogWatcher confirm(COLOR_CYAN);
-        int ret = run_until(
-            console, context,
-            [](BotBaseContext& context){
-                for(int i = 0; i < 10; i++){
-                    pbf_press_button(context, BUTTON_A, 40, 230);
-                }
-            },
-            {confirm}
-        );
-        if (ret < 0){
-            dump_image_and_throw_recoverable_exception(
-                info, console, "ConfirmationNotFound", "Release confirmation not found."
-            );
-        }
-        context.wait_for(std::chrono::milliseconds(500));
-    }
-
-    pbf_press_dpad(context, DPAD_UP, 20, 30);
-    context.wait_for_all_requests();
-
-    console.log("Waiting for release to finish...");
-    if (ensure_empty) {
-        // Detect when there is no longer pokemon in the current slot
-        SomethingInBoxSlotWatcher done_releasing(COLOR_RED, false);
-        int ret = run_until(
-            console, context,
-            [](BotBaseContext& context){
-                pbf_mash_button(context, BUTTON_A, 5 * TICKS_PER_SECOND);
-            },
-            {done_releasing}
-        );
-        if (ret < 0){
-            dump_image_and_throw_recoverable_exception(
-                info, console, "ReleaseFailed", "Unable to release pokemon."
-            );
-        }
-        context.wait_for(std::chrono::milliseconds(100));
-    } else {
-        pbf_mash_button(context, BUTTON_A, 60);
-        AdvanceDialogWatcher advance_dialog(COLOR_RED);
-        int ret = wait_until(
-            console, context,
-            std::chrono::seconds(10),
-            {advance_dialog}
-        );
-        if (ret < 0){
-            dump_image_and_throw_recoverable_exception(
-                info, console, "AfterReleaseDialogNotFound", "Unable to find after-release advance dialog."
-            );
-        }
-        pbf_wait(context, 100);
-        pbf_mash_button(context, BUTTON_A, 60);
-        pbf_wait(context, 200);
-        context.wait_for_all_requests();
-        // console.overlay().add_log("Released", COLOR_WHITE);
-        // context.wait_for(std::chrono::seconds(30));
-    }
-    
-    return true;
-#endif
 }
 
 uint8_t check_empty_slots_in_party(const ProgramInfo& info, ConsoleHandle& console, BotBaseContext& context){
