02b1981f0d82d899600e64d34f42a7078a75605f
kichithewolf <kichithewolf@gmail.com>
2023-01-07 20:44:02 -0500

    fix explicit calls

--- SerialPrograms/Source/PokemonSV/Programs/General/PokemonSV_ESPTraining.cpp
+++ SerialPrograms/Source/PokemonSV/Programs/General/PokemonSV_ESPTraining.cpp
@@ -54,7 +54,7 @@ struct ESPTraining_Descriptor::Stats : public StatsTracker {
         m_display_order.emplace_back("Excitement");
         m_display_order.emplace_back("Anger");
         m_display_order.emplace_back("Times Cleared");
-        m_display_order.emplace_back(Stat("Errors"));
+        m_display_order.emplace_back("Errors", true);
     }
     std::atomic<uint64_t>& m_emotions;
     std::atomic<uint64_t>& m_joy;
