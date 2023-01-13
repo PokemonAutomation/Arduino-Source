e8edafb8508f4d9a841db22166fd9df7ab691c46
Mysticial <a-yee@u.northwestern.edu>
2023-01-12 20:30:30 -0800

    fix

--- SerialPrograms/Source/PokemonSV/Inference/PokemonSV_OverworldDetector.cpp
+++ SerialPrograms/Source/PokemonSV/Inference/PokemonSV_OverworldDetector.cpp
@@ -152,6 +152,7 @@ bool OverworldWatcher::process_frame(const VideoSnapshot& frame){
     ImageViewRGB32 start = extract_box_reference(m_start_of_detection, m_radar_inside);
     ImageViewRGB32 current = extract_box_reference(frame, m_radar_inside);
     if (start.width() != current.width() || start.height() != current.height()){
+        m_start_of_detection = frame;
         return false;
     }
 
@@ -159,6 +160,7 @@ bool OverworldWatcher::process_frame(const VideoSnapshot& frame){
     double rmsd = ImageMatch::pixel_RMSD(start, current);
 //    cout << "rmsd = " << rmsd << endl;
     if (rmsd > 2.0){
+        m_start_of_detection = frame;
         return false;
     }
 
