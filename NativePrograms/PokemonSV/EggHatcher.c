43fcd294709f872e3c35cd76024a22270f2a8be3
nymphaea-hydriades <87930564+nymphaea-hydriades@users.noreply.github.com>
2023-02-14 21:28:58 -0500

    Adding EggHatcher for SV

---
+++ NativePrograms/PokemonSV/EggHatcher.c
@@ -0,0 +1,7 @@
+#include "Common/NintendoSwitch/NintendoSwitch_ControllerDefs.h"
+#include "Programs/EggHatcher.h"
+
+const uint8_t BOXES_TO_HATCH    =   2;
+const uint16_t STEPS_TO_HATCH   =   5120;
+const bool HAS_CLONED_RIDER     =   false;
+const uint16_t SAFETY_COEFF     =   1.1;
---
+++ NativePrograms/PokemonSV/Programs/EggHatcher.h
@@ -0,0 +1,6 @@
+#include "Common/NintendoSwitch/NintendoSwitch_ControllerDefs.h"
+
+extern const uint8_t BOXES_TO_HATCH;
+extern const uint16_t STEPS_TO_HATCH;
+extern const bool HAS_CLONED_RIDER;
+extern const uint16_t SAFETY_COEFF;
