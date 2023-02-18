38c8e866e58efdb95954b7b1c009c0b6e035f716
Nymphea <87930564+nymphaea-hydriades@users.noreply.github.com>
2023-02-18 10:02:23 -0500

    Changing Fetcher start conditions

--- NativePrograms/PokemonSV/Programs/EggFetcher_Core.c
+++ NativePrograms/PokemonSV/Programs/EggFetcher_Core.c
@@ -8,6 +8,31 @@
 #include "NativePrograms/PokemonSwSh/Libraries/PokemonSwSh_Settings.h"
 #include "EggFetcher.h"
 
+void fly_to_gate(void) {
+    pbf_press_button(BUTTON_A, 1, 500);
+    pbf_press_button(BUTTON_Y, 1, 800);
+    pbf_move_left_joystick(STICK_MAX, STICK_MAX, 6, 500);
+    pbf_mash_button(BUTTON_A, 1000);
+};
+
+void open_picnic(void) {
+    pbf_move_left_joystick(STICK_MIN, STICK_CENTER, 190, 0);
+    pbf_press_button(BUTTON_L, 5, 100);
+    pbf_press_button(BUTTON_X, 1, 200);
+    pbf_press_button(BUTTON_A, 1, 1000);
+}
+
+void move_to_basket(void) {
+    pbf_press_button(BUTTON_L, 5, 100);
+    pbf_move_left_joystick(STICK_MIN, STICK_CENTER, 50, 0);
+    pbf_move_left_joystick(STICK_CENTER, STICK_MIN, 70, 0);
+    pbf_press_button(BUTTON_L, 5, 100);
+    pbf_move_left_joystick(STICK_MAX, STICK_CENTER, 50, 0);
+    pbf_move_left_joystick(STICK_CENTER, STICK_MAX, 100, 50);
+    pbf_press_button(BUTTON_L, 5, 100);
+    pbf_press_button(BUTTON_PLUS, 5, 200);
+}
+
 int main(void){
     // START PROGRAM ON MICROCONTROLLER
     start_program_callback();
@@ -22,9 +47,12 @@ int main(void){
     };
     pbf_press_button(BUTTON_A, 5, 500);
 
-    for (int rounds = 0; rounds < MAX_SAND; rounds++) {
-        // MOVE TO TABLE
-        pbf_move_left_joystick(0x80, 0xff, 250, 100);
+    open_picnic();
+    move_to_basket();
+
+    for (int curSand = 0; curSand < MAX_SAND; curSand++) {
+        pbf_move_left_joystick(STICK_CENTER, STICK_MIN, 250, 100);
+        // START SANDWICH
         pbf_press_button(BUTTON_A, 5, 250);
         pbf_press_button(BUTTON_A, 5, 750);
         // SELECT RECIPE
@@ -41,12 +69,13 @@ int main(void){
         pbf_wait(3000);
         pbf_press_button(BUTTON_A, 5, 150);
         // WAIT AT BASKET
-        pbf_move_left_joystick(0x80, 0x00, 5, 100);
+        pbf_move_left_joystick(STICK_CENTER, STICK_MAX, 5, 100);
         for (int rounds = 0; rounds < 10; rounds++) {
             pbf_wait(22500);
             pbf_press_button(BUTTON_A, 5, 150);
             pbf_mash_button(BUTTON_B, 4000);
         };
+        pbf_wait(200);
     };
     // EXIT TO IDLE MODE
     pbf_press_button(BUTTON_HOME, 10, 125);
