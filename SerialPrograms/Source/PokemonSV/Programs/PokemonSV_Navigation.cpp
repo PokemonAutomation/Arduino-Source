8badaab56a3003faaea14ded7e96da9b95107979
Nymphea <87930564+NympheaR@users.noreply.github.com>
2023-08-04 15:03:49 +0800

    Spacing

--- SerialPrograms/Source/PokemonSV/Programs/PokemonSV_Navigation.cpp
+++ SerialPrograms/Source/PokemonSV/Programs/PokemonSV_Navigation.cpp
@@ -189,7 +189,7 @@ void fly_to_overworld_from_map(const ProgramInfo& info, ConsoleHandle& console, 
             console.log("Detected fly confirmation prompt.");
             pbf_press_button(context, BUTTON_A, 20, 130);
             continue;
-
+            
         default:
             throw OperationFailedException(
                 ErrorReport::SEND_ERROR_REPORT, console,
@@ -297,7 +297,7 @@ void leave_picnic(const ProgramInfo& info, ConsoleHandle& console, BotBaseContex
     // Mash A to confirm
     pbf_mash_button(context, BUTTON_A, 150);
     context.wait_for_all_requests();
-    
+
     // Wait for overworld:
     OverworldWatcher overworld(COLOR_CYAN);
     int ret = wait_until(
