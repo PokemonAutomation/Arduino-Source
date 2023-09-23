df8f5a5afafcf7d8d1539378ca984c5ffe264401
Mysticial <a-yee@u.northwestern.edu>
2023-09-23 13:27:30 -0700

    Not beta.

--- SerialPrograms/Source/CommonFramework/Globals.cpp
+++ SerialPrograms/Source/CommonFramework/Globals.cpp
@@ -20,7 +20,7 @@ namespace PokemonAutomation{
 //  misleading version information.
 //
 
-const bool IS_BETA_VERSION = true;
+const bool IS_BETA_VERSION = false;
 const int PROGRAM_VERSION_MAJOR = 0;
 const int PROGRAM_VERSION_MINOR = 40;
 const int PROGRAM_VERSION_PATCH = 6;
