5c075a23f004d63b84173a2186ad539772f9d1c7
jw098 <justin.wong098@gmail.com>
2024-11-03 11:55:45 -0800

    segments 8 to 9 (#498)

---
+++ SerialPrograms/Source/PokemonSV/Programs/AutoStory/PokemonSV_AutoStory_Segment_08.cpp
@@ -0,0 +1,225 @@
+/*  AutoStory
+ *
+ *  From: https://github.com/PokemonAutomation/Arduino-Source
+ *
+ */
+
+#include "CommonFramework/GlobalSettingsPanel.h"
+#include "CommonFramework/Exceptions/FatalProgramException.h"
+#include "CommonFramework/Exceptions/OperationFailedException.h"
+#include "CommonFramework/InferenceInfra/InferenceRoutines.h"
+#include "CommonFramework/Notifications/ProgramNotifications.h"
+#include "CommonFramework/Tools/StatsTracking.h"
+#include "CommonFramework/Tools/VideoResolutionCheck.h"
+#include "NintendoSwitch/Commands/NintendoSwitch_Commands_PushButtons.h"
+#include "Pokemon/Pokemon_Strings.h"
+#include "PokemonSwSh/Inference/PokemonSwSh_IvJudgeReader.h"
+#include "PokemonSV/Programs/PokemonSV_GameEntry.h"
+#include "PokemonSV/Programs/PokemonSV_SaveGame.h"
+#include "PokemonSV/Inference/PokemonSV_TutorialDetector.h"
+#include "PokemonSV_AutoStoryTools.h"
+#include "PokemonSV_AutoStory_Segment_08.h"
+
+//#include <iostream>
+//using std::cout;
+//using std::endl;
+//#include <unordered_map>
+//#include <algorithm>
+
+namespace PokemonAutomation{
+namespace NintendoSwitch{
+namespace PokemonSV{
+
+using namespace Pokemon;
+
+
+
+
+std::string AutoStory_Segment_08::name() const{
+    return "08: Beat Team Star and arrive at School";
+}
+
+std::string AutoStory_Segment_08::start_text() const{
+    return "Start: At Mesagoza South Pokecenter.";
+}
+
+std::string AutoStory_Segment_08::end_text() const{
+    return "End: Battled Team Star, talked to Jacq, standing in classroom.";
+}
+
+void AutoStory_Segment_08::run_segment(SingleSwitchProgramEnvironment& env, BotBaseContext& context, AutoStoryOptions options) const{
+    AutoStoryStats& stats = env.current_stats<AutoStoryStats>();
+
+    context.wait_for_all_requests();
+    env.console.log("Start Segment 08: Beat Team Star and arrive at School", COLOR_ORANGE);
+
+    checkpoint_13(env, context, options.notif_status_update);
+    checkpoint_14(env, context, options.notif_status_update);
+    checkpoint_15(env, context, options.notif_status_update);
+    
+
+    context.wait_for_all_requests();
+    env.console.log("End Segment 08: Beat Team Star and arrive at School", COLOR_GREEN);
+    stats.m_segment++;
+    env.update_stats();
+
+}
+
+
+void checkpoint_13(
+    SingleSwitchProgramEnvironment& env, 
+    BotBaseContext& context, 
+    EventNotificationOption& notif_status_update
+){
+    // reset rate: 0%. 0 resets out of 70.
+    AutoStoryStats& stats = env.current_stats<AutoStoryStats>();
+    bool first_attempt = true;
+    while (true){
+    try{
+        do_action_and_monitor_for_battles(env.program_info(), env.console, context,
+        [&](const ProgramInfo& info, ConsoleHandle& console, BotBaseContext& context){        
+        
+            if (first_attempt){
+                checkpoint_save(env, context, notif_status_update);
+                first_attempt = false;
+            } 
+
+            fly_to_overlapping_flypoint(info, env.console, context);
+
+            context.wait_for_all_requests();
+            realign_player(info, env.console, context, PlayerRealignMode::REALIGN_NEW_MARKER, 0, 80, 50);
+            walk_forward_while_clear_front_path(info, env.console, context, 500);
+            walk_forward_until_dialog(info, env.console, context, NavigationMovementMode::DIRECTIONAL_ONLY, 30);
+        });
+
+        env.console.log("clear_dialog: Talk with Nemona at Mesagoza gate. Stop when detect battle.");
+        clear_dialog(env.console, context, ClearDialogMode::STOP_BATTLE, 60,
+            {CallbackEnum::PROMPT_DIALOG, CallbackEnum::DIALOG_ARROW, CallbackEnum::BATTLE});
+        
+        env.console.log("run_battle_press_A: Battle with Nemona at Mesagoza gate. Stop when detect dialog.");
+        // story continues even if you lose
+        run_battle_press_A(env.console, context, BattleStopCondition::STOP_DIALOG);
+        
+        env.console.log("clear_dialog: Talk with Nemona within Mesagoza. Stop when detect overworld.");
+        clear_dialog(env.console, context, ClearDialogMode::STOP_OVERWORLD, 60, 
+            {CallbackEnum::OVERWORLD, CallbackEnum::PROMPT_DIALOG, CallbackEnum::WHITE_A_BUTTON});
+        
+       
+        break;
+    }catch(...){
+        context.wait_for_all_requests();
+        env.console.log("Resetting from checkpoint.");
+        reset_game(env.program_info(), env.console, context);
+        stats.m_reset++;
+        env.update_stats();
+    }             
+    }
+
+}
+
+void checkpoint_14(
+    SingleSwitchProgramEnvironment& env, 
+    BotBaseContext& context, 
+    EventNotificationOption& notif_status_update
+){
+    AutoStoryStats& stats = env.current_stats<AutoStoryStats>();
+    bool first_attempt = true;
+    while (true){
+    try{
+        if (first_attempt){
+            checkpoint_save(env, context, notif_status_update);
+            first_attempt = false;
+        }         
+        context.wait_for_all_requests();
+        // realign diagonally to the left
+        realign_player(env.program_info(), env.console, context, PlayerRealignMode::REALIGN_NEW_MARKER, 80, 0, 100);
+        // walk forward so you're off center
+        pbf_move_left_joystick(context, 128, 0, 100, 100);
+        // realign going straight
+        realign_player(env.program_info(), env.console, context, PlayerRealignMode::REALIGN_NEW_MARKER, 128, 0, 100);
+        // walk forward, while still off center
+        pbf_move_left_joystick(context, 128, 0, 2000, 100);
+        // realign diagonally to the right
+        realign_player(env.program_info(), env.console, context, PlayerRealignMode::REALIGN_NEW_MARKER, 178, 0, 100);
+        // walk forward so you're closer to the center
+        pbf_move_left_joystick(context, 128, 0, 150, 100);
+        // realign going straight
+        realign_player(env.program_info(), env.console, context, PlayerRealignMode::REALIGN_NEW_MARKER, 128, 0, 100);
+        // walk forward until hit dialog at top of stairs
+        walk_forward_until_dialog(env.program_info(), env.console, context, NavigationMovementMode::DIRECTIONAL_ONLY, 60);
+        // clear dialog until battle. with prompt, battle
+        env.console.log("clear_dialog: Talk with Team Star at the top of the stairs. Stop when detect battle.");
+        clear_dialog(env.console, context, ClearDialogMode::STOP_BATTLE, 60, {CallbackEnum::PROMPT_DIALOG, CallbackEnum::BATTLE, CallbackEnum::DIALOG_ARROW});
+        // run battle until dialog
+        env.console.log("run_battle_press_A: Battle with Team Star grunt 1. Stop when detect dialog.");
+        run_battle_press_A(env.console, context, BattleStopCondition::STOP_DIALOG, {}, true);
+        // clear dialog until battle, with prompt, white button, tutorial, battle
+        env.console.log("clear_dialog: Talk with Team Star and Nemona. Receive Tera orb. Stop when detect battle.");
+        clear_dialog(env.console, context, ClearDialogMode::STOP_BATTLE, 60, 
+            {CallbackEnum::PROMPT_DIALOG, CallbackEnum::WHITE_A_BUTTON, CallbackEnum::TUTORIAL, CallbackEnum::BATTLE, CallbackEnum::DIALOG_ARROW});
+        // run battle until dialog
+        env.console.log("run_battle_press_A: Battle with Team Star grunt 2. Stop when detect dialog.");
+        run_battle_press_A(env.console, context, BattleStopCondition::STOP_DIALOG, {}, true);
+        // clear dialog until overworld
+        clear_dialog(env.console, context, ClearDialogMode::STOP_OVERWORLD, 60, {CallbackEnum::OVERWORLD});
+       
+        break;
+    }catch(...){
+        context.wait_for_all_requests();
+        env.console.log("Resetting from checkpoint.");
+        reset_game(env.program_info(), env.console, context);
+        stats.m_reset++;
+        env.update_stats();
+    }             
+    }
+
+}
+
+void checkpoint_15(
+    SingleSwitchProgramEnvironment& env, 
+    BotBaseContext& context, 
+    EventNotificationOption& notif_status_update
+){
+    AutoStoryStats& stats = env.current_stats<AutoStoryStats>();
+    bool first_attempt = true;
+    while (true){
+    try{
+        if (first_attempt){
+            checkpoint_save(env, context, notif_status_update);
+            first_attempt = false;
+        }         
+        context.wait_for_all_requests();
+        // realign diagonally to the right
+        realign_player(env.program_info(), env.console, context, PlayerRealignMode::REALIGN_NEW_MARKER, 178, 0, 100);
+        // walk forward so you're closer to the center
+        pbf_move_left_joystick(context, 128, 0, 100, 100);
+        // realign going straight
+        realign_player(env.program_info(), env.console, context, PlayerRealignMode::REALIGN_NEW_MARKER, 128, 0, 100);
+        // walk forward up stairs
+        pbf_move_left_joystick(context, 128, 0, 1000, 100);
+        // realign going straight
+        realign_player(env.program_info(), env.console, context, PlayerRealignMode::REALIGN_OLD_MARKER);
+        // walk forward until hit dialog inside the school
+        walk_forward_until_dialog(env.program_info(), env.console, context, NavigationMovementMode::DIRECTIONAL_ONLY, 60);
+
+        env.console.log("clear_dialog: Talk with Nemona, Clavell, and Jacq inside the school. Stop when detect overworld.");
+        clear_dialog(env.console, context, ClearDialogMode::STOP_OVERWORLD, 60, 
+            {CallbackEnum::PROMPT_DIALOG, CallbackEnum::OVERWORLD});
+       
+        break;
+    }catch(...){
+        context.wait_for_all_requests();
+        env.console.log("Resetting from checkpoint.");
+        reset_game(env.program_info(), env.console, context);
+        stats.m_reset++;
+        env.update_stats();
+    }             
+    }
+
+}
+
+
+
+}
+}
+}
---
+++ SerialPrograms/Source/PokemonSV/Programs/AutoStory/PokemonSV_AutoStory_Segment_09.cpp
@@ -0,0 +1,328 @@
+/*  AutoStory
+ *
+ *  From: https://github.com/PokemonAutomation/Arduino-Source
+ *
+ */
+
+#include "CommonFramework/GlobalSettingsPanel.h"
+#include "CommonFramework/Exceptions/FatalProgramException.h"
+#include "CommonFramework/Exceptions/OperationFailedException.h"
+#include "CommonFramework/InferenceInfra/InferenceRoutines.h"
+#include "CommonFramework/Notifications/ProgramNotifications.h"
+#include "CommonFramework/Tools/StatsTracking.h"
+#include "CommonFramework/Tools/VideoResolutionCheck.h"
+#include "NintendoSwitch/Commands/NintendoSwitch_Commands_PushButtons.h"
+#include "Pokemon/Pokemon_Strings.h"
+#include "PokemonSwSh/Inference/PokemonSwSh_IvJudgeReader.h"
+#include "PokemonSV/Programs/PokemonSV_GameEntry.h"
+#include "PokemonSV/Programs/PokemonSV_SaveGame.h"
+#include "PokemonSV/Inference/PokemonSV_TutorialDetector.h"
+#include "PokemonSV_AutoStoryTools.h"
+#include "PokemonSV_AutoStory_Segment_09.h"
+
+//#include <iostream>
+//using std::cout;
+//using std::endl;
+//#include <unordered_map>
+//#include <algorithm>
+
+namespace PokemonAutomation{
+namespace NintendoSwitch{
+namespace PokemonSV{
+
+using namespace Pokemon;
+
+
+
+
+std::string AutoStory_Segment_09::name() const{
+    return "09: Complete tutorial";
+}
+
+std::string AutoStory_Segment_09::start_text() const{
+    return "Start: Battled Team Star, talked to Jacq, standing in classroom.";
+}
+
+std::string AutoStory_Segment_09::end_text() const{
+    return "End: Finished tutorial. Acquired all 3 questlines. Got on ride for first time.";
+}
+
+void AutoStory_Segment_09::run_segment(SingleSwitchProgramEnvironment& env, BotBaseContext& context, AutoStoryOptions options) const{
+    AutoStoryStats& stats = env.current_stats<AutoStoryStats>();
+
+    context.wait_for_all_requests();
+    env.console.overlay().add_log("Start Segment 09: Complete tutorial", COLOR_ORANGE);
+
+    checkpoint_16(env, context, options.notif_status_update);
+    checkpoint_17(env, context, options.notif_status_update);
+    checkpoint_18(env, context, options.notif_status_update);
+    checkpoint_19(env, context, options.notif_status_update);
+    checkpoint_20(env, context, options.notif_status_update);
+
+    context.wait_for_all_requests();
+    env.console.log("End Segment 09: Complete tutorial", COLOR_GREEN);
+    stats.m_segment++;
+    env.update_stats();
+
+}
+
+
+
+
+void checkpoint_16(
+    SingleSwitchProgramEnvironment& env, 
+    BotBaseContext& context, 
+    EventNotificationOption& notif_status_update
+){
+    AutoStoryStats& stats = env.current_stats<AutoStoryStats>();
+    bool first_attempt = true;
+    while (true){
+    try{
+        if (first_attempt){
+            checkpoint_save(env, context, notif_status_update);
+            first_attempt = false;
+        }         
+        context.wait_for_all_requests();
+
+        // walk left
+        pbf_move_left_joystick(context, 0, 128, 400, 100);
+        // walk down to classroom exit.
+        pbf_move_left_joystick(context, 128, 255, 300, 100);
+        env.console.log("clear_dialog: Leave classroom.");
+        clear_dialog(env.console, context, ClearDialogMode::STOP_TIMEOUT, 5);
+
+        // Wait for detection of school navigation menu
+        wait_for_gradient_arrow(env.program_info(), env.console, context, {0.031, 0.193, 0.047, 0.078}, 5);
+
+        // enter Cafeteria
+        pbf_mash_button(context, BUTTON_A, 3 * TICKS_PER_SECOND);
+        pbf_wait(context, 3 * TICKS_PER_SECOND);
+        context.wait_for_all_requests();
+
+        // walk forward
+        pbf_move_left_joystick(context, 128, 0, 300, 100);
+        // turn left 
+        pbf_move_left_joystick(context, 0, 128, 50, 100);
+
+        // talk to Arven. stop at overworld. need prompt, overworld, white button A. and book?
+        env.console.log("Talk with Arven. Receive Titan questline (Path of Legends).");
+        press_A_until_dialog(env.program_info(), env.console, context, 1);
+        mash_button_till_overworld(env.console, context, BUTTON_A, 360);
+        
+       
+        break;
+    }catch(...){
+        context.wait_for_all_requests();
+        env.console.log("Resetting from checkpoint.");
+        reset_game(env.program_info(), env.console, context);
+        stats.m_reset++;
+        env.update_stats();
+    }             
+    }
+
+}
+
+void checkpoint_17(
+    SingleSwitchProgramEnvironment& env, 
+    BotBaseContext& context, 
+    EventNotificationOption& notif_status_update
+){
+    AutoStoryStats& stats = env.current_stats<AutoStoryStats>();
+    bool first_attempt = true;
+    while (true){
+    try{
+        if (first_attempt){
+            checkpoint_save(env, context, notif_status_update);
+            first_attempt = false;
+        }         
+        context.wait_for_all_requests();
+
+        // walk backwards until dialog
+        walk_forward_until_dialog(env.program_info(), env.console, context, NavigationMovementMode::DIRECTIONAL_ONLY, 20, 255);
+        env.console.log("Talk with Cassiopeia.");
+        mash_button_till_overworld(env.console, context, BUTTON_A, 360);
+
+        // re-orient camera
+        pbf_press_button(context, BUTTON_L, 20, 100);
+        // move backwards towards front desk
+        pbf_move_left_joystick(context, 128, 255, 200, 100);
+        // re-orient camera
+        pbf_press_button(context, BUTTON_L, 20, 100);
+        // move right towards navigation kiosk
+        pbf_move_left_joystick(context, 255, 128, 100, 100);
+        // open school navigation screen
+        press_button_until_gradient_arrow(env.program_info(), env.console, context, {0.031, 0.193, 0.047, 0.078});
+        // go to staff room
+        basic_menu_navigation(env.program_info(), env.console, context, {0.031, 0.193, 0.047, 0.078}, {0.031, 0.193 + 0.074219, 0.047, 0.078}, DPAD_DOWN, 1);
+        // enter staff room
+        pbf_mash_button(context, BUTTON_A, 3 * TICKS_PER_SECOND);
+        pbf_wait(context, 3 * TICKS_PER_SECOND);
+
+        env.console.log("clear_dialog: See Geeta. Talk to Nemona. Receive Gym/Elite Four questline (Victory Road).");
+        clear_dialog(env.console, context, ClearDialogMode::STOP_OVERWORLD, 60, 
+            {CallbackEnum::OVERWORLD, CallbackEnum::PROMPT_DIALOG});
+
+        
+       
+        break;
+    }catch(...){
+        context.wait_for_all_requests();
+        env.console.log("Resetting from checkpoint.");
+        reset_game(env.program_info(), env.console, context);
+        stats.m_reset++;
+        env.update_stats();
+    }             
+    }
+
+}
+
+void checkpoint_18(
+    SingleSwitchProgramEnvironment& env, 
+    BotBaseContext& context, 
+    EventNotificationOption& notif_status_update
+){
+    AutoStoryStats& stats = env.current_stats<AutoStoryStats>();
+    bool first_attempt = true;
+    while (true){
+    try{
+        if (first_attempt){
+            checkpoint_save(env, context, notif_status_update);
+            first_attempt = false;
+        }         
+        context.wait_for_all_requests();
+
+        // walk down
+        pbf_move_left_joystick(context, 128, 255, 200, 100);
+        // walk left towards door
+        pbf_move_left_joystick(context, 0, 128, 100, 100);
+
+        // wait for school navigation menu
+        context.wait_for_all_requests();
+        wait_for_gradient_arrow(env.program_info(), env.console, context, {0.031, 0.193, 0.047, 0.078}, 10);
+        // enter Directors office
+        pbf_mash_button(context, BUTTON_A, 6 * TICKS_PER_SECOND);
+
+        env.console.log("Talk to Clavell in his office, and the professor.");
+        clear_dialog(env.console, context, ClearDialogMode::STOP_TIMEOUT, 25, 
+            {CallbackEnum::PROMPT_DIALOG});  // max time between dialog: 17s. set timeout to 25 seconds for buffer.
+        // mash A to get through the Random A press that you need. when the professor shows you area zero.
+        pbf_mash_button(context, BUTTON_A, 3 * TICKS_PER_SECOND);
+        clear_dialog(env.console, context, ClearDialogMode::STOP_OVERWORLD, 60, 
+            {CallbackEnum::OVERWORLD, CallbackEnum::PROMPT_DIALOG});
+       
+        break;
+    }catch(...){
+        context.wait_for_all_requests();
+        env.console.log("Resetting from checkpoint.");
+        reset_game(env.program_info(), env.console, context);
+        stats.m_reset++;
+        env.update_stats();
+    }             
+    }
+
+}
+
+void checkpoint_19(
+    SingleSwitchProgramEnvironment& env, 
+    BotBaseContext& context, 
+    EventNotificationOption& notif_status_update
+){
+    AutoStoryStats& stats = env.current_stats<AutoStoryStats>();
+    bool first_attempt = true;
+    while (true){
+    try{
+        if (first_attempt){
+            checkpoint_save(env, context, notif_status_update);
+            first_attempt = false;
+        }         
+        context.wait_for_all_requests();
+
+        // walk right 
+        pbf_move_left_joystick(context, 255, 128, 50, 100);
+        // walk down towards door
+        pbf_move_left_joystick(context, 128, 255, 200, 100);
+
+        env.console.log("Talk to Nemona and go to dorm.");
+        mash_button_till_overworld(env.console, context, BUTTON_A, 360);
+
+        // walk forward
+        pbf_move_left_joystick(context, 128, 0, 100, 100);
+        // walk left towards bed
+        pbf_move_left_joystick(context, 0, 128, 100, 100);
+
+        env.console.log("Go to bed. Time passes until treasure hunt.");
+        mash_button_till_overworld(env.console, context, BUTTON_A, 360);
+
+       
+        break;
+    }catch(...){
+        context.wait_for_all_requests();
+        env.console.log("Resetting from checkpoint.");
+        reset_game(env.program_info(), env.console, context);
+        stats.m_reset++;
+        env.update_stats();
+    }             
+    }
+
+}
+
+
+void checkpoint_20(
+    SingleSwitchProgramEnvironment& env, 
+    BotBaseContext& context, 
+    EventNotificationOption& notif_status_update
+){
+    AutoStoryStats& stats = env.current_stats<AutoStoryStats>();
+    bool first_attempt = true;
+    while (true){
+    try{
+        if (first_attempt){
+            checkpoint_save(env, context, notif_status_update);
+            first_attempt = false;
+        }         
+        context.wait_for_all_requests();
+       
+        //walk right towards door
+        pbf_move_left_joystick(context, 255, 128, 200, 100);
+
+        wait_for_gradient_arrow(env.program_info(), env.console, context, {0.031, 0.193, 0.047, 0.078}, 10);
+
+        env.console.log("Leave dorm for schoolyard.");
+        mash_button_till_overworld(env.console, context, BUTTON_A, 360);
+
+        walk_forward_until_dialog(env.program_info(), env.console, context, NavigationMovementMode::DIRECTIONAL_ONLY, 60, 0);
+
+        env.console.log("Talk to Nemona, Arven, Cassiopeia.");
+        clear_dialog(env.console, context, ClearDialogMode::STOP_TIMEOUT, 16, 
+            {CallbackEnum::PROMPT_DIALOG, CallbackEnum::BLACK_DIALOG_BOX}); // max time between dialog: 11
+        
+        // mash A to get through the Random A press that you need. when the Nemona shows you a Poke Gym.
+        pbf_mash_button(context, BUTTON_A, 250);
+
+        clear_dialog(env.console, context, ClearDialogMode::STOP_TIMEOUT, 10, 
+            {CallbackEnum::TUTORIAL}); // max time between dialog: 3
+        
+        env.console.log("Get on ride.");
+        pbf_mash_button(context, BUTTON_PLUS, 1 * TICKS_PER_SECOND);
+
+        mash_button_till_overworld(env.console, context, BUTTON_A, 360);
+
+        break;
+    }catch(...){
+        context.wait_for_all_requests();
+        env.console.log("Resetting from checkpoint.");
+        reset_game(env.program_info(), env.console, context);
+        stats.m_reset++;
+        env.update_stats();
+    }             
+    }
+
+}
+
+
+
+
+
+}
+}
+}
