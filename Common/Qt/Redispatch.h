f3bb8ea93fd74e5354da229a9faa1998a9b5b4fe
Alexander Yee <a-yee@u.northwestern.edu>
2024-11-24 16:08:13 -0800

    Fix build.

--- Common/Qt/Redispatch.h
+++ Common/Qt/Redispatch.h
@@ -12,9 +12,10 @@
 namespace PokemonAutomation{
 
 
-//  Disable these since they are dangerous.
-//void dispatch_to_main_thread(std::function<void()> lambda);
-//void queue_on_main_thread(std::function<void()> lambda);
+//  Be careful with these due to re-entrancy and object lifetime.
+void dispatch_to_main_thread(std::function<void()> lambda);
+void queue_on_main_thread(std::function<void()> lambda);
+
 
 void run_on_main_thread_and_wait(std::function<void()> lambda);
 
