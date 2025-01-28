c2b6f01d496685c40681da727884e1640ac223ce
kichithewolf <kichithewolf@gmail.com>
2025-01-28 09:31:22 -0500

    Update PokemonRSE_SoundListener.cpp

--- SerialPrograms/Source/PokemonRSE/Programs/TestPrograms/PokemonRSE_SoundListener.cpp
+++ SerialPrograms/Source/PokemonRSE/Programs/TestPrograms/PokemonRSE_SoundListener.cpp
@@ -58,9 +58,6 @@ SoundListener::SoundListener()
     PA_ADD_OPTION(STOP_ON_DETECTED_SOUND);
 }
 
-
-// void search_alpha_roar_from_audio_dump();
-
 void SoundListener::program(SingleSwitchProgramEnvironment& env, SwitchControllerContext& context){
     // search_alpha_roar_from_audio_dump();
     // return;
@@ -95,100 +92,6 @@ void SoundListener::program(SingleSwitchProgramEnvironment& env, SwitchControlle
 
     std::cout << "Audio test program Sound listener finished." << std::endl;
 }
-
-
-// A function used to search for the alpha roar on LA audio dump.
-// But we didn't find the shound sound :P
-void search_alpha_roar_from_audio_dump(){
-
-    const size_t SAMPLE_RATE = 48000;
-
-    SpectrogramMatcher matcher(
-        "Alpha Roar",
-        AudioTemplateCache::instance().get_throw("PokemonBDSP/AlphaRoar", SAMPLE_RATE),
-        SpectrogramMatcher::Mode::RAW, SAMPLE_RATE,
-        100.0
-    );
-    
-    // std::string file_listFile = "./scripts/short_audio_files.txt";
-    std::string file_listFile = "1.txt";
-    // std::string file_listFile = "./scripts/all_audio_files.txt";
-    std::ifstream fin(file_listFile.c_str());
-    std::vector<std::string> file_list;
-    while(!fin.eof()){
-        std::string line;
-        std::getline(fin, line);
-        file_list.push_back(line);
-        fin >> std::ws;
-    }
-    std::cout << "File num " << file_list.size() << std::endl;
-
-    std::map<float, std::string> closest_files;
-
-    std::ofstream fout("file_check_output.txt");
-
-    for(size_t fileIdx = 0; fileIdx < file_list.size(); fileIdx++){
-        matcher.clear();
-
-
-        const auto& path = file_list[fileIdx];
-        std::ostringstream os;
-        os << "File " << fileIdx << "/" << file_list.size() << " " << path << " ";
-        AudioTemplate audio = loadAudioTemplate(path);
-        if (audio.numWindows() == 0){
-            os << "Fail" << std::endl;
-            fout << os.str();
-            std::cout << os.str() << std::flush;
-            continue;
-        }
-
-        // audio.scale(2.0);
-
-        os << "#W " << audio.numWindows() << " ";
-
-        // match!
-        float minScore = FLT_MAX;
-        std::vector<AudioSpectrum> new_spectrums;
-        size_t numStreamWindows = std::max(matcher.numMatchedWindows(), audio.numWindows());
-        for(size_t audioIdx = 0; audioIdx < numStreamWindows; audioIdx++){
-            new_spectrums.clear();
-            AlignedVector<float> freqVector(audio.numFrequencies());
-            if (audioIdx < audio.numWindows()){
-                const float * freq = audio.getWindow(audioIdx);
-                memcpy(freqVector.data(), freq, sizeof(float) * audio.numFrequencies());
-            }else{
-                // add zero-freq window
-            }
-            new_spectrums.emplace_back(
-                audioIdx, SAMPLE_RATE,
-                std::make_unique<AlignedVector<float>>(std::move(freqVector))
-            );
-            float score = matcher.match(new_spectrums);
-            minScore = std::min(score, minScore);
-        } // end audio Idx
-
-        os << "dist " << minScore << std::endl;
-        fout << os.str();
-        std::cout << os.str() << std::flush;
-
-        closest_files.emplace(minScore, path);
-    }
-
-    fout.close();
-
-    auto it = closest_files.begin();
-    std::cout << "--------------" << std::endl;
-    fout.open("file_check_output_sorted.txt");
-    for(int i = 0; it != closest_files.end(); i++, it++){
-        if (i < 40)
-            std::cout << it->first << ", " << it->second << std::endl;
-        fout << it->first << ", " << it->second << std::endl;
-    }
-    fout.close();
-    return;
-}
-
-
 
 
 
