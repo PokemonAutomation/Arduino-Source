6e47810cfcfe56af5692697b680a3c85075ce9d7
kichithewolf <kichithewolf@gmail.com>
2025-01-28 15:20:16 -0500

    non-test starter reset, panels

--- SerialPrograms/Source/PokemonRSE/PokemonRSE_Panels.cpp
+++ SerialPrograms/Source/PokemonRSE/PokemonRSE_Panels.cpp
@@ -10,8 +10,8 @@
 
 #include "PokemonRSE_Settings.h"
 
-//#include "Programs/ShinyHunting/PokemonRSE_AudioStarterReset.h"
-//#include "Programs/ShinyHunting/PokemonRSE_StarterReset.h"
+#include "Programs/ShinyHunting/PokemonRSE_AudioStarterReset.h"
+#include "Programs/ShinyHunting/PokemonRSE_StarterReset.h"
 #include "Programs/TestPrograms/PokemonRSE_SoundListener.h"
 
 namespace PokemonAutomation{
@@ -33,12 +33,12 @@ std::vector<PanelEntry> PanelListFactory::make_panels() const{
     //ret.emplace_back("---- General ----");
 
     ret.emplace_back("---- Shiny Hunting ----");
-    //ret.emplace_back(make_single_switch_program<AudioStarterReset_Descriptor, AudioStarterReset>());
+    ret.emplace_back(make_single_switch_program<AudioStarterReset_Descriptor, AudioStarterReset>());
     
 
     if (PreloadSettings::instance().DEVELOPER_MODE){
         ret.emplace_back("---- Test ----");
-        //ret.emplace_back(make_single_switch_program<StarterReset_Descriptor, StarterReset>()); //outdated early test program
+        ret.emplace_back(make_single_switch_program<StarterReset_Descriptor, StarterReset>()); //outdated early test program
 
         ret.emplace_back("---- Developer Tools ----");
         ret.emplace_back(make_single_switch_program<SoundListener_Descriptor, SoundListener>());
