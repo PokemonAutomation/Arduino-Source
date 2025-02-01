0ad76a6a6f8d9d9f7fe08ee730607ced1f863afa
Eric Zhang <ericzklm@hotmail.com>
2025-02-01 15:11:19 -0800

    QT6.5 Backend Workaround
    Workaround for MacOS involving QVideoFrames

--- SerialPrograms/Source/CommonFramework/VideoPipeline/Backends/CameraWidgetQt6.5.cpp
+++ SerialPrograms/Source/CommonFramework/VideoPipeline/Backends/CameraWidgetQt6.5.cpp
@@ -275,9 +275,11 @@ void CameraSession::connect_video_sink(QVideoSink* sink){
                 WriteSpinLock lg(m_frame_lock);
 
                 //  Skip duplicate frames.
+                #ifndef __APPLE__
                 if (frame.startTime() <= m_last_frame.startTime()){
                     return;
                 }
+                #endif
 
                 m_last_frame = frame;
                 m_last_frame_timestamp = now;
