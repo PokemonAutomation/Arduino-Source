a1bb543c42b385f13e50c9a48253aea0b40f8afe
kichithewolf <kichithewolf@gmail.com>
2025-03-19 10:05:38 -0400

    lgpe gift reset, feedback type for lgpe programs

---
+++ SerialPrograms/Source/PokemonLGPE/Programs/ShinyHunting/PokemonLGPE_GiftReset.h
@@ -0,0 +1,49 @@
+/*  LGPE Gift Reset
+ *
+ *  From: https://github.com/PokemonAutomation/
+ *
+ */
+
+#ifndef PokemonAutomation_PokemonLGPE_GiftReset_H
+#define PokemonAutomation_PokemonLGPE_GiftReset_H
+
+#include "NintendoSwitch/Controllers/NintendoSwitch_Joycon.h"
+#include "NintendoSwitch/NintendoSwitch_SingleSwitchProgram.h"
+#include "NintendoSwitch/Options/NintendoSwitch_GoHomeWhenDoneOption.h"
+#include "CommonFramework/Notifications/EventNotificationsTable.h"
+#include "Common/Cpp/Options/SimpleIntegerOption.h"
+
+namespace PokemonAutomation{
+namespace NintendoSwitch{
+namespace PokemonLGPE{
+
+class GiftReset_Descriptor : public SingleSwitchProgramDescriptor{
+public:
+    GiftReset_Descriptor();
+    struct Stats;
+    virtual std::unique_ptr<StatsTracker> make_stats() const override;
+};
+
+class GiftReset : public SingleSwitchProgramInstance{
+public:
+    GiftReset();
+    virtual void program(SingleSwitchProgramEnvironment& env, CancellableScope& scope) override;
+
+private:
+    GoHomeWhenDoneOption GO_HOME_WHEN_DONE;
+
+    EventNotificationOption NOTIFICATION_SHINY;
+    EventNotificationOption NOTIFICATION_STATUS_UPDATE;
+    EventNotificationsOption NOTIFICATIONS;
+};
+
+
+
+
+}
+}
+}
+#endif
+
+
+
