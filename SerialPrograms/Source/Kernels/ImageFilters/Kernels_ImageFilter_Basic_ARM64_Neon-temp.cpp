e2286c8ae3c232a84c7d076402ce2fe2c06bf30c
Alexander Yee <a-yee@u.northwestern.edu>
2025-05-07 00:33:04 -0700

    Rename part 1.

---
+++ SerialPrograms/Source/Kernels/ImageFilters/Kernels_ImageFilter_Basic_ARM64_Neon-temp.cpp
@@ -0,0 +1,35 @@
+/*  Image Filters Basic (arm64 NEON)
+ *
+ *  From: https://github.com/PokemonAutomation/
+ *
+ */
+
+#ifdef PA_AutoDispatch_arm64_20_M1
+
+#include <stdint.h>
+#include <cstddef>
+#include "Kernels/Kernels_arm64_NEON.h"
+// #include "Kernels/PartialWordAccess/Kernels_PartialWordAccess_arm64_NEON.h"
+#include "Kernels_ImageFilter_Basic_Routines.h"
+
+#include <iostream>
+#include <iomanip>
+
+namespace PokemonAutomation{
+namespace Kernels{
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
+#endif
