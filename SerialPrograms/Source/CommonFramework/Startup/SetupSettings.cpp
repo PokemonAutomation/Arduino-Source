a5765edbab94f7e994f0b352f152326b5addbfac
Gin <>
2025-05-10 10:53:02 -0700

    Add missing resources msg box

--- SerialPrograms/Source/CommonFramework/Startup/SetupSettings.cpp
+++ SerialPrograms/Source/CommonFramework/Startup/SetupSettings.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <QFile>
+#include <QDir>
 #include <QMessageBox>
 #include <QApplication>
 #if QT_VERSION >= QT_VERSION_CHECK(6, 5, 0)
@@ -23,27 +24,27 @@ using std::endl;
 
 namespace PokemonAutomation{
 
-bool migrate_settings(Logger& logger, std::string file_name){
-    QFile root_file(QString::fromStdString(file_name));
+bool migrate_settings(Logger& logger, const std::string& file_name){
+    QFile cur_dir_file(QString::fromStdString(file_name));
     QFile folder_file(QString::fromStdString(SETTINGS_PATH() + file_name));
 
-    logger.log("Checking settings configuration...");
+    logger.log("Checking if user setting JSON file need to be migrated...");
 
-    if (!root_file.exists() && !folder_file.exists()){
-        logger.log("Clean install, nothing to do.");
+    if (!cur_dir_file.exists() && !folder_file.exists()){
+        logger.log("Clean install, nothing to migrate.");
         return true;
     }
 
-    if (!root_file.exists() && folder_file.exists()){
+    if (!cur_dir_file.exists() && folder_file.exists()){
         logger.log("Migrated, nothing to do.");
         return true;
     }
 
-    if (root_file.exists() && !folder_file.exists()){
-        logger.log("Migrating root file to the folder...");
-        root_file.copy(folder_file.fileName());
-        logger.log("Renaming root file as backup...");
-        root_file.rename(root_file.fileName() + ".bak");
+    if (cur_dir_file.exists() && !folder_file.exists()){
+        logger.log("Migrating old setting file to the new folder...");
+        cur_dir_file.copy(folder_file.fileName());
+        logger.log("Renaming old setting file as backup...");
+        cur_dir_file.rename(cur_dir_file.fileName() + ".bak");
         logger.log("Migrated.");
         QMessageBox box;
         box.setTextFormat(Qt::RichText);
@@ -58,29 +59,26 @@ bool migrate_settings(Logger& logger, std::string file_name){
         return true;
     }
 
-    if (root_file.exists() && folder_file.exists()){
-        logger.log("Two configs detected, showing prompt...");
-        QMessageBox box;
-        box.setTextFormat(Qt::RichText);
-        box.critical(
-            nullptr,
-            "Two settings files detected!",
-            QString::fromStdString(
-                "Detected two settings files at these locations:<br><br>" +
-                root_file.fileName().toStdString() + " (versions 0.29 and older)<br>" +
-                folder_file.fileName().toStdString() + " (version 0.30)" +
-                "<br><br>This probably happened because you used an early beta of v0.30.x which created a new settings file at the new location. "
-                "Please either delete or rename the file you do not want to use."
-            )
-        );
-        return false;
-    }
-
-    return true;
+    // last case: cur_dir_file.exists() && folder_file.exists()
+    logger.log("Two configs detected, showing prompt...");
+    QMessageBox box;
+    box.setTextFormat(Qt::RichText);
+    box.critical(
+        nullptr,
+        "Two settings files detected!",
+        QString::fromStdString(
+            "Detected two settings files at these locations:<br><br>" +
+            cur_dir_file.fileName().toStdString() + " (versions 0.29 and older)<br>" +
+            folder_file.fileName().toStdString() + " (version 0.30)" +
+            "<br><br>This probably happened because you used an early beta of v0.30.x which created a new "
+            "settings file at the new location. Please either delete or rename the file you do not want to use."
+        )
+    );
+    return false;
 }
 
 bool migrate_stats(Logger& logger){
-    logger.log("Checking if stats should be migrated...");
+    logger.log("Checking if stats file should be migrated...");
 
     std::string path = GlobalSettings::instance().STATS_FILE;
     if (path != "PA-Stats.txt"){
@@ -90,25 +88,27 @@ bool migrate_stats(Logger& logger){
 
     const std::string new_path = GlobalSettings::instance().STATS_FILE.default_value();
 
-    QFile root_file(QString::fromStdString(path));
+    // old location: current working directory
+    QFile cur_dir_file(QString::fromStdString(path));
+    // new location: 
     QFile folder_file(QString::fromStdString(new_path));
 
-    if (!root_file.exists() && !folder_file.exists()){
-        logger.log("Clean install, nothing to do.");
+    if (!cur_dir_file.exists() && !folder_file.exists()){
+        logger.log("Clean install, nothing to migrate.");
         return true;
     }
 
-    if (!root_file.exists() && folder_file.exists()){
+    if (!cur_dir_file.exists() && folder_file.exists()){
         logger.log("File migrated. Stats path in settings not updated. Updating...");
         GlobalSettings::instance().STATS_FILE.restore_defaults();
         return true;
     }
 
-    if (root_file.exists() && !folder_file.exists()){
-        logger.log("Migrating root file to the folder...");
-        root_file.copy(folder_file.fileName());
-        logger.log("Renaming root file as backup...");
-        root_file.rename(root_file.fileName() + ".bak");
+    if (cur_dir_file.exists() && !folder_file.exists()){
+        logger.log("Migrating old stats file to the folder...");
+        cur_dir_file.copy(folder_file.fileName());
+        logger.log("Renaming old stats file as backup...");
+        cur_dir_file.rename(cur_dir_file.fileName() + ".bak");
         GlobalSettings::instance().STATS_FILE.restore_defaults();
         logger.log("Migrated.");
         QMessageBox box;
@@ -124,25 +124,22 @@ bool migrate_stats(Logger& logger){
         return true;
     }
 
-    if (root_file.exists() && folder_file.exists()){
-        logger.log("Two stats files detected, showing prompt...");
-        QMessageBox box;
-        box.setTextFormat(Qt::RichText);
-        box.critical(
-            nullptr,
-            "Two settings files detected!",
-            QString::fromStdString(
-                "Detected two stats files at these locations:<br><br>" +
-                root_file.fileName().toStdString() + " (versions 0.29 and older)<br>" +
-                folder_file.fileName().toStdString() + " (version 0.30)" +
-                "<br><br>This probably happened because you used an early beta of v0.30.x which created a new stats file at the new location. "
-                "Please either delete or rename the file you do not want to use."
-            )
-        );
-        return false;
-    }
-
-    return true;
+    // last case: cur_dir_file.exists() && folder_file.exists()
+    logger.log("Two stats files detected, showing prompt...");
+    QMessageBox box;
+    box.setTextFormat(Qt::RichText);
+    box.critical(
+        nullptr,
+        "Two settings files detected!",
+        QString::fromStdString(
+            "Detected two stats files at these locations:<br><br>" +
+            cur_dir_file.fileName().toStdString() + " (versions 0.29 and older)<br>" +
+            folder_file.fileName().toStdString() + " (version 0.30)" +
+            "<br><br>This probably happened because you used an early beta of v0.30.x which created a new stats file at the new location. "
+            "Please either delete or rename the file you do not want to use."
+        )
+    );
+    return false;
 }
 
 
@@ -198,6 +195,28 @@ void set_permissions(QObject& object){
 }
 
 
+bool check_resource_folder(Logger& logger){
+    QDir resources_folder(QString::fromStdString(RESOURCE_PATH()));
+    if (resources_folder.exists()){
+        logger.log("Found Resource folder at " + RESOURCE_PATH());
+        return true;
+    }
+    logger.log("No Resources/ folder at " + RESOURCE_PATH());
+    QMessageBox box;
+    box.setTextFormat(Qt::RichText);
+    box.critical(
+        nullptr,
+        "No Resources Folder!",
+        QString::fromStdString(
+            "No Resources Folder:<br><br>"
+            "Make sure you download Resources folder from latest release at<br>"
+            "https://github.com/PokemonAutomation/ComputerControl/releases/<br><br>"
+            "The Resources folder is part of PA-SerialPrograms-[VERSION]-[DATE].zip<br><br>"
+            "Extract the Resources folder from the zip file and place it at following path:<br><br>" + RESOURCE_PATH()
+        )
+    );
+    return false;
+}
 
 
 
--- SerialPrograms/Source/CommonFramework/Startup/SetupSettings.h
+++ SerialPrograms/Source/CommonFramework/Startup/SetupSettings.h
@@ -15,14 +15,23 @@ namespace PokemonAutomation{
 
 class Logger;
 
+// Move user setting JSON file from old location to the new one
+// return true if there is no need for migration or the migration is successful
+// return false if migration fails. In this case it will also pop a critical error message box
+bool migrate_settings(Logger& logger, const std::string& file_name);
 
-bool migrate_settings(Logger& logger, std::string file_name);
+// Move stats file from old location to the new one
+// return true if there is no need for migration or the migration is successful
+// return false if migration fails. In this case it will also pop a critical error message box
 bool migrate_stats(Logger& logger);
 
 // Use Qt to setup permissions of cameras and microphones on macOS.
 // See https://www.qt.io/blog/permission-apis-in-qt-6.5
 void set_permissions(QObject& object);
 
+// Return true if there is the required Resources/ folder.
+// When the folder is missing, it will also pop a critical error message box
+bool check_resource_folder(Logger& logger);
 
 }
 #endif
