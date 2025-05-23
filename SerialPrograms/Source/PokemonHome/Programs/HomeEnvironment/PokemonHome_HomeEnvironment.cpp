9c712bd58ebb697e090c2ea7871073f4ff3176f7
Kellen Long <kellenllong@gmail.com>
2025-05-23 14:58:35 -0500

    Added framework to HomeEnvironment for navigation

--- SerialPrograms/Source/PokemonHome/Programs/HomeEnvironment/PokemonHome_HomeEnvironment.cpp
+++ SerialPrograms/Source/PokemonHome/Programs/HomeEnvironment/PokemonHome_HomeEnvironment.cpp
@@ -4,23 +4,30 @@ namespace PokemonAutomation{
 namespace NintendoSwitch{
 namespace PokemonHome{
 
-Cursor::Cursor(SingleSwitchProgramEnvironment& env, ProControllerContext& context){
+// Alias for the callable type
+using NavigationFunction = std::function<void(SingleSwitchProgramEnvironment&, ProControllerContext&, const std::string&)>;
+
+// Alias for the unordered_map type
+using NavigationMap = std::unordered_map<PageID, std::vector<std::pair<PageID, NavigationFunction>>>;
+
+
+HomeCursor::HomeCursor(SingleSwitchProgramEnvironment& env, ProControllerContext&){
 
 }
 
-void Cursor::locate_position(){
+void HomeCursor::locate_position(){
     // TODO: Implement locate_position logic
 }
 
-void Cursor::move_cursor_to(SingleSwitchProgramEnvironment& env, ProControllerContext& context, const std::pair<size_t, size_t>& cursor){
+void HomeCursor::move_cursor_to(SingleSwitchProgramEnvironment& env, ProControllerContext& context, const std::pair<size_t, size_t>& cursor){
     // TODO: Implement cursor movement logic
 }
 
-void Cursor::pick_up_pokemon(SingleSwitchProgramEnvironment& env, ProControllerContext& context){
+void HomeCursor::pick_up_pokemon(SingleSwitchProgramEnvironment& env, ProControllerContext& context){
     // TODO: Implement pick up logic
 }
 
-void Cursor::put_down_pokemon(SingleSwitchProgramEnvironment& env, ProControllerContext& context){
+void HomeCursor::put_down_pokemon(SingleSwitchProgramEnvironment& env, ProControllerContext& context){
     // TODO: Implement put down logic
 }
 
@@ -34,6 +41,42 @@ PokemonHome_HomeEnvironment::PokemonHome_HomeEnvironment(SingleSwitchProgramEnvi
 
 void PokemonHome_HomeEnvironment::DetectHome(SingleSwitchProgramEnvironment& env, ProControllerContext& context){
 
+}
+
+void PokemonHome_HomeEnvironment::initialize_navigation_map(SingleSwitchProgramEnvironment& env, ProControllerContext& context) {
+    // Title screen to game selection
+    navigation_map[PageID::TITLE_SCREEN] = {
+        {PageID::MAIN_MENU, [](SingleSwitchProgramEnvironment& env, ProControllerContext& context, const std::string&) {
+            // Press A
+        }},
+    };
+
+    // Game selection to Main View Base
+    navigation_map[PageID::GAME_SELECTION] = {
+        {PageID::MAIN_VIEW_BASE, [](SingleSwitchProgramEnvironment& env, ProControllerContext& context, const std::string& game) {
+             std::cout << "Selecting game: " << game << " and entering Main View Base.\n";
+         }},
+    };
+
+    // Main View Base to other pages
+    navigation_map[PageID::MAIN_VIEW_BASE] = {
+        {PageID::LIST_VIEW, [](SingleSwitchProgramEnvironment& env, ProControllerContext& context, const std::string&) {
+             std::cout << "Navigating to List View from Main View Base.\n";
+         }},
+        {PageID::MAIN_VIEW_OPTION_MENU, [](SingleSwitchProgramEnvironment& env, ProControllerContext& context, const std::string&) {
+             std::cout << "Opening Option Menu in Main View.\n";
+         }},
+    };
+
+    // Main View Option Menu to Summary or Markings
+    navigation_map[PageID::MAIN_VIEW_OPTION_MENU] = {
+        {PageID::SUMMARY_VIEW, [](SingleSwitchProgramEnvironment& env, ProControllerContext& context, const std::string&) {
+             std::cout << "Navigating to Summary View from Option Menu.\n";
+         }},
+        {PageID::MARKINGS_VIEW, [](SingleSwitchProgramEnvironment& env, ProControllerContext& context, const std::string&) {
+             std::cout << "Navigating to Markings View from Option Menu.\n";
+         }},
+    };
 }
 
 }
--- SerialPrograms/Source/PokemonHome/Programs/HomeEnvironment/PokemonHome_HomeEnvironment.h
+++ SerialPrograms/Source/PokemonHome/Programs/HomeEnvironment/PokemonHome_HomeEnvironment.h
@@ -2,14 +2,16 @@
 #define PokemonAutomation_PokemonHome_HomeEnvironment_H
 
 #include "NintendoSwitch/NintendoSwitch_SingleSwitchProgram.h"
+#include "PokemonHome/Options/PokemonHome_BoxSortingTable.h"
+#include <functional>
+#include <unordered_map>
 
-
-namespace PokemonAutomation{
-namespace NintendoSwitch{
-namespace PokemonHome{
+namespace PokemonAutomation {
+namespace NintendoSwitch {
+namespace PokemonHome {
 
 enum class PageID {
-    TILE_SCREEN,
+    TITLE_SCREEN,
     MAIN_MENU,
     GAME_SELECTION,
     BOX_VIEW,
@@ -17,8 +19,27 @@ enum class PageID {
     MARKINGS_VIEW,
     LIST_VIEW,
     UNKNOWN
-    // etc
 };
+
+}
+}
+}
+
+namespace std {
+template <>
+struct hash<PokemonAutomation::NintendoSwitch::PokemonHome::PageID> {
+    std::size_t operator()(const PokemonAutomation::NintendoSwitch::PokemonHome::PageID& id) const noexcept {
+        return std::hash<int>()(static_cast<int>(id));
+    }
+};
+}
+
+namespace PokemonAutomation {
+namespace NintendoSwitch {
+namespace PokemonHome {
+
+// Continue with other enums, classes, and declarations
+using NavigationFunction = ::std::function<void(SingleSwitchProgramEnvironment&, ProControllerContext&, const std::string&)>;
 
 enum class GameStatus {
     NONE,
@@ -32,21 +53,27 @@ enum class GameStatus {
     POKEMON_SHIELD,
     POKEMON_SCARLET,
     POKEMON_VIOLET,
+    CURRENT,
     UNKNOWN
 };
 
+enum class SecondaryBoxStatus {
+    FALSE,
+    TRUE,
+    UNKNOWN
+};
 
-class Cursor{
+class HomeCursor {
     const size_t MAX_ROWS = 5;
     const size_t MAX_COLUMNS = 6;
 
 public:
-    Cursor(SingleSwitchProgramEnvironment& env, ProControllerContext& context);
+    HomeCursor(SingleSwitchProgramEnvironment&, ProControllerContext&);
 
     void locate_position();
-    void move_cursor_to(SingleSwitchProgramEnvironment& env, ProControllerContext& context, const std::pair<size_t, size_t>& cursor);
-    void pick_up_pokemon(SingleSwitchProgramEnvironment& env, ProControllerContext& context);
-    void put_down_pokemon(SingleSwitchProgramEnvironment& env, ProControllerContext& context);
+    void move_cursor_to(SingleSwitchProgramEnvironment&, ProControllerContext&, const std::pair<size_t, size_t>);
+    void pick_up_pokemon(SingleSwitchProgramEnvironment&, ProControllerContext&);
+    void put_down_pokemon(SingleSwitchProgramEnvironment&, ProControllerContext&);
 
 private:
     size_t row;
@@ -55,22 +82,27 @@ private:
     bool InSecondaryBoxes;
 };
 
-
-class PokemonHome_HomeEnvironment : public SingleSwitchProgramInstance{
+class PokemonHome_HomeEnvironment : public SingleSwitchProgramInstance {
 
 public:
     PokemonHome_HomeEnvironment(SingleSwitchProgramEnvironment& env, ProControllerContext& context);
 
-    void DetectHome(SingleSwitchProgramEnvironment& env, ProControllerContext& context);
+    void navigate_to(SingleSwitchProgramEnvironment&, ProControllerContext&, const GameStatus, const std::pair<size_t, size_t>, const size_t);
+    void DetectHome(SingleSwitchProgramEnvironment&, ProControllerContext&);
 
 private:
-    Cursor cursor;
+    void initialize_navigation_map(SingleSwitchProgramEnvironment&, ProControllerContext&);
+    PageID find_navigation_path(SingleSwitchProgramEnvironment&, ProControllerContext&, PageID, PageID, const GameStatus);
+    void perform_navigation_step(SingleSwitchProgramEnvironment&, ProControllerContext&, PageID, PageID, const GameStatus);
+
+    HomeCursor cursor;
     GameStatus GameOpen;
     PageID current_view;
 
+    std::unordered_map<PageID, std::vector<std::pair<PageID, NavigationFunction>>> navigation_map;
 };
 
 }
 }
 }
-#endif // POKEMONHOME_HOMEENVIRONMENT_H
+#endif
