9586fdaa43bc95b074b863657cb48f0ea3078817
Alexander Yee <a-yee@u.northwestern.edu>
2025-05-27 20:28:50 -0700

    Show change history in update notification.

--- SerialPrograms/Source/CommonFramework/Startup/NewVersionCheck.cpp
+++ SerialPrograms/Source/CommonFramework/Startup/NewVersionCheck.cpp
@@ -66,6 +66,14 @@ bool compare_version(Logger& logger, const JsonObject* json){
 
     return false;
 }
+std::string get_changes(const JsonObject* json){
+    const std::string* changes = json->get_string("Changes");
+    if (changes == nullptr){
+        return "";
+    }
+    return "<br><br>" + *changes;
+}
+
 void check_new_version(Logger& logger){
     bool check_release = GlobalSettings::instance().CHECK_FOR_UPDATES->RELEASE;
     bool check_beta = GlobalSettings::instance().CHECK_FOR_UPDATES->BETA;
@@ -102,19 +110,23 @@ void check_new_version(Logger& logger){
         return;
     }
 
+//    cout << json.dump() << endl;
 
     if (check_private_beta){
         const JsonObject* node = obj->get_object("PrivateBeta");
         if (compare_version(logger, node)){
             QMessageBox box;
             box.setTextFormat(Qt::RichText);
+            std::string text = "A new private beta is available!<br>";
+            text += make_text_url(
+                "https://discord.com/channels/695809740428673034/732736538965704726",
+                "Download from our Discord!"
+            );
+            text += get_changes(node);
             box.information(
                 nullptr,
                 "A new private beta is available!",
-                QString::fromStdString(
-                    "A new private beta is available!<br>" +
-                    make_text_url("https://discord.com/channels/695809740428673034/732736538965704726", "Download from our Discord!")
-                )
+                QString::fromStdString(text)
             );
             return;
         }
@@ -124,13 +136,16 @@ void check_new_version(Logger& logger){
         if (compare_version(logger, node)){
             QMessageBox box;
             box.setTextFormat(Qt::RichText);
+            std::string text = "A new beta is available!<br>";
+            text += make_text_url(
+                "https://github.com/PokemonAutomation/ComputerControl/releases",
+                "Download here."
+            );
+            text += get_changes(node);
             box.information(
                 nullptr,
                 "A new beta is available!",
-                QString::fromStdString(
-                    "A new beta is available!<br>" +
-                make_text_url("https://github.com/PokemonAutomation/ComputerControl/releases", "Download here.")
-                )
+                QString::fromStdString(text)
             );
             return;
         }
@@ -140,13 +155,16 @@ void check_new_version(Logger& logger){
         if (compare_version(logger, node)){
             QMessageBox box;
             box.setTextFormat(Qt::RichText);
+            std::string text = "A new version is available!<br>";
+            text += make_text_url(
+                "https://github.com/PokemonAutomation/ComputerControl/releases",
+                "Download here."
+            );
+            text += get_changes(node);
             box.information(
                 nullptr,
                 "New Version Available!",
-                QString::fromStdString(
-                    "A new version is available!<br>" +
-                    make_text_url("https://github.com/PokemonAutomation/ComputerControl/releases", "Download here.")
-                )
+                QString::fromStdString(text)
             );
             return;
         }
