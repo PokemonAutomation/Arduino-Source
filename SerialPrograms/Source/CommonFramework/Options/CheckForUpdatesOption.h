1819d0422800ac4758dac56cdca60b346c02f882
Alexander Yee <a-yee@u.northwestern.edu>
2025-05-27 21:28:04 -0700

    Improved update nag.

--- SerialPrograms/Source/CommonFramework/Options/CheckForUpdatesOption.h
+++ SerialPrograms/Source/CommonFramework/Options/CheckForUpdatesOption.h
@@ -9,6 +9,7 @@
 
 #include "Common/Cpp/Options/GroupOption.h"
 #include "Common/Cpp/Options/BooleanCheckBoxOption.h"
+#include "Common/Cpp/Options/StringOption.h"
 #include "CommonFramework/Globals.h"
 
 namespace PokemonAutomation{
@@ -21,35 +22,44 @@ public:
             "Check for Updates",
             LockMode::UNLOCK_WHILE_RUNNING,
             EnableMode::ALWAYS_ENABLED,
-            false
+            true
         )
-        , RELEASE(
+        , RELEASE0(
             "<b>New Releases:</b><br>Automatically check for new stable releases.",
             LockMode::UNLOCK_WHILE_RUNNING,
             true
         )
-        , BETA(
+        , PUBLIC_BETA0(
             "<b>Public Betas:</b><br>Automatically check for new public betas.",
             LockMode::UNLOCK_WHILE_RUNNING,
             IS_BETA_VERSION
         )
-        , PRIVATE_BETA(
+        , PRIVATE_BETA0(
             "<b>Private Betas:</b><br>Automatically check for new private betas.",
             LockMode::UNLOCK_WHILE_RUNNING,
             IS_BETA_VERSION
         )
+        ,SKIP_VERSION(
+            false,
+            "<b>Skip this Version:</b><br>Skip this version and don't notify until the next update.",
+            LockMode::UNLOCK_WHILE_RUNNING,
+            "",
+            "0.50.0"
+        )
     {
-        PA_ADD_OPTION(RELEASE);
-        PA_ADD_OPTION(BETA);
+        PA_ADD_OPTION(RELEASE0);
+        PA_ADD_OPTION(PUBLIC_BETA0);
         if (IS_BETA_VERSION){
-            PA_ADD_OPTION(PRIVATE_BETA);
+            PA_ADD_OPTION(PRIVATE_BETA0);
         }
+        PA_ADD_OPTION(SKIP_VERSION);
     }
 
 public:
-    BooleanCheckBoxOption RELEASE;
-    BooleanCheckBoxOption BETA;
-    BooleanCheckBoxOption PRIVATE_BETA;
+    BooleanCheckBoxOption RELEASE0;
+    BooleanCheckBoxOption PUBLIC_BETA0;
+    BooleanCheckBoxOption PRIVATE_BETA0;
+    StringOption SKIP_VERSION;
 };
 
 
--- SerialPrograms/Source/CommonFramework/Startup/NewVersionCheck.cpp
+++ SerialPrograms/Source/CommonFramework/Startup/NewVersionCheck.cpp
@@ -4,6 +4,7 @@
  *
  */
 
+#include <QPushButton>
 #include <QMessageBox>
 #include "Common/Cpp/Time.h"
 #include "Common/Cpp/Json/JsonObject.h"
@@ -25,6 +26,58 @@ namespace PokemonAutomation{
 static auto CHECK_FOR_UPDATES_PERIOD = std::chrono::days(1);
 
 
+struct ProgramVersion{
+    int major;
+    int minor;
+    int patch;
+    std::string version_text;
+
+    ProgramVersion(Logger& logger, const JsonObject* json){
+        if (!json->read_integer(major, "VersionMajor")){
+            throw_and_log<ParseException>(logger, "Invalid version JSON: Missing major version.");
+        }
+        if (!json->read_integer(minor, "VersionMinor")){
+            throw_and_log<ParseException>(logger, "Invalid version JSON: Missing minor version.");
+        }
+        if (!json->read_integer(patch, "VersionPatch")){
+            throw_and_log<ParseException>(logger, "Invalid version JSON: Missing patch #.");
+        }
+        version_text = std::to_string(major)
+            + "." + std::to_string(minor)
+            + "." + std::to_string(patch);
+    }
+
+    bool is_newer() const{
+        if (major > PROGRAM_VERSION_MAJOR){
+            return true;
+        }
+        if (major < PROGRAM_VERSION_MAJOR){
+            return false;
+        }
+
+        if (minor > PROGRAM_VERSION_MINOR){
+            return true;
+        }
+        if (minor < PROGRAM_VERSION_MINOR){
+            return false;
+        }
+
+        if (patch > PROGRAM_VERSION_PATCH){
+            return true;
+        }
+
+        return false;
+    }
+
+    bool show_update_nag() const{
+        if (!is_newer()){
+            return false;
+        }
+        std::string skip_version = GlobalSettings::instance().CHECK_FOR_UPDATES->SKIP_VERSION;
+        return version_text != skip_version;
+    }
+};
+
 bool compare_version(Logger& logger, const JsonObject* json){
     if (json == nullptr){
         logger.log("Invalid version JSON.", COLOR_RED);
@@ -32,10 +85,28 @@ bool compare_version(Logger& logger, const JsonObject* json){
     }
 
     int major;
+    int minor;
+    int patch;
     if (!json->read_integer(major, "VersionMajor")){
         logger.log("Invalid version JSON: Missing major version.", COLOR_RED);
         return false;
     }
+    if (!json->read_integer(minor, "VersionMinor")){
+        logger.log("Invalid version JSON: Missing minor version.", COLOR_RED);
+        return false;
+    }
+    if (!json->read_integer(patch, "VersionPatch")){
+        logger.log("Invalid version JSON: Missing patch #.", COLOR_RED);
+        return false;
+    }
+    std::string full_version = std::to_string(major)
+        + "." + std::to_string(minor)
+        + "." + std::to_string(patch);
+    std::string skip_version = GlobalSettings::instance().CHECK_FOR_UPDATES->SKIP_VERSION;
+    if (full_version == skip_version){
+        return false;
+    }
+
     if (major > PROGRAM_VERSION_MAJOR){
         return true;
     }
@@ -43,11 +114,6 @@ bool compare_version(Logger& logger, const JsonObject* json){
         return false;
     }
 
-    int minor;
-    if (!json->read_integer(minor, "VersionMinor")){
-        logger.log("Invalid version JSON: Missing minor version.", COLOR_RED);
-        return false;
-    }
     if (minor > PROGRAM_VERSION_MINOR){
         return true;
     }
@@ -55,29 +121,66 @@ bool compare_version(Logger& logger, const JsonObject* json){
         return false;
     }
 
-    int patch;
-    if (!json->read_integer(patch, "VersionPatch")){
-        logger.log("Invalid version JSON: Missing patch #.", COLOR_RED);
-        return false;
-    }
     if (patch > PROGRAM_VERSION_PATCH){
         return true;
     }
 
     return false;
 }
-std::string get_changes(const JsonObject* json){
-    const std::string* changes = json->get_string("Changes");
+std::string get_changes(const JsonObject& json){
+    const std::string* changes = json.get_string("Changes");
     if (changes == nullptr){
         return "";
     }
     return "<br><br>" + *changes;
 }
+void show_update_box(
+    const std::string& title,
+    const std::string& link_text,
+    const std::string& link_url,
+    const std::string& header,
+    const ProgramVersion& version,
+    const JsonObject& node
+){
+    QMessageBox box;
+    QPushButton* ok = box.addButton(QMessageBox::Ok);
+    QPushButton* skip = box.addButton("Skip this version.", QMessageBox::NoRole);
+
+    box.setTextFormat(Qt::RichText);
+    std::string text = header + "<br>";
+    text += make_text_url(link_url, link_text);
+    text += get_changes(node);
+
+
+    box.setWindowTitle(QString::fromStdString(title));
+    box.setText(QString::fromStdString(text));
+
+//    box.open();
+
+    box.exec();
+
+    QAbstractButton* clicked = box.clickedButton();
+    if (clicked == ok){
+        return;
+    }
+    if (clicked == skip){
+        GlobalSettings::instance().CHECK_FOR_UPDATES->SKIP_VERSION.set(version.version_text);
+        return;
+    }
+
+
+//    box.information(
+//        nullptr,
+//        QString::fromStdString(title),
+//        QString::fromStdString(text)
+//    );
+}
+
 
 void check_new_version(Logger& logger){
-    bool check_release = GlobalSettings::instance().CHECK_FOR_UPDATES->RELEASE;
-    bool check_beta = GlobalSettings::instance().CHECK_FOR_UPDATES->BETA;
-    bool check_private_beta = GlobalSettings::instance().CHECK_FOR_UPDATES->PRIVATE_BETA;
+    bool check_release = GlobalSettings::instance().CHECK_FOR_UPDATES->RELEASE0;
+    bool check_beta = GlobalSettings::instance().CHECK_FOR_UPDATES->PUBLIC_BETA0;
+    bool check_private_beta = GlobalSettings::instance().CHECK_FOR_UPDATES->PRIVATE_BETA0;
     bool updates_enabled = false;
     updates_enabled |= check_release;
     updates_enabled |= check_beta;
@@ -110,61 +213,47 @@ void check_new_version(Logger& logger){
         return;
     }
 
+
 //    cout << json.dump() << endl;
 
     if (check_private_beta){
         const JsonObject* node = obj->get_object("PrivateBeta");
-        if (compare_version(logger, node)){
-            QMessageBox box;
-            box.setTextFormat(Qt::RichText);
-            std::string text = "A new private beta is available!<br>";
-            text += make_text_url(
+        ProgramVersion latest(logger, node);
+        if (latest.show_update_nag()){
+            show_update_box(
+                "New Private Beta Available!",
+                "Download from our Discord!",
                 "https://discord.com/channels/695809740428673034/732736538965704726",
-                "Download from our Discord!"
-            );
-            text += get_changes(node);
-            box.information(
-                nullptr,
                 "A new private beta is available!",
-                QString::fromStdString(text)
+                latest, *node
             );
             return;
         }
     }
     if (check_beta){
         const JsonObject* node = obj->get_object("Beta");
-        if (compare_version(logger, node)){
-            QMessageBox box;
-            box.setTextFormat(Qt::RichText);
-            std::string text = "A new beta is available!<br>";
-            text += make_text_url(
+        ProgramVersion latest(logger, node);
+        if (latest.show_update_nag()){
+            show_update_box(
+                "New Beta Available!",
+                "Download here.",
                 "https://github.com/PokemonAutomation/ComputerControl/releases",
-                "Download here."
-            );
-            text += get_changes(node);
-            box.information(
-                nullptr,
                 "A new beta is available!",
-                QString::fromStdString(text)
+                latest, *node
             );
             return;
         }
     }
     if (check_release){
         const JsonObject* node = obj->get_object("Release");
-        if (compare_version(logger, node)){
-            QMessageBox box;
-            box.setTextFormat(Qt::RichText);
-            std::string text = "A new version is available!<br>";
-            text += make_text_url(
-                "https://github.com/PokemonAutomation/ComputerControl/releases",
-                "Download here."
-            );
-            text += get_changes(node);
-            box.information(
-                nullptr,
+        ProgramVersion latest(logger, node);
+        if (latest.show_update_nag()){
+            show_update_box(
                 "New Version Available!",
-                QString::fromStdString(text)
+                "Download here.",
+                "https://github.com/PokemonAutomation/ComputerControl/releases",
+                "A new version is available!",
+                latest, *node
             );
             return;
         }
