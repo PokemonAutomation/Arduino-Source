873dbd015937b7336ab5726132d0f53a5c08ca30
Monstertov <rob@monstertov.nl>
2025-06-01 00:06:59 +0200

    Improved clarity by fixing typo: 'Unkwown' corrected to 'Unknown'

--- SerialPrograms/Source/Pokemon/Pokemon_StatsCalculation.cpp
+++ SerialPrograms/Source/Pokemon/Pokemon_StatsCalculation.cpp
@@ -118,7 +118,7 @@ NatureAdjustments get_nature_adjustments(NatureCheckerValue nature){
         return ret;
 
     default:
-        throw InternalProgramError(nullptr, PA_CURRENT_FUNCTION, "Unkwown Nature: " + std::to_string((int)nature));
+        throw InternalProgramError(nullptr, PA_CURRENT_FUNCTION, "Unknown Nature: " + std::to_string((int)nature));
     }
 }
 #endif
