7e8a958fd7ac570f691a7a4679429286febaf0fa
Monstertov <rob@monstertov.nl>
2025-05-31 23:57:25 +0200

    Addressed typo: changed 'orignal' to 'original'

--- 3rdParty/QtWavFile/WavFile.cpp
+++ 3rdParty/QtWavFile/WavFile.cpp
@@ -1,199 +1,199 @@
-/*  WAV FILE
- *
- *  From: https://github.com/PokemonAutomation/
- *
- *  Class to read a .wav file. Edited from Qt multimedia example Spectrum:
- *  https://code.qt.io/cgit/qt/qtmultimedia.git/tree/examples/multimedia/spectrum/app/wavfile.cpp?h=5.15
- *  The git repo for Qt multimedia code and exmamples is https://code.qt.io/qt/qtmultimedia.git 
- *  The version of the code is Qt5.
- *  Since the code is licensed by Qt under BSD, we can safely use it with our code base, provided
- *  that we have the orignal BSD license below and release the source code.
- */
-
-/****************************************************************************
-**
-** Copyright (C) 2017 The Qt Company Ltd.
-** Contact: https://www.qt.io/licensing/
-**
-** This file is part of the examples of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:BSD$
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see https://www.qt.io/terms-conditions. For further
-** information use the contact form at https://www.qt.io/contact-us.
-**
-** BSD License Usage
-** Alternatively, you may use this file under the terms of the BSD license
-** as follows:
-**
-** "Redistribution and use in source and binary forms, with or without
-** modification, are permitted provided that the following conditions are
-** met:
-**   * Redistributions of source code must retain the above copyright
-**     notice, this list of conditions and the following disclaimer.
-**   * Redistributions in binary form must reproduce the above copyright
-**     notice, this list of conditions and the following disclaimer in
-**     the documentation and/or other materials provided with the
-**     distribution.
-**   * Neither the name of The Qt Company Ltd nor the names of its
-**     contributors may be used to endorse or promote products derived
-**     from this software without specific prior written permission.
-**
-**
-** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
-
-#include <limits>
-#include <qendian.h>
-#include <QVector>
-#include <QDebug>
-#include "WavFile.h"
-
-#include <iostream>
-
-struct chunk
-{
-    char        id[4];
-    quint32     size;
-};
-
-struct RIFFHeader
-{
-    chunk       descriptor;     // "RIFF"
-    char        type[4];        // "WAVE"
-};
-
-struct WAVEHeader
-{
-    chunk       descriptor;
-    quint16     audioFormat;
-    quint16     numChannels;
-    quint32     sample_rate;
-    quint32     byteRate;
-    quint16     blockAlign;
-    quint16     bitsPerSample;
-};
-
-struct DATAHeader
-{
-    chunk       descriptor;
-};
-
-struct CombinedHeader
-{
-    RIFFHeader  riff;
-    WAVEHeader  wave;
-};
-
-WavFile::WavFile(QObject *parent)
-    : QFile(parent)
-    , m_headerLength(0)
-{
-
-}
-
-bool WavFile::open(const QString &fileName)
-{
-    close();
-    setFileName(fileName);
-    return QFile::open(QIODevice::ReadOnly) && readHeader();
-}
-
-const QAudioFormat &WavFile::audioFormat() const
-{
-    return m_fileFormat;
-}
-
-qint64 WavFile::headerLength() const
-{
-return m_headerLength;
-}
-
-bool WavFile::readHeader()
-{
-    seek(0);
-    CombinedHeader header;
-    bool result = read(reinterpret_cast<char *>(&header), sizeof(CombinedHeader)) == sizeof(CombinedHeader);
-    if (result) {
-        if ((memcmp(&header.riff.descriptor.id, "RIFF", 4) == 0
-            || memcmp(&header.riff.descriptor.id, "RIFX", 4) == 0)
-            && memcmp(&header.riff.type, "WAVE", 4) == 0
-            && memcmp(&header.wave.descriptor.id, "fmt ", 4) == 0
-            && (header.wave.audioFormat == 1 || header.wave.audioFormat == 0)) {
-
-            // Read off remaining header information
-            DATAHeader dataHeader;
-
-            if (qFromLittleEndian<quint32>(header.wave.descriptor.size) > sizeof(WAVEHeader)) {
-                // Extended data available
-                quint16 extraFormatBytes;
-                if (peek((char*)&extraFormatBytes, sizeof(quint16)) != sizeof(quint16))
-                    return false;
-                const qint64 throwAwayBytes = sizeof(quint16) + qFromLittleEndian<quint16>(extraFormatBytes);
-                if (read(throwAwayBytes).size() != throwAwayBytes)
-                    return false;
-            }
-
-            if (read((char*)&dataHeader, sizeof(DATAHeader)) != sizeof(DATAHeader))
-                return false;
-
-            // Establish format
-
-#if QT_VERSION_MAJOR == 5        
-            if (memcmp(&header.riff.descriptor.id, "RIFF", 4) == 0)
-                m_fileFormat.setByteOrder(QAudioFormat::LittleEndian);
-            else
-                m_fileFormat.setByteOrder(QAudioFormat::BigEndian);
-#elif QT_VERSION_MAJOR == 6
-            // Qt6 audio format does not handle little or big endian.
-            // So it always assumes little endian.
-            if (memcmp(&header.riff.descriptor.id, "RIFF", 4) != 0){
-                std::cout << "Error: wav file uses big endian format but Qt6 audio format does not support it." << std::endl;
-                result = false;
-            }
-#endif
-            int bps = qFromLittleEndian<quint16>(header.wave.bitsPerSample);
-            m_fileFormat.setChannelCount(qFromLittleEndian<quint16>(header.wave.numChannels));
-            m_fileFormat.setSampleRate(qFromLittleEndian<quint32>(header.wave.sample_rate));
-#if QT_VERSION_MAJOR == 5            
-            m_fileFormat.setCodec("audio/pcm");
-            m_fileFormat.setSampleSize(qFromLittleEndian<quint16>(header.wave.bitsPerSample));
-            m_fileFormat.setSampleType(bps == 8 ? QAudioFormat::UnSignedInt : QAudioFormat::SignedInt);
-#elif QT_VERSION_MAJOR == 6
-            const int bitsPerSample = qFromLittleEndian<quint16>(header.wave.bitsPerSample);
-            if (bitsPerSample == 8 && bps == 8){
-                m_fileFormat.setSampleFormat(QAudioFormat::SampleFormat::UInt8);
-            } else if (bitsPerSample == 16 && bps != 8){
-                m_fileFormat.setSampleFormat(QAudioFormat::SampleFormat::Int16);
-            } else if (bitsPerSample == 32 && bps != 8){
-                m_fileFormat.setSampleFormat(QAudioFormat::SampleFormat::Int32);
-            } else {
-                std::cout << "Error: wav file uses sample type that Qt6 audio format does not support: bits per sample: " << 
-                    bitsPerSample << ", " << (bps == 8 ? "unsigned" : "signed") << std::endl;
-                result = false;
-            }
-#endif
-        } else {
-            result = false;
-        }
-    }
-    m_headerLength = pos();
-    return result;
-}
+/*  WAV FILE
+ *
+ *  From: https://github.com/PokemonAutomation/
+ *
+ *  Class to read a .wav file. Edited from Qt multimedia example Spectrum:
+ *  https://code.qt.io/cgit/qt/qtmultimedia.git/tree/examples/multimedia/spectrum/app/wavfile.cpp?h=5.15
+ *  The git repo for Qt multimedia code and exmamples is https://code.qt.io/qt/qtmultimedia.git 
+ *  The version of the code is Qt5.
+ *  Since the code is licensed by Qt under BSD, we can safely use it with our code base, provided
+ *  that we have the original BSD license below and release the source code.
+ */
+
+/****************************************************************************
+**
+** Copyright (C) 2017 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the examples of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:BSD$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** BSD License Usage
+** Alternatively, you may use this file under the terms of the BSD license
+** as follows:
+**
+** "Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions are
+** met:
+**   * Redistributions of source code must retain the above copyright
+**     notice, this list of conditions and the following disclaimer.
+**   * Redistributions in binary form must reproduce the above copyright
+**     notice, this list of conditions and the following disclaimer in
+**     the documentation and/or other materials provided with the
+**     distribution.
+**   * Neither the name of The Qt Company Ltd nor the names of its
+**     contributors may be used to endorse or promote products derived
+**     from this software without specific prior written permission.
+**
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include <limits>
+#include <qendian.h>
+#include <QVector>
+#include <QDebug>
+#include "WavFile.h"
+
+#include <iostream>
+
+struct chunk
+{
+    char        id[4];
+    quint32     size;
+};
+
+struct RIFFHeader
+{
+    chunk       descriptor;     // "RIFF"
+    char        type[4];        // "WAVE"
+};
+
+struct WAVEHeader
+{
+    chunk       descriptor;
+    quint16     audioFormat;
+    quint16     numChannels;
+    quint32     sample_rate;
+    quint32     byteRate;
+    quint16     blockAlign;
+    quint16     bitsPerSample;
+};
+
+struct DATAHeader
+{
+    chunk       descriptor;
+};
+
+struct CombinedHeader
+{
+    RIFFHeader  riff;
+    WAVEHeader  wave;
+};
+
+WavFile::WavFile(QObject *parent)
+    : QFile(parent)
+    , m_headerLength(0)
+{
+
+}
+
+bool WavFile::open(const QString &fileName)
+{
+    close();
+    setFileName(fileName);
+    return QFile::open(QIODevice::ReadOnly) && readHeader();
+}
+
+const QAudioFormat &WavFile::audioFormat() const
+{
+    return m_fileFormat;
+}
+
+qint64 WavFile::headerLength() const
+{
+return m_headerLength;
+}
+
+bool WavFile::readHeader()
+{
+    seek(0);
+    CombinedHeader header;
+    bool result = read(reinterpret_cast<char *>(&header), sizeof(CombinedHeader)) == sizeof(CombinedHeader);
+    if (result) {
+        if ((memcmp(&header.riff.descriptor.id, "RIFF", 4) == 0
+            || memcmp(&header.riff.descriptor.id, "RIFX", 4) == 0)
+            && memcmp(&header.riff.type, "WAVE", 4) == 0
+            && memcmp(&header.wave.descriptor.id, "fmt ", 4) == 0
+            && (header.wave.audioFormat == 1 || header.wave.audioFormat == 0)) {
+
+            // Read off remaining header information
+            DATAHeader dataHeader;
+
+            if (qFromLittleEndian<quint32>(header.wave.descriptor.size) > sizeof(WAVEHeader)) {
+                // Extended data available
+                quint16 extraFormatBytes;
+                if (peek((char*)&extraFormatBytes, sizeof(quint16)) != sizeof(quint16))
+                    return false;
+                const qint64 throwAwayBytes = sizeof(quint16) + qFromLittleEndian<quint16>(extraFormatBytes);
+                if (read(throwAwayBytes).size() != throwAwayBytes)
+                    return false;
+            }
+
+            if (read((char*)&dataHeader, sizeof(DATAHeader)) != sizeof(DATAHeader))
+                return false;
+
+            // Establish format
+
+#if QT_VERSION_MAJOR == 5        
+            if (memcmp(&header.riff.descriptor.id, "RIFF", 4) == 0)
+                m_fileFormat.setByteOrder(QAudioFormat::LittleEndian);
+            else
+                m_fileFormat.setByteOrder(QAudioFormat::BigEndian);
+#elif QT_VERSION_MAJOR == 6
+            // Qt6 audio format does not handle little or big endian.
+            // So it always assumes little endian.
+            if (memcmp(&header.riff.descriptor.id, "RIFF", 4) != 0){
+                std::cout << "Error: wav file uses big endian format but Qt6 audio format does not support it." << std::endl;
+                result = false;
+            }
+#endif
+            int bps = qFromLittleEndian<quint16>(header.wave.bitsPerSample);
+            m_fileFormat.setChannelCount(qFromLittleEndian<quint16>(header.wave.numChannels));
+            m_fileFormat.setSampleRate(qFromLittleEndian<quint32>(header.wave.sample_rate));
+#if QT_VERSION_MAJOR == 5            
+            m_fileFormat.setCodec("audio/pcm");
+            m_fileFormat.setSampleSize(qFromLittleEndian<quint16>(header.wave.bitsPerSample));
+            m_fileFormat.setSampleType(bps == 8 ? QAudioFormat::UnSignedInt : QAudioFormat::SignedInt);
+#elif QT_VERSION_MAJOR == 6
+            const int bitsPerSample = qFromLittleEndian<quint16>(header.wave.bitsPerSample);
+            if (bitsPerSample == 8 && bps == 8){
+                m_fileFormat.setSampleFormat(QAudioFormat::SampleFormat::UInt8);
+            } else if (bitsPerSample == 16 && bps != 8){
+                m_fileFormat.setSampleFormat(QAudioFormat::SampleFormat::Int16);
+            } else if (bitsPerSample == 32 && bps != 8){
+                m_fileFormat.setSampleFormat(QAudioFormat::SampleFormat::Int32);
+            } else {
+                std::cout << "Error: wav file uses sample type that Qt6 audio format does not support: bits per sample: " << 
+                    bitsPerSample << ", " << (bps == 8 ? "unsigned" : "signed") << std::endl;
+                result = false;
+            }
+#endif
+        } else {
+            result = false;
+        }
+    }
+    m_headerLength = pos();
+    return result;
+}
