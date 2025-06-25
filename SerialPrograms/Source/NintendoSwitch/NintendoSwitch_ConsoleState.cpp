e6f93183e207f403791341be8d7d3bba4183e410
Alexander Yee <a-yee@u.northwestern.edu>
2025-06-25 02:32:32 -0700

    Fix log spam.

--- SerialPrograms/Source/NintendoSwitch/NintendoSwitch_ConsoleState.cpp
+++ SerialPrograms/Source/NintendoSwitch/NintendoSwitch_ConsoleState.cpp
@@ -135,10 +135,12 @@ void ConsoleState::set_console_type(Logger& logger, ConsoleType type){
 
     }while (false);
 
-    logger.log(
-        std::string("Setting console type to: ") + ConsoleType_strings(type),
-        COLOR_BLUE
-    );
+    if (!confirmed || current != type){
+        logger.log(
+            std::string("Setting console type to: ") + ConsoleType_strings(type),
+            COLOR_BLUE
+        );
+    }
 
     m_data->m_console_type.store(type, std::memory_order_relaxed);
     m_data->m_console_type_confirmed.store(true, std::memory_order_relaxed);
