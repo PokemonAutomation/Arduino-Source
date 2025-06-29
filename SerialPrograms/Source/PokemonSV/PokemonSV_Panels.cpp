fcc605e254474a3673c22ce03a82ed564d8edba0
Alexander Yee <a-yee@u.northwestern.edu>
2025-06-29 02:22:38 -0700

    Deprecate the non-RNG Item Printer.

--- SerialPrograms/Source/PokemonSV/PokemonSV_Panels.cpp
+++ SerialPrograms/Source/PokemonSV/PokemonSV_Panels.cpp
@@ -31,7 +31,6 @@
 #include "Programs/Farming/PokemonSV_FlyingTrialFarmer.h"
 #include "Programs/Farming/PokemonSV_BBQSoloFarmer.h"
 #include "Programs/Farming/PokemonSV_MaterialFarmer.h"
-#include "Programs/ItemPrinter/PokemonSV_AutoItemPrinter.h"
 #include "Programs/ItemPrinter/PokemonSV_ItemPrinterRNG.h"
 
 #include "Programs/Eggs/PokemonSV_EggFetcher.h"
@@ -59,8 +58,10 @@
 #include "Programs/Glitches/PokemonSV_RideCloner-1.0.1.h"
 #include "Programs/Glitches/PokemonSV_CloneItems-1.0.1.h"
 
-#include "Programs/TestPrograms/PokemonSV_SoundListener.h"
+//  Deprecated
+#include "Programs/ItemPrinter/PokemonSV_AutoItemPrinter.h"
 
+#include "Programs/TestPrograms/PokemonSV_SoundListener.h"
 #include "Programs/FormHunting/PokemonSV_ThreeSegmentDudunsparceFinder.h"
 
 #ifdef PA_OFFICIAL
@@ -88,7 +89,6 @@ std::vector<PanelEntry> PanelListFactory::make_panels() const{
     ret.emplace_back(make_single_switch_program<ClothingBuyer_Descriptor, ClothingBuyer>());
     ret.emplace_back(make_single_switch_program<AutonomousBallThrower_Descriptor, AutonomousBallThrower>());
     ret.emplace_back(make_single_switch_program<SizeChecker_Descriptor, SizeChecker>());
-    ret.emplace_back(make_single_switch_program<AutoItemPrinter_Descriptor, AutoItemPrinter>());
 
 //    ret.emplace_back("---- Trading ----");
     ret.emplace_back(make_multi_switch_program<SelfBoxTrade_Descriptor, SelfBoxTrade>());
@@ -146,6 +146,9 @@ std::vector<PanelEntry> PanelListFactory::make_panels() const{
 
     ret.emplace_back("---- Public Betas ----");
     ret.emplace_back(make_single_switch_program<AutoStory_Descriptor, AutoStory>());
+
+    ret.emplace_back("---- Deprecated Programs ----");
+    ret.emplace_back(make_single_switch_program<AutoItemPrinter_Descriptor, AutoItemPrinter>());
 
     if (PreloadSettings::instance().DEVELOPER_MODE || IS_BETA_VERSION){
         ret.emplace_back("---- Untested/Beta/WIP ----");
