5f2279278122dd5175fb767f5ed3e093bb067760
Gin <>
2025-07-09 21:19:35 -0700

    use base64 to store annotation mask

--- SerialPrograms/Source/ML/Programs/ML_LabelImages.cpp
+++ SerialPrograms/Source/ML/Programs/ML_LabelImages.cpp
@@ -19,6 +19,7 @@
 #include <filesystem>
 #include <cmath>
 #include "CommonFramework/Globals.h"
+#include "Common/Cpp/BitmapConversion.h"
 #include "Common/Cpp/Json/JsonArray.h"
 #include "Common/Cpp/Json/JsonObject.h"
 #include "Common/Cpp/Json/JsonValue.h"
@@ -64,11 +65,15 @@ ObjectAnnotation json_to_object_annotation(const JsonValue& value){
         size_t(mask_box_array[2].to_integer_throw()),
         size_t(mask_box_array[3].to_integer_throw())
     );
-    size_t mask_width = anno_obj.mask_box.width(), mask_height = anno_obj.mask_box.height();
-    anno_obj.mask.resize(mask_width * mask_height);
-    const JsonArray& mask_values = json_obj.get_array_throw("Mask");
-    for(size_t i = 0; i < anno_obj.mask.size(); i++){
-        anno_obj.mask[i] = bool(mask_values[i].to_integer_throw());
+    const size_t mask_width = anno_obj.mask_box.width(), mask_height = anno_obj.mask_box.height();
+    const size_t num_mask_ele = mask_width * mask_height;
+    
+    const std::string mask_base64 = json_obj.get_string_throw("Mask");
+    anno_obj.mask = unpack_bit_vector_from_base64(mask_base64, num_mask_ele);
+    if (anno_obj.mask.size() != num_mask_ele){
+        std::string err_msg = "wrong decoded object annotation mask size: decoded " + std::to_string(anno_obj.mask.size())
+            + " but should be " + std::to_string(num_mask_ele);
+        throw ParseException(err_msg);
     }
 
     anno_obj.label = json_obj.get_string_throw("Label");
@@ -92,11 +97,7 @@ JsonObject object_annotation_to_json(const ObjectAnnotation& object_annotation){
     mask_box_arr.push_back(int64_t(object_annotation.mask_box.max_y));
     json_obj["MaskBox"] = std::move(mask_box_arr);
 
-    JsonArray mask_arr;
-    for(size_t i = 0; i < object_annotation.mask.size(); i++){
-        mask_arr.push_back(int64_t(object_annotation.mask[i]));
-    }
-    json_obj["Mask"] = std::move(mask_arr);
+    json_obj["Mask"] = pack_bit_vector_to_base64(object_annotation.mask);
 
     json_obj["Label"] = object_annotation.label;
 
@@ -270,7 +271,7 @@ void LabelImages::load_image_related_data(const std::string& image_path, size_t 
         return;
     }
 
-    JsonValue loaded_json = parse_json(json_content);
+    const JsonValue loaded_json = parse_json(json_content);
     const JsonArray* json_array = loaded_json.to_array();
     if (json_array == nullptr){
         m_fail_to_load_annotation_file = true;
