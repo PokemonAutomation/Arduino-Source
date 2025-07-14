a2dcaa8139bc56273682ac320e520219bf8d9718
Alexander Yee <a-yee@u.northwestern.edu>
2025-07-13 18:00:13 -0700

    PLA pokeballs sprites for Pokemon Home.

--- SerialPrograms/Source/PokemonHome/Inference/PokemonHome_BallReader.cpp
+++ SerialPrograms/Source/PokemonHome/Inference/PokemonHome_BallReader.cpp
@@ -4,11 +4,15 @@
  *
  */
 
+#include "Kernels/Waterfill/Kernels_Waterfill_Session.h"
 #include "CommonFramework/ImageTypes/ImageViewRGB32.h"
 #include "CommonFramework/Notifications/ProgramInfo.h"
 #include "CommonFramework/Tools/ErrorDumper.h"
-#include "PokemonSwSh/Resources/PokemonSwSh_PokeballSprites.h"
-#include "PokemonBDSP/Inference/PokemonBDSP_PokeballSpriteMatcher.h"
+#include "CommonTools/Images/BinaryImage_FilterRgb32.h"
+#include "CommonTools/ImageMatch/ImageCropper.h"
+#include "PokemonHome/Resources/PokemonHome_PokeballSprites.h"
+//#include "PokemonSwSh/Resources/PokemonSwSh_PokeballSprites.h"
+//#include "PokemonBDSP/Inference/PokemonBDSP_PokeballSpriteMatcher.h"
 #include "PokemonHome_BallReader.h"
 
 namespace PokemonAutomation{
@@ -16,12 +20,70 @@ namespace NintendoSwitch{
 namespace PokemonHome{
 
 
+
+
+PokeballSpriteMatcher::PokeballSpriteMatcher(double min_euclidean_distance)
+    : CroppedImageDictionaryMatcher({1, 128})
+    , m_min_euclidean_distance_squared(min_euclidean_distance * min_euclidean_distance)
+{
+    for (const auto& item : PokemonHome::ALL_POKEBALL_SPRITES()){
+        add(item.first, remove_white_border(item.second.sprite));
+    }
+}
+ImageRGB32 PokeballSpriteMatcher::remove_white_border(const ImageViewRGB32& image){
+    using namespace Kernels::Waterfill;
+
+    ImageRGB32 ret = image.copy();
+    {
+        auto matrix = compress_rgb32_to_binary_range(ret, 0x00000000, 0x7f000000);
+        auto session = make_WaterfillSession(matrix);
+        auto iter = session->make_iterator(200);
+        WaterfillObject object;
+        if (!iter->find_next(object, true)){
+            return ret;
+        }
+        filter_by_mask(object.packed_matrix(), ret, Color(0xffffffff), false);
+    }
+    {
+        auto matrix = compress_rgb32_to_binary_range(ret, 0x00808080, 0xffffffff);
+        auto session = make_WaterfillSession(matrix);
+        auto iter = session->make_iterator(200);
+        WaterfillObject object;
+        if (!iter->find_next(object, true)){
+            return ret;
+        }
+        filter_by_mask(object.packed_matrix(), ret, Color(0x00000000), false);
+    }
+    return ret;
+}
+auto PokeballSpriteMatcher::get_crop_candidates(const ImageViewRGB32& image) const -> std::vector<ImageViewRGB32>{
+    ImageStats border = image_border_stats(image);
+    ImagePixelBox box = ImageMatch::enclosing_rectangle_with_pixel_filter(
+        image,
+        [&](Color pixel){
+            double r = (double)pixel.red() - border.average.r;
+            double g = (double)pixel.green() - border.average.g;
+            double b = (double)pixel.blue() - border.average.b;
+            bool stop = r*r + g*g + b*b >= m_min_euclidean_distance_squared;
+            return stop;
+        }
+    );
+    std::vector<ImageViewRGB32> ret;
+    ret.emplace_back(extract_box_reference(image, box));
+    return ret;
+}
+
+
+
+
+
+
 const double BallReader::MAX_ALPHA = 0.40;
 const double BallReader::ALPHA_SPREAD = 0.02;
 
 
-const PokemonBDSP::PokeballSpriteMatcher& BALL_SPRITE_MATCHER(){
-    static PokemonBDSP::PokeballSpriteMatcher matcher;
+const PokeballSpriteMatcher& BALL_SPRITE_MATCHER(){
+    static PokeballSpriteMatcher matcher;
     return matcher;
 }
 
---
+++ SerialPrograms/Source/PokemonHome/Resources/PokemonHome_PokeballSprites.h
@@ -0,0 +1,24 @@
+/*  Pokemon s Pokeball Sprites
+ *
+ *  From: https://github.com/PokemonAutomation/
+ *
+ */
+
+#ifndef PokemonAutomation_PokemonHome_PokeballSprites_H
+#define PokemonAutomation_PokemonHome_PokeballSprites_H
+
+#include "CommonTools/Resources/SpriteDatabase.h"
+
+namespace PokemonAutomation{
+namespace NintendoSwitch{
+namespace PokemonHome{
+
+
+const SpriteDatabase& ALL_POKEBALL_SPRITES();
+
+
+
+}
+}
+}
+#endif
