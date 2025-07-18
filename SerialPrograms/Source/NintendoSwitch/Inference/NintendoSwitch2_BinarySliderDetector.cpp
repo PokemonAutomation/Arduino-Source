4caa014abcdff81da4cd9260e39ceaa0a880b68a
Alexander Yee <a-yee@u.northwestern.edu>
2025-07-17 20:00:42 -0700

    Fix slider detection.

--- SerialPrograms/Source/NintendoSwitch/Inference/NintendoSwitch2_BinarySliderDetector.cpp
+++ SerialPrograms/Source/NintendoSwitch/Inference/NintendoSwitch2_BinarySliderDetector.cpp
@@ -42,45 +42,69 @@ std::vector<std::pair<bool, ImagePixelBox>> BinarySliderDetector::detect(const I
 
     ImageViewRGB32 region = extract_box_reference(image, m_box);
 
-    auto matrix = compress_rgb32_to_binary_range(region, 0xffc0c0c0, 0xffffffff);
-    auto session = make_WaterfillSession(matrix);
-    auto iter = session->make_iterator(200);
-    WaterfillObject object;
-
-    std::vector<std::pair<bool, ImagePixelBox>> ret;
-
-    while (iter->find_next(object, false)){
-        double aspect_ratio = object.aspect_ratio();
-        if (aspect_ratio < 0.9 || aspect_ratio > 1.1){
-            continue;
+    std::vector<PackedBinaryMatrix> matrices = compress_rgb32_to_binary_range(
+        region,
+        {
+            {0xffc0c0c0, 0xffffffff},
+            {0xffd0d0d0, 0xffffffff},
+            {0xffe0e0e0, 0xffffffff},
+            {0xfff0f0f0, 0xffffffff},
         }
-        ImageViewRGB32 cropped = extract_box_reference(region, object);
+    );
 
-        double best_off_rmsd = 9999;
-        best_off_rmsd = std::min(best_off_rmsd, LIGHT_OFF_CURSOR.rmsd(cropped));
-        best_off_rmsd = std::min(best_off_rmsd, LIGHT_OFF_NOCURSOR.rmsd(cropped));
-        best_off_rmsd = std::min(best_off_rmsd, DARK_OFF_CURSOR.rmsd(cropped));
-        best_off_rmsd = std::min(best_off_rmsd, DARK_OFF_NOCURSOR.rmsd(cropped));
+    auto session = make_WaterfillSession();
 
-        double best_on_rmsd = 9999;
-        best_on_rmsd = std::min(best_on_rmsd, LIGHT_ON_CURSOR.rmsd(cropped));
-        best_on_rmsd = std::min(best_on_rmsd, LIGHT_ON_NOCURSOR.rmsd(cropped));
-        best_on_rmsd = std::min(best_on_rmsd, DARK_ON_CURSOR.rmsd(cropped));
-        best_on_rmsd = std::min(best_on_rmsd, DARK_ON_NOCURSOR.rmsd(cropped));
+    std::vector<std::pair<bool, ImagePixelBox>> best;
 
-//        cout << "best_off_rmsd = " << best_off_rmsd << endl;
-//        cout << "best_on_rmsd  = " << best_on_rmsd << endl;
+    for (PackedBinaryMatrix& matrix : matrices){
+//        cout << "-----------------" << endl;
+        session->set_source(matrix);
+        auto iter = session->make_iterator(200);
+        WaterfillObject object;
 
-        bool on = best_on_rmsd < best_off_rmsd;
-        double best = on ? best_on_rmsd : best_off_rmsd;
+        std::vector<std::pair<bool, ImagePixelBox>> sliders;
 
-        if (best < 60){
-            ret.emplace_back(on, object);
+        while (iter->find_next(object, false)){
+            double aspect_ratio = object.aspect_ratio();
+            if (aspect_ratio < 0.9 || aspect_ratio > 1.1){
+                continue;
+            }
+            ImageViewRGB32 cropped = extract_box_reference(region, object);
+
+            double best_off_rmsd = 9999;
+            best_off_rmsd = std::min(best_off_rmsd, LIGHT_OFF_CURSOR.rmsd(cropped));
+            best_off_rmsd = std::min(best_off_rmsd, LIGHT_OFF_NOCURSOR.rmsd(cropped));
+            best_off_rmsd = std::min(best_off_rmsd, DARK_OFF_CURSOR.rmsd(cropped));
+            best_off_rmsd = std::min(best_off_rmsd, DARK_OFF_NOCURSOR.rmsd(cropped));
+
+            double best_on_rmsd = 9999;
+            best_on_rmsd = std::min(best_on_rmsd, LIGHT_ON_CURSOR.rmsd(cropped));
+            best_on_rmsd = std::min(best_on_rmsd, LIGHT_ON_NOCURSOR.rmsd(cropped));
+            best_on_rmsd = std::min(best_on_rmsd, DARK_ON_CURSOR.rmsd(cropped));
+            best_on_rmsd = std::min(best_on_rmsd, DARK_ON_NOCURSOR.rmsd(cropped));
+
+    //        cout << "best_off_rmsd = " << best_off_rmsd << endl;
+    //        cout << "best_on_rmsd  = " << best_on_rmsd << endl;
+
+            bool on = best_on_rmsd < best_off_rmsd;
+            double best_rmsd = on ? best_on_rmsd : best_off_rmsd;
+
+            if (best_rmsd < 60){
+                sliders.emplace_back(on, object);
+            }
+
+//            for (auto& item : sliders){
+//                cout << item.first << " : " << item.second.max_y << endl;
+//            }
+
         }
 
+        if (best.size() < sliders.size()){
+            best = std::move(sliders);
+        }
     }
 
-    return ret;
+    return best;
 }
 
 
