c82db11019bef74ce35edd702c26e5800727f602
jw098 <justin.wong098@gmail.com>
2025-08-02 12:20:54 -0700

    UnexpectedBattleException now inherits OperationFailedException (#658)
    * UnexpectedBattleException now inherits OperationFailedException
    
    * fix build

--- SerialPrograms/Source/CommonFramework/Exceptions/UnexpectedBattleException.h
+++ SerialPrograms/Source/CommonFramework/Exceptions/UnexpectedBattleException.h
@@ -7,18 +7,22 @@
 #ifndef PokemonAutomation_UnexpectedBattleException_H
 #define PokemonAutomation_UnexpectedBattleException_H
 
-#include "ScreenshotException.h"
+#include "OperationFailedException.h"
 
 namespace PokemonAutomation{
-
-class FatalProgramException;
-
+namespace NintendoSwitch{
 
 //  Thrown by subroutines if caught in an wild battle in-game unexpectedly.
 //  These include recoverable errors which can be consumed by the program.
-class UnexpectedBattleException : public ScreenshotException{
+class UnexpectedBattleException : public OperationFailedException{
 public:
-    using ScreenshotException::ScreenshotException;
+    UnexpectedBattleException(
+        ErrorReport error_report,
+        std::string message,
+        VideoStream& stream
+    )
+        : OperationFailedException(error_report, std::move(message), stream)
+    {}
 
     virtual const char* name() const override{ return "UnexpectedBattleException"; }
 };
@@ -27,5 +31,6 @@ public:
 
 
 
+}
 }
 #endif
