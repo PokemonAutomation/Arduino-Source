272b7d7c4168b4e153c7509c332e4d11f7a8b640
Gin <>
2025-09-14 18:10:00 -0700

    cleanup

--- SerialPrograms/Source/ML/Programs/ML_RunYOLO.cpp
+++ SerialPrograms/Source/ML/Programs/ML_RunYOLO.cpp
@@ -16,7 +16,6 @@
 #include "CommonFramework/VideoPipeline/VideoOverlayScopes.h"
 #include "CommonFramework/VideoPipeline/VideoFeed.h"
 #include "ML/Inference/ML_YOLOv5Detector.h"
-#include "Common/Cpp/MemoryUtilization/MemoryUtilization.h"
 #include "ML_RunYOLO.h"
 
 namespace PokemonAutomation{
@@ -41,11 +40,6 @@ RunYOLO_Descriptor::RunYOLO_Descriptor()
 RunYOLO::RunYOLO() {}
 
 void RunYOLO::program(NintendoSwitch::SingleSwitchProgramEnvironment& env, NintendoSwitch::ProControllerContext& context){
-
-    MemoryUsage mu = process_memory_usage();
-    std::cout << "Memory usage: " << mu.total_system_memory << " " << mu.total_used_system_memory << " " 
-        << mu.process_physical_memory << " " << mu.process_virtual_memory << std::endl;
-    
     YOLOv5Watcher watcher(env.console.overlay());
 
     wait_until(env.console, context, WallClock::max(), {watcher});
