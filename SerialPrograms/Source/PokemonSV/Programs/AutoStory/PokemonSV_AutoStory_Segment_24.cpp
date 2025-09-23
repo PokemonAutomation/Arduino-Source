7b71ed2bfdeff4ac9d45aa66b0a09d07f99c42ce
jw098 <justin.wong098@gmail.com>
2025-09-23 16:18:15 -0700

    checkpoint 56: beat Orthworm

--- SerialPrograms/Source/PokemonSV/Programs/AutoStory/PokemonSV_AutoStory_Segment_24.cpp
+++ SerialPrograms/Source/PokemonSV/Programs/AutoStory/PokemonSV_AutoStory_Segment_24.cpp
@@ -54,6 +54,8 @@ void AutoStory_Segment_24::run_segment(
     env.console.log("Start Segment " + name(), COLOR_ORANGE);
 
     checkpoint_55(env, context, options.notif_status_update, stats);
+    checkpoint_56(env, context, options.notif_status_update, stats);
+    // checkpoint_57(env, context, options.notif_status_update, stats);
 
     context.wait_for_all_requests();
     env.console.log("End Segment " + name(), COLOR_GREEN);
@@ -70,7 +72,7 @@ void checkpoint_55(
     checkpoint_reattempt_loop(env, context, notif_status_update, stats,
     [&](size_t attempt_number){
 
-        handle_unexpected_battles(env.program_info(), env.console, context,
+        do_action_and_monitor_for_battles(env.program_info(), env.console, context,
         [&](const ProgramInfo& info, VideoStream& stream, ProControllerContext& context){
             
             DirectionDetector direction;
@@ -129,6 +131,7 @@ void checkpoint_55(
 
         walk_forward_until_dialog(env.program_info(), env.console, context, NavigationMovementMode::DIRECTIONAL_ONLY, 30);
 
+        env.console.log("Battle Orthworm Titan phase 1.");
         run_wild_battle_press_A(env.console, context, BattleStopCondition::STOP_OVERWORLD);
 
     });    
@@ -144,6 +147,61 @@ void checkpoint_56(
     checkpoint_reattempt_loop(env, context, notif_status_update, stats,
     [&](size_t attempt_number){
 
+        do_action_and_monitor_for_battles(env.program_info(), env.console, context,
+        [&](const ProgramInfo& info, VideoStream& stream, ProControllerContext& context){
+            
+            DirectionDetector direction;
+
+            direction.change_direction(env.program_info(), env.console, context, 5.042435);
+            pbf_move_left_joystick(context, 128, 0, 900, 100);
+
+            direction.change_direction(env.program_info(), env.console, context, 5.360763);
+            pbf_move_left_joystick(context, 128, 0, 500, 100);
+
+            direction.change_direction(env.program_info(), env.console, context, 5.85);
+            pbf_move_left_joystick(context, 128, 0, 700, 100);
+
+            direction.change_direction(env.program_info(), env.console, context, 5.428);
+            pbf_move_left_joystick(context, 128, 0, 600, 100);
+
+            direction.change_direction(env.program_info(), env.console, context, 4.908646);
+            pbf_move_left_joystick(context, 128, 0, 300, 100);
+
+            direction.change_direction(env.program_info(), env.console, context, 1.169728);
+            pbf_move_left_joystick(context, 128, 0, 200, 100);
+            pbf_move_left_joystick(context, 255, 0, 200, 100);
+            pbf_move_left_joystick(context, 0, 0, 200, 100);
+
+            // now aligned to the wall next to the hole/passage   
+
+            // walk backwards
+            direction.change_direction(env.program_info(), env.console, context, 2.303077);
+            pbf_move_left_joystick(context, 128, 255, 400, 100);
+            direction.change_direction(env.program_info(), env.console, context, 3.908360);
+
+            get_on_ride(env.program_info(), env.console, context);
+
+            // charge at orthworm
+            pbf_move_left_joystick(context, 128, 0, 408ms, 0ms);
+            pbf_controller_state(context, BUTTON_LCLICK, DPAD_NONE, 128, 0, 128, 128, 102ms);
+            pbf_move_left_joystick(context, 128, 0, 1970ms, 0ms);
+            pbf_controller_state(context, BUTTON_NONE, DPAD_NONE, 128, 0, 0, 128, 432ms);
+            pbf_move_left_joystick(context, 128, 0, 1993ms, 0ms);
+            pbf_controller_state(context, BUTTON_NONE, DPAD_NONE, 128, 0, 0, 128, 301ms);
+            pbf_move_left_joystick(context, 128, 0, 307ms, 0ms);
+            pbf_controller_state(context, BUTTON_NONE, DPAD_NONE, 128, 0, 0, 128, 194ms);
+            pbf_move_left_joystick(context, 128, 0, 886ms, 0ms);
+            pbf_controller_state(context, BUTTON_NONE, DPAD_NONE, 128, 0, 0, 128, 626ms);
+            pbf_move_left_joystick(context, 128, 0, 2651ms, 0ms);
+        });
+
+        walk_forward_until_dialog(env.program_info(), env.console, context, NavigationMovementMode::DIRECTIONAL_ONLY, 30);
+
+        // battle the titan phase 2
+        clear_dialog(env.console, context, ClearDialogMode::STOP_BATTLE, 60, {CallbackEnum::BATTLE});  
+        env.console.log("Battle Orthworm Titan phase 2.");
+        run_wild_battle_press_A(env.console, context, BattleStopCondition::STOP_DIALOG, {CallbackEnum::DIALOG_ARROW});
+        mash_button_till_overworld(env.console, context, BUTTON_A, 360);
 
     });    
 
