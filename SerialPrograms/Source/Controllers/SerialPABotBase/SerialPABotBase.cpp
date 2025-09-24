63a0430fe67ced5e15fc7aebd187eee79bd271bf
Alexander Yee <a-yee@u.northwestern.edu>
2025-09-23 21:24:02 -0700

    Firmware version.

--- SerialPrograms/Source/Controllers/SerialPABotBase/SerialPABotBase.cpp
+++ SerialPrograms/Source/Controllers/SerialPABotBase/SerialPABotBase.cpp
@@ -21,7 +21,7 @@ const std::map<pabb_ProgramID, uint32_t>& SUPPORTED_DEVICES(){
         {PABB_PID_PABOTBASE_ProMicro,           2025090300},
         {PABB_PID_PABOTBASE_Teensy2,            2025090300},
         {PABB_PID_PABOTBASE_TeensyPP2,          2025090300},
-        {PABB_PID_PABOTBASE_ESP32,              2025090302},
+        {PABB_PID_PABOTBASE_ESP32,              2025092300},
         {PABB_PID_PABOTBASE_ESP32S3,            2025090300},
         {PABB_PID_PABOTBASE_Pico1W_USB,         2025090401},
         {PABB_PID_PABOTBASE_Pico1W_UART,        2025090401},
