da2ee4a770522db62282cc8d92f5dd42cbaac97e
Gin <>
2025-09-27 18:24:06 -0700

    update add_new_file script for the new file storing code file path list

--- SerialPrograms/Scripts/add_new_file.py
+++ SerialPrograms/Scripts/add_new_file.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 
 """
-Add a new file to CMakeList.txt and SerialPrograms.pro
+Add a new file to SourceFiles.cmake, the CMake file that sets all code paths
 
 Usage: python3 add_new_file.py <path to new file relative to Arduino-Source/
 
 The script will try to find the root code folder "Arduino-Source/" from the path of the current directory
 where this script is executed.
 
-Then it will add the new file path (as the input parameter of this program) to proper places in CMakeList.txt
-and SerialPrograms.pro.
+Then it will add the new file path (as the input parameter of this program) to proper places in SourceFiles.cmake.
 
 Example usage:
 python3 add_new_file.py Source/PokemonSV/Inference/Map/PokemonSV_MapFlyMenuDetectorDetector.h
@@ -75,18 +74,16 @@ def get_code_file_range(file_lines: List[str], starting_line: str, ending_line: 
 
 
 
-cmakelists_path = os.path.join(code_root_path, "SerialPrograms", "CMakeLists.txt")
-pro_path = os.path.join(code_root_path, "SerialPrograms", "SerialPrograms.pro")
-print(f"CMakeLists path: {cmakelists_path}")
-print(f"QT Pro project file path: {pro_path}")
+cmake_file_path = os.path.join(code_root_path, "SerialPrograms", "SourceFiles.cmake")
+print(f"SourceFiles.cmake path: {cmake_file_path}")
 
-file_lines = read_lines(cmakelists_path)
+file_lines = read_lines(cmake_file_path)
 old_file_lines = file_lines
 
 code_file_start, code_file_end = get_code_file_range(file_lines, "file(GLOB MAIN_SOURCES", ")")
 code_file_lines = set(file_lines[code_file_start:code_file_end])
 old_num_code_files = len(code_file_lines)
-print(f"{old_num_code_files} lines from CMakeLists are for code file paths")
+print(f"{old_num_code_files} lines from SourceFiles.cmake are for code file paths")
 
 # Add new file
 if len(target_path) > 0:
@@ -94,73 +91,16 @@ if len(target_path) > 0:
 
 code_file_lines = list(code_file_lines)
 code_file_lines.sort()
-if len(code_file_lines) == old_num_code_files:
+if len(target_path) > 0 and len(code_file_lines) == old_num_code_files:
 	print("Warning: you are adding an existing file! The added file is ignored")
 
 # print(f"\"{code_file_lines[0]}\"")
 
 file_lines = file_lines[0:code_file_start] + code_file_lines + file_lines[code_file_end:]
 file_lines = [line + "\r\n" for line in file_lines]
-with open(cmakelists_path, "w") as f:
+with open(cmake_file_path, "w") as f:
 	f.writelines(file_lines)
-print(f"Writed changes back to {cmakelists_path}")
-
-if not os.path.exists(pro_path):
-	print(f"Pro file not found. End.")
-	exit(0)
-
-file_lines = read_lines(pro_path)
-
-# print("\n".join(file_lines[0:70]))
-
-# lines = [line for line in file_lines if line.startswith("SOURCES")]
-# print(lines)
-
-
-def process_pro_lines(file_lines: List[str], starting_line: str, new_path: str) -> List[str]:
-
-	code_file_start, code_file_end = get_code_file_range(file_lines, starting_line, "")
-	# print(code_file_start, code_file_end)
-	# print(f"Starting line {file_lines[code_file_start]}")
-
-	code_file_lines = file_lines[code_file_start:code_file_end]
-
-	code_file_lines = [ line[0:-1].rstrip() if line.endswith("\\") else line for line in code_file_lines]
-
-	if len(new_path) > 0:
-		code_file_lines.append("    " + new_path)
-
-	# Remove duplicates and sort
-	code_file_lines = list(set(code_file_lines))
-	code_file_lines.sort()
-
-	# Add back " \\":
-	code_file_lines = [line + " \\" for line in code_file_lines]
-	code_file_lines[-1] = code_file_lines[-1][0:-2]
-
-	# print("\n".join(code_file_lines[0:10]))
-
-	file_lines = file_lines[0:code_file_start] + code_file_lines + file_lines[code_file_end:]
-
-	return file_lines
-
-if target_path.endswith(".cpp"):
-	file_lines = process_pro_lines(file_lines, "SOURCES += \\", target_path)
-elif target_path.endswith(".h") or target_path.endswith(".tpp"):
-	file_lines = process_pro_lines(file_lines, "HEADERS += \\", target_path)
-else:
-	file_lines = process_pro_lines(file_lines, "SOURCES += \\", "")
-	file_lines = process_pro_lines(file_lines, "HEADERS += \\", "")
-
-# print("\n".join(file_lines[0:30]))
-
-# Add back CRLF
-file_lines = [line + "\r\n" for line in file_lines]
-
-with open(pro_path, "w") as f:
-	f.writelines(file_lines)
-print(f"Writed changes back to {pro_path}")
-
+print(f"Writed changes back to {cmake_file_path}")
 
 
 
