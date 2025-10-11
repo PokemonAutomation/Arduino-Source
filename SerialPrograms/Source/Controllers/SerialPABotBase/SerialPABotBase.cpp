f0bc7d49084416e4c4ebd5f260ce305f38558d35
Alexander Yee <a-yee@u.northwestern.edu>
2025-10-11 11:53:22 -0700

    Fix wireless color table loading. Enforce latest Pico firmware.

--- SerialPrograms/Source/Controllers/SerialPABotBase/SerialPABotBase.cpp
+++ SerialPrograms/Source/Controllers/SerialPABotBase/SerialPABotBase.cpp
@@ -23,10 +23,10 @@ const std::map<pabb_ProgramID, uint32_t>& SUPPORTED_DEVICES(){
         {PABB_PID_PABOTBASE_TeensyPP2,          2025090303},
         {PABB_PID_PABOTBASE_ESP32,              2025092305},
         {PABB_PID_PABOTBASE_ESP32S3,            2025090304},
-        {PABB_PID_PABOTBASE_Pico1W_USB,         2025090409},
-        {PABB_PID_PABOTBASE_Pico1W_UART,        2025090409},
-        {PABB_PID_PABOTBASE_Pico2W_USB,         2025090409},
-        {PABB_PID_PABOTBASE_Pico2W_UART,        2025090409},
+        {PABB_PID_PABOTBASE_Pico1W_USB,         2025090410},
+        {PABB_PID_PABOTBASE_Pico1W_UART,        2025090410},
+        {PABB_PID_PABOTBASE_Pico2W_USB,         2025090410},
+        {PABB_PID_PABOTBASE_Pico2W_UART,        2025090410},
     };
     return database;
 }
