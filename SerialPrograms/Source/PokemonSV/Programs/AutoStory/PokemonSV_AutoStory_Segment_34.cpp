8f30528a98f32a6a64e304dafb4d3094a3696d19
jw098 <justin.wong098@gmail.com>
2025-10-19 22:58:44 -0700

    checkpoint 92: beat Penny

--- SerialPrograms/Source/PokemonSV/Programs/AutoStory/PokemonSV_AutoStory_Segment_34.cpp
+++ SerialPrograms/Source/PokemonSV/Programs/AutoStory/PokemonSV_AutoStory_Segment_34.cpp
@@ -3,7 +3,7 @@
  *  From: https://github.com/PokemonAutomation/
  *
  */
-
+#include "PokemonSV/Programs/Battles/PokemonSV_SinglesBattler.h"
 
 #include "CommonFramework/Exceptions/OperationFailedException.h"
 #include "CommonTools/Async/InferenceRoutines.h"
@@ -105,6 +105,85 @@ void checkpoint_92(SingleSwitchProgramEnvironment& env, ProControllerContext& co
     checkpoint_reattempt_loop(env, context, notif_status_update, stats,
     [&](size_t attempt_number){
 
+        // walk down
+        pbf_move_left_joystick(context, 128, 255, 100, 100);
+        // walk right towards door
+        pbf_move_left_joystick(context, 255, 128, 200, 100);
+
+        wait_for_gradient_arrow(env.program_info(), env.console, context, {0.031, 0.193, 0.047, 0.078}, 10);
+
+        env.console.log("Leave dorm for schoolyard.");
+        pbf_press_dpad(context, DPAD_UP, 13, 20);
+        pbf_press_dpad(context, DPAD_LEFT, 13, 20);
+        pbf_press_dpad(context, DPAD_DOWN, 13, 20);
+        wait_for_gradient_arrow(env.program_info(), env.console, context, {0.031, 0.790, 0.047, 0.078}, 10);
+        pbf_mash_button(context, BUTTON_A, 1000ms);
+
+        clear_dialog(env.console, context, ClearDialogMode::STOP_BATTLE, 60, {CallbackEnum::BATTLE, CallbackEnum::DIALOG_ARROW, CallbackEnum::PROMPT_DIALOG});
+        SinglesMoveEntry move1{SinglesMoveType::Move1, true};  // Moonblast
+        std::vector<SinglesMoveEntry> move_table1 = {move1};
+        bool terastallized = false;
+        bool is_won = run_pokemon(env.console, context, move_table1, true, terastallized);
+        if (!is_won){// throw exception if we lose
+            OperationFailedException::fire(
+                ErrorReport::SEND_ERROR_REPORT,
+                "Failed to beat the Ground trainer. Reset.",
+                env.console
+            );
+        }
+        
+        mash_button_till_overworld(env.console, context, BUTTON_A);
+
+        // now back in dorm room.
+
+        // walk right towards door
+        pbf_move_left_joystick(context, 255, 128, 200, 100);
+
+        wait_for_gradient_arrow(env.program_info(), env.console, context, {0.031, 0.193, 0.047, 0.078}, 10);
+
+        env.console.log("Leave dorm for Director's office.");
+        pbf_press_dpad(context, DPAD_DOWN, 13, 20);
+        pbf_press_dpad(context, DPAD_RIGHT, 13, 20);
+        pbf_press_dpad(context, DPAD_DOWN, 13, 20);
+        wait_for_gradient_arrow(env.program_info(), env.console, context, {0.031, 0.345, 0.047, 0.078}, 10);
+        pbf_mash_button(context, BUTTON_A, 1000ms);
+
+        wait_for_overworld(env.program_info(), env.console, context);
+
+        walk_forward_until_dialog(env.program_info(), env.console, context, NavigationMovementMode::DIRECTIONAL_ONLY, 30);
+
+        mash_button_till_overworld(env.console, context, BUTTON_A);
+
+         // walk right towards door
+        pbf_move_left_joystick(context, 255, 128, 200, 100);
+
+        wait_for_gradient_arrow(env.program_info(), env.console, context, {0.031, 0.193, 0.047, 0.078}, 10);
+        mash_button_till_overworld(env.console, context, BUTTON_A);
+
+        // now in school lobby
+
+        pbf_move_left_joystick(context, 128, 255, 1000, 100);
+        // wait for dialog when leaving school lobby
+        pbf_wait(context, 3 * TICKS_PER_SECOND);    
+        
+        mash_button_till_overworld(env.console, context, BUTTON_A);
+        
+        realign_player(env.program_info(), env.console, context, PlayerRealignMode::REALIGN_OLD_MARKER);
+        handle_when_stationary_in_overworld(env.program_info(), env.console, context, 
+            [&](const ProgramInfo& info, VideoStream& stream, ProControllerContext& context){
+                overworld_navigation(env.program_info(), env.console, context, 
+                    NavigationStopCondition::STOP_DIALOG, NavigationMovementMode::DIRECTIONAL_SPAM_A, 
+                    128, 0, 60, 60, false);
+            }, 
+            [&](const ProgramInfo& info, VideoStream& stream, ProControllerContext& context){
+                pbf_move_left_joystick(context, 0, 255, 40, 50);
+                realign_player(env.program_info(), env.console, context, PlayerRealignMode::REALIGN_OLD_MARKER);
+            }
+        );
+
+        mash_button_till_overworld(env.console, context, BUTTON_A);
+
+        move_cursor_towards_flypoint_and_go_there(env.program_info(), env.console, context, {ZoomChange::ZOOM_IN, 0, 0, 0}, FlyPoint::FAST_TRAVEL);
 
     });   
 }
