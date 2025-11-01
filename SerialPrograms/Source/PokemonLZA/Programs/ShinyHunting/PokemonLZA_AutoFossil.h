10de9f64591cf133962dfe3ee7d560b5c72ab461
Gin <>
2025-11-01 13:30:29 -0700

    improve auto fossil

--- SerialPrograms/Source/PokemonLZA/Programs/ShinyHunting/PokemonLZA_AutoFossil.h
+++ SerialPrograms/Source/PokemonLZA/Programs/ShinyHunting/PokemonLZA_AutoFossil.h
@@ -43,14 +43,14 @@ private:
     // return true if found a match
     bool check_fossils_in_one_box(SingleSwitchProgramEnvironment& env, ProControllerContext& context, size_t num_fossils_in_box);
 
-    PokemonLA::StopOnOption STOP_ON;
-
     SimpleIntegerOption<uint32_t> NUM_FOSSILS;
-    EventNotificationOption FOUND_SHINY_OR_ALPHA;
+    IntegerEnumDropdownOption WHICIH_FOSSIL;
+    PokemonLA::StopOnOption STOP_ON;
     
     BooleanCheckBoxOption TAKE_VIDEO;
     GoHomeWhenDoneOption GO_HOME_WHEN_DONE;
 
+    EventNotificationOption FOUND_SHINY_OR_ALPHA;
     EventNotificationOption NOTIFICATION_STATUS;
     EventNotificationsOption NOTIFICATIONS;
 };
