b74e8f7ca8c01d6076969d5ddb3ca8bc25d2a9d0
Alexander Yee <a-yee@u.northwestern.edu>
2025-11-01 11:17:29 -0700

    Make shiny action behavior consistent with bench program. Formatting.

--- SerialPrograms/Source/PokemonLZA/Programs/ShinyHunting/PokemonLZA_WildZoneEntrance.cpp
+++ SerialPrograms/Source/PokemonLZA/Programs/ShinyHunting/PokemonLZA_WildZoneEntrance.cpp
@@ -25,44 +25,52 @@ using namespace Pokemon;
 
 
 ShinyHunt_WildZoneEntrance_Descriptor::ShinyHunt_WildZoneEntrance_Descriptor()
-    : SingleSwitchProgramDescriptor("PokemonLZA:ShinyHunt-WildZoneEntrance", STRING_POKEMON + " LZA",
-                                    "Shiny Hunt - Wild Zone Entrance",
-                                    "Programs/PokemonLZA/ShinyHunt-WildZoneEntrance.html",
-                                    "Shiny hunt by repeatedly entering Wild Zone from its entrance.",
-                                    ProgramControllerClass::StandardController_NoRestrictions, FeedbackType::REQUIRED,
-                                    AllowCommandsWhenRunning::DISABLE_COMMANDS, {}) {}
-class ShinyHunt_WildZoneEntrance_Descriptor::Stats : public StatsTracker {
+    : SingleSwitchProgramDescriptor(
+        "PokemonLZA:ShinyHunt-WildZoneEntrance", STRING_POKEMON + " LZA",
+        "Shiny Hunt - Wild Zone Entrance",
+        "Programs/PokemonLZA/ShinyHunt-WildZoneEntrance.html",
+        "Shiny hunt by repeatedly entering Wild Zone from its entrance.",
+        ProgramControllerClass::StandardController_NoRestrictions, FeedbackType::REQUIRED,
+        AllowCommandsWhenRunning::DISABLE_COMMANDS, {}
+    )
+{}
+class ShinyHunt_WildZoneEntrance_Descriptor::Stats : public StatsTracker{
 public:
-    Stats() : resets(m_stats["Wild Zone"]), shinies(m_stats["Shiny Sounds"]), errors(m_stats["Errors"]) {
+    Stats()
+        : resets(m_stats["Wild Zone"])
+        , shinies(m_stats["Shiny Sounds"])
+        , errors(m_stats["Errors"])
+    {
         m_display_order.emplace_back("Wild Zone");
         m_display_order.emplace_back("Shiny Sounds");
         m_display_order.emplace_back("Errors", HIDDEN_IF_ZERO);
-
-        m_aliases["Shinies"] = "Shiny Sounds";
-        m_aliases["Shinies Detected"] = "Shiny Sounds";
     }
 
     std::atomic<uint64_t>& resets;
     std::atomic<uint64_t>& shinies;
     std::atomic<uint64_t>& errors;
 };
-std::unique_ptr<StatsTracker> ShinyHunt_WildZoneEntrance_Descriptor::make_stats() const {
+std::unique_ptr<StatsTracker> ShinyHunt_WildZoneEntrance_Descriptor::make_stats() const{
     return std::unique_ptr<StatsTracker>(new Stats());
 }
 
 
 ShinyHunt_WildZoneEntrance::ShinyHunt_WildZoneEntrance()
-    : WALK_IN_ZONE("<b>WALK IN ZONE:</b><br>Walk this long in the zone after passing through the gate.",
-                   LockMode::UNLOCK_WHILE_RUNNING, "500 ms"),
-      SHINY_DETECTED("Shiny Detected", "", "2000 ms", ShinySoundDetectedAction::NOTIFY_ON_FIRST_ONLY),
-      NOTIFICATION_STATUS("Status Update", true, false, std::chrono::seconds(3600)),
-      NOTIFICATIONS({
-          &NOTIFICATION_STATUS,
-          &SHINY_DETECTED.NOTIFICATIONS,
-          &NOTIFICATION_PROGRAM_FINISH,
-          &NOTIFICATION_ERROR_RECOVERABLE,
-          &NOTIFICATION_ERROR_FATAL,
-      }) {
+    : WALK_IN_ZONE(
+        "<b>WALK IN ZONE:</b><br>Walk this long in the zone after passing through the gate.",
+        LockMode::UNLOCK_WHILE_RUNNING,
+        "500 ms"
+    )
+    , SHINY_DETECTED("Shiny Detected", "", "2000 ms", ShinySoundDetectedAction::NOTIFY_ON_FIRST_ONLY)
+    , NOTIFICATION_STATUS("Status Update", true, false, std::chrono::seconds(3600))
+    , NOTIFICATIONS({
+        &NOTIFICATION_STATUS,
+        &SHINY_DETECTED.NOTIFICATIONS,
+        &NOTIFICATION_PROGRAM_FINISH,
+        &NOTIFICATION_ERROR_RECOVERABLE,
+        &NOTIFICATION_ERROR_FATAL,
+    })
+{
     PA_ADD_STATIC(SHINY_REQUIRES_AUDIO);
     PA_ADD_OPTION(WALK_IN_ZONE);
     PA_ADD_OPTION(SHINY_DETECTED);
@@ -70,38 +78,45 @@ ShinyHunt_WildZoneEntrance::ShinyHunt_WildZoneEntrance()
 }
 
 
-void run_to_gate(ConsoleHandle& console, ProControllerContext& context) {
-    ButtonWatcher buttonA(COLOR_RED, ButtonType::ButtonA, {0, 0, 1, 1}, &console.overlay());
-    int ret = run_until<ProControllerContext>(console, context,
-                                              [](ProControllerContext& context) {
-                                                  for (int c = 0; c < 10; c++) {
-                                                      pbf_move_left_joystick(context, 128, 0, 800ms, 200ms);
-                                                  }
-                                              },
-                                              {{buttonA}});
+void run_to_gate(ConsoleHandle& console, ProControllerContext& context){
+    ButtonWatcher buttonA(COLOR_RED, ButtonType::ButtonA, {0.3, 0.2, 0.4, 0.7}, &console.overlay());
+    int ret = run_until<ProControllerContext>(
+        console, context,
+        [](ProControllerContext& context){
+            for (int c = 0; c < 10; c++){
+                pbf_move_left_joystick(context, 128, 0, 800ms, 200ms);
+            }
+        },
+        {{buttonA}}
+    );
 
-    switch (ret) {
-        case 0:
-            break;
-        default:
-            OperationFailedException::fire(ErrorReport::SEND_ERROR_REPORT,
-                                           "run_to_gate(): Unable to detect entrance after 10 seconds.", console);
+    switch (ret){
+    case 0:
+        break;
+    default:
+        OperationFailedException::fire(
+            ErrorReport::SEND_ERROR_REPORT,
+            "run_to_gate(): Unable to detect entrance after 10 seconds.",
+            console
+        );
     }
 }
 
-void enter_wild_zone_entrance(ConsoleHandle& console, ProControllerContext& context, Milliseconds walk_in_zone) {
+void enter_wild_zone_entrance(ConsoleHandle& console, ProControllerContext& context, Milliseconds walk_in_zone){
     BlackScreenOverWatcher black_screen(COLOR_BLUE);
-    int ret = run_until<ProControllerContext>(console, context,
-                                              [&](ProControllerContext& context) {
-                                                  pbf_mash_button(context, BUTTON_B, 200ms);  // dismiss menu if any
-                                                  run_to_gate(console, context);
-                                                  pbf_mash_button(context, BUTTON_A, 2000ms);
-                                                  pbf_move_left_joystick(context, 128, 0, walk_in_zone, 200ms);
-                                                  context.wait_for_all_requests();
-                                                  pbf_press_button(context, BUTTON_PLUS, 100ms, 100ms);  // open map
-                                              },
-                                              {{black_screen}});
-    if (ret == 0) {
+    int ret = run_until<ProControllerContext>(
+        console, context,
+        [&](ProControllerContext& context){
+            pbf_mash_button(context, BUTTON_B, 200ms);  // dismiss menu if any
+            run_to_gate(console, context);
+            pbf_mash_button(context, BUTTON_A, 2000ms);
+            pbf_move_left_joystick(context, 128, 0, walk_in_zone, 200ms);
+            context.wait_for_all_requests();
+            pbf_press_button(context, BUTTON_PLUS, 100ms, 100ms);  // open map
+        },
+        {{black_screen}}
+    );
+    if (ret == 0){
         console.log("[WildZoneEntrance] Detected day/night change after entering.");
         context.wait_for(std::chrono::milliseconds(2000));
         pbf_mash_button(context, BUTTON_B, 200ms);             // dismiss menu if any
@@ -113,37 +128,44 @@ void enter_wild_zone_entrance(ConsoleHandle& console, ProControllerContext& cont
     context.wait_for(std::chrono::milliseconds(4000));  // TODO: differ NS1 wait time from NS2
 }
 
-void ShinyHunt_WildZoneEntrance::program(SingleSwitchProgramEnvironment& env, ProControllerContext& context) {
+void ShinyHunt_WildZoneEntrance::program(SingleSwitchProgramEnvironment& env, ProControllerContext& context){
     ShinyHunt_WildZoneEntrance_Descriptor::Stats& stats =
         env.current_stats<ShinyHunt_WildZoneEntrance_Descriptor::Stats>();
-    
-    if (SHINY_DETECTED.ACTION == ShinySoundDetectedAction::NOTIFY_ON_ALL) {
-        throw UserSetupError(env.console, "Shiny would be detected/notified at most once. Choose one of the other 2 options");
+
+    if (SHINY_DETECTED.ACTION == ShinySoundDetectedAction::NOTIFY_ON_ALL){
+        throw UserSetupError(
+            env.console,
+            "Shiny would be detected/notified at most once. Choose one of the other 2 options."
+        );
     }
 
-    uint8_t shiny_count = 0;
-
-    {  // first run with shiny detection
+    while (true){
         float shiny_coefficient = 1.0;
         PokemonLA::ShinySoundDetector shiny_detector(env.console, [&](float error_coefficient) -> bool {
             //  Warning: This callback will be run from a different thread than this function.
-            shiny_count++;
             stats.shinies++;
             env.update_stats();
             shiny_coefficient = error_coefficient;
             return true;
         });
 
-        run_until<ProControllerContext>(env.console, context,
-                                        [&](ProControllerContext& context) {
-                                            while (true) {
-                                                send_program_status_notification(env, NOTIFICATION_STATUS);
-                                                stats.resets++;
-                                                enter_wild_zone_entrance(env.console, context, WALK_IN_ZONE);
-                                                env.update_stats();
-                                            }
-                                        },
-                                        {{shiny_detector}});
+        int ret = run_until<ProControllerContext>(
+            env.console, context,
+            [&](ProControllerContext& context){
+                while (true){
+                    send_program_status_notification(env, NOTIFICATION_STATUS);
+                    stats.resets++;
+                    enter_wild_zone_entrance(env.console, context, WALK_IN_ZONE);
+                    env.update_stats();
+                }
+            },
+            {{shiny_detector}}
+        );
+
+        //  This should never happen.
+        if (ret != 0){
+            continue;
+        }
 
         context.wait_for(std::chrono::milliseconds(1000));
 
@@ -154,16 +176,12 @@ void ShinyHunt_WildZoneEntrance::program(SingleSwitchProgramEnvironment& env, Pr
         pbf_mash_button(context, BUTTON_A, 600ms);             // teleporting or just mashing button
         pbf_mash_button(context, BUTTON_B, 200ms);             // in case need to dismiss map
 
-        SHINY_DETECTED.on_shiny_sound(env, env.console, context, shiny_count, shiny_coefficient);
-    }
-
-    // continue with shiny sound detection is pointless
-    if (SHINY_DETECTED.ACTION != ShinySoundDetectedAction::STOP_PROGRAM) {
-        while (true) {
-            send_program_status_notification(env, NOTIFICATION_STATUS);
-            stats.resets++;
-            enter_wild_zone_entrance(env.console, context, WALK_IN_ZONE);
-            env.update_stats();
+        if (SHINY_DETECTED.on_shiny_sound(
+            env, env.console, context,
+            stats.shinies,
+            shiny_coefficient
+        )){
+            break;
         }
     }
 
