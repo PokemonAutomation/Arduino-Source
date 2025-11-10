f5bd235c3f71a6001bba4524867b140a504c302b
Nymphea <87930564+NympheaR@users.noreply.github.com>
2025-11-11 00:40:43 +0900

    Add IvJudgeReader

---
+++ SerialPrograms/Source/PokemonLZA/Inference/Boxes/PokemonLZA_IvJudgeReader.h
@@ -0,0 +1,48 @@
+/*  IV Judge Reader
+ *
+ *  From: https://github.com/PokemonAutomation/
+ *
+ */
+
+#ifndef PokemonAutomation_PokemonLZA_IvJudgeReader_H
+#define PokemonAutomation_PokemonLZA_IvJudgeReader_H
+
+#include "CommonFramework/VideoPipeline/VideoOverlayScopes.h"
+#include "Pokemon/Inference/Pokemon_IvJudgeReader.h"
+
+namespace PokemonAutomation{
+namespace NintendoSwitch{
+namespace PokemonLZA{
+using namespace Pokemon;
+
+
+const IvJudgeReader& IV_READER();
+
+
+class IvJudgeReaderScope{
+public:
+    IvJudgeReaderScope(VideoOverlay& overlay, Language language);
+
+    IvJudgeReader::Results read(Logger& logger, const ImageViewRGB32& frame);
+
+    std::vector<ImageViewRGB32> dump_images(const ImageViewRGB32& frame);
+
+private:
+    IvJudgeValue read(Logger& logger, const ImageViewRGB32& frame, const OverlayBoxScope& box);
+
+private:
+    Language m_language;
+    OverlayBoxScope m_box0;
+    OverlayBoxScope m_box1;
+    OverlayBoxScope m_box2;
+    OverlayBoxScope m_box3;
+    OverlayBoxScope m_box4;
+    OverlayBoxScope m_box5;
+};
+
+
+
+}
+}
+}
+#endif
