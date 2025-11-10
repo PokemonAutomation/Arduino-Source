c1fb465a86163191a39e2aaf93ffd69d4d55fe4e
Nymphea <87930564+NympheaR@users.noreply.github.com>
2025-11-11 00:41:58 +0900

    Add LZA StatsReset

---
+++ SerialPrograms/Source/PokemonLZA/Programs/NonShinyHunting/PokemonLZA_StatsReset.h
@@ -0,0 +1,66 @@
+/*  Stats Reset
+ *
+ *  From: https://github.com/PokemonAutomation/
+ *
+ */
+
+#ifndef PokemonAutomation_PokemonLZA_StatsReset_H
+#define PokemonAutomation_PokemonLZA_StatsReset_H
+
+#include "Common/Cpp/Options/EnumDropdownOption.h"
+#include "CommonFramework/Notifications/EventNotificationsTable.h"
+#include "CommonTools/Options/LanguageOCROption.h"
+#include "NintendoSwitch/Options/NintendoSwitch_GoHomeWhenDoneOption.h"
+#include "NintendoSwitch/Options/NintendoSwitch_StartInGripMenuOption.h"
+#include "NintendoSwitch/NintendoSwitch_SingleSwitchProgram.h"
+#include "Pokemon/Options/Pokemon_IvJudgeOption.h"
+
+namespace PokemonAutomation{
+namespace NintendoSwitch{
+namespace PokemonLZA{
+
+using namespace Pokemon;
+
+
+class StatsReset_Descriptor : public SingleSwitchProgramDescriptor{
+public:
+    StatsReset_Descriptor();
+
+    struct Stats;
+    virtual std::unique_ptr<StatsTracker> make_stats() const override;
+};
+
+
+
+class StatsReset : public SingleSwitchProgramInstance{
+public:
+    StatsReset();
+    virtual void program(SingleSwitchProgramEnvironment& env, ProControllerContext& context) override;
+
+private:
+    StartInGripOrGameOption START_LOCATION;
+    GoHomeWhenDoneOption GO_HOME_WHEN_DONE;
+
+    OCR::LanguageOCROption LANGUAGE;
+
+    enum class GiftPokemon{
+        FLOETTE,
+    };
+    EnumDropdownOption<GiftPokemon> POKEMON;
+
+    IVJudgeFilterOption HP;
+    IVJudgeFilterOption ATTACK;
+    IVJudgeFilterOption DEFENSE;
+    IVJudgeFilterOption SPATK;
+    IVJudgeFilterOption SPDEF;
+    IVJudgeFilterOption SPEED;
+
+    EventNotificationOption NOTIFICATION_PROGRAM_FINISH;
+    EventNotificationsOption NOTIFICATIONS;
+};
+
+
+}
+}
+}
+#endif
