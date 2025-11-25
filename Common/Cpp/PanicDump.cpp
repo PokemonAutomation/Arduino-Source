03d747c3172e864ba70c84ff46814617a92d6a01
Alexander Yee <a-yee@u.northwestern.edu>
2025-11-24 22:36:55 -0800

    Let exceptions propagate in Windows since we have crash dumps.

--- Common/Cpp/PanicDump.cpp
+++ Common/Cpp/PanicDump.cpp
@@ -32,7 +32,7 @@ void panic_dump(const char* location, const char* message){
 
 
 void run_with_catch(const char* location, std::function<void()>&& lambda){
-#if 0
+#ifdef _WIN32
     lambda();
 #else
     try{
