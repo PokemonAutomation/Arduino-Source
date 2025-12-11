0294c32f44844ba0e2021c943233b6f9cf14a171
jw098 <justin.wong098@gmail.com>
2025-12-10 18:09:07 -0800

    adjust segment 31: team star fighting

--- SerialPrograms/Source/PokemonSV/Programs/AutoStory/PokemonSV_AutoStory_Segment_31.h
+++ SerialPrograms/Source/PokemonSV/Programs/AutoStory/PokemonSV_AutoStory_Segment_31.h
@@ -58,28 +58,42 @@ public:
     virtual void run_checkpoint(SingleSwitchProgramEnvironment& env, ProControllerContext& context, AutoStoryOptions options, AutoStoryStats& stats) const override;
 };
 
+class AutoStory_Checkpoint_82 : public AutoStory_Checkpoint{
+public:
+    virtual std::string name() const override;
+    virtual std::string start_text() const override;
+    virtual std::string end_text() const override;
+    virtual void run_checkpoint(SingleSwitchProgramEnvironment& env, ProControllerContext& context, AutoStoryOptions options, AutoStoryStats& stats) const override;
+};
+
 
 
 // start: At North Province Area One Pokecenter.
-// end: At Team Star (Fighting) base. Spoke to Clavell.
+// end: At North Province Area Two Pokecenter
 void checkpoint_78(SingleSwitchProgramEnvironment& env, ProControllerContext& context, EventNotificationOption& notif_status_update, AutoStoryStats& stats);
 
-// start: At Team Star (Fighting) base. Spoke to Clavell.
-// end: Beat Team Star (Fighting) grunt. At gate of base.
+// start: At North Province Area Two Pokecenter
+// end: At North Province Area Two Pokecenter
 void checkpoint_79(SingleSwitchProgramEnvironment& env, ProControllerContext& context, EventNotificationOption& notif_status_update, AutoStoryStats& stats);
 
 
-// start: Beat Team Star (Fighting) grunt. At gate of base.
+// start: At North Province Area Two Pokecenter
 // end: Beat Team Star (Fighting)
 void checkpoint_80(SingleSwitchProgramEnvironment& env, ProControllerContext& context, EventNotificationOption& notif_status_update, AutoStoryStats& stats);
 
 // start: Beat Team Star (Fighting)
-// end: At North Province Area Two Pokecenter
+// end: At Alfornada Pokecenter.
 void checkpoint_81(SingleSwitchProgramEnvironment& env, ProControllerContext& context, EventNotificationOption& notif_status_update, AutoStoryStats& stats);
 
 
+// start: At Alfornada Pokecenter.
+// end: At Alfornada Pokecenter.
+void checkpoint_82(SingleSwitchProgramEnvironment& env, ProControllerContext& context, EventNotificationOption& notif_status_update, AutoStoryStats& stats);
+
+
+
 // moves player from North Province Area One to Team Star Fighting base
-void move_from_north_province_area_one_to_fighting_base(SingleSwitchProgramEnvironment& env, ProControllerContext& context);
+void move_from_north_province_area_one_to_north_province_area_two(SingleSwitchProgramEnvironment& env, ProControllerContext& context);
 
 
 void beat_team_star_fighting1(SingleSwitchProgramEnvironment& env, ProControllerContext& context);
@@ -87,6 +101,10 @@ void beat_team_star_fighting1(SingleSwitchProgramEnvironment& env, ProController
 void beat_team_star_fighting2(SingleSwitchProgramEnvironment& env, ProControllerContext& context);
 
 void move_from_fighting_base_to_north_province_area_two(SingleSwitchProgramEnvironment& env, ProControllerContext& context);
+
+
+void move_from_west_province_area_one_north_to_alfornada(SingleSwitchProgramEnvironment& env, ProControllerContext& context);
+
 
 
 }
