2efc51f91ffc2978d57d76757f0a57e728b6a17b
Ȇ✠Saͥbͣeͫr👑Ⰰ <naussika@users.noreply.github.com>
2025-12-16 21:44:33 +0800

    FlySpotReset: fixes moving cursor on map

--- SerialPrograms/Source/PokemonLZA/Programs/ShinyHunting/PokemonLZA_ShinyHunt_FlySpotReset.cpp
+++ SerialPrograms/Source/PokemonLZA/Programs/ShinyHunting/PokemonLZA_ShinyHunt_FlySpotReset.cpp
@@ -159,14 +159,14 @@ void route_wild_zone_19(
     bool to_zoom_to_max){
     if (run_a_straight_path_in_overworld(env.console, context, 0, 80, 6500ms) == 0) {
         open_map(env.console, context, to_zoom_to_max);
-        pbf_move_left_joystick(context, 0, 64, 100ms, 100ms);
+        pbf_move_left_joystick(context, 64, 144, 100ms, 100ms);
         if (fly_from_map(env.console, context) == FastTravelState::NOT_AT_FLY_SPOT) {
             pbf_move_left_joystick(context, 128, 192, 100ms, 100ms);
             fly_from_map(env.console, context);
         }
     } else {
         open_map(env.console, context, to_zoom_to_max);
-        pbf_move_left_joystick(context, 0, 64, 100ms, 100ms);
+        pbf_move_left_joystick(context, 64, 128, 100ms, 100ms);
         fly_from_map(env.console, context);
     }
     wait_until_overworld(env.console, context, 50s);
