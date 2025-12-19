a0ab056e793fff3533aaffe603d71f6bf020538e
jw098 <justin.wong098@gmail.com>
2025-12-18 17:27:44 -0800

    adjust routine flying betwen cities in Medali

--- SerialPrograms/Source/PokemonSV/Programs/AutoStory/PokemonSV_AutoStory_Segment_26.cpp
+++ SerialPrograms/Source/PokemonSV/Programs/AutoStory/PokemonSV_AutoStory_Segment_26.cpp
@@ -173,9 +173,11 @@ void checkpoint_62(
 
         DirectionDetector direction;
         env.console.log("Fly back to Medali East Pokecenter");
-        env.console.log("Fly to neighbouring Pokecenter, then fly back, to clear any pokemon covering the minimap. Fly via Cascaraffa north to clear minimap. End up in Medal East Pokecenter.");
+        env.console.log("Fly to neighbouring Pokecenter, then fly back, to clear any pokemon covering the minimap.");
+        env.console.log("Fly to Cascaraffa north to clear minimap. Then Medali West. End up in Medal East Pokecenter.");
         move_cursor_towards_flypoint_and_go_there(env.program_info(), env.console, context, {ZoomChange::KEEP_ZOOM, 0, 180, 100}, FlyPoint::POKECENTER);
-        move_cursor_towards_flypoint_and_go_there(env.program_info(), env.console, context, {ZoomChange::KEEP_ZOOM, 255, 80, 150});
+        move_cursor_towards_flypoint_and_go_there(env.program_info(), env.console, context, {ZoomChange::KEEP_ZOOM, 255, 80, 85});
+        move_cursor_towards_flypoint_and_go_there(env.program_info(), env.console, context, {ZoomChange::ZOOM_IN, 0, 0, 0});
 
 
         pbf_press_button(context, BUTTON_L, 50, 50);
@@ -252,9 +254,11 @@ void checkpoint_63(
 
         // fly to Medali East Pokecenter
         env.console.log("Fly back to Medali East Pokecenter");
-        env.console.log("Fly to neighbouring Pokecenter, then fly back, to clear any pokemon covering the minimap. Fly via Cascaraffa north to clear minimap. End up in Medal East Pokecenter.");
+        env.console.log("Fly to neighbouring Pokecenter, then fly back, to clear any pokemon covering the minimap.");
+        env.console.log("Fly to Cascaraffa north to clear minimap. Then Medali West. End up in Medal East Pokecenter.");
         move_cursor_towards_flypoint_and_go_there(env.program_info(), env.console, context, {ZoomChange::KEEP_ZOOM, 0, 180, 100}, FlyPoint::POKECENTER);
-        move_cursor_towards_flypoint_and_go_there(env.program_info(), env.console, context, {ZoomChange::KEEP_ZOOM, 255, 80, 150});
+        move_cursor_towards_flypoint_and_go_there(env.program_info(), env.console, context, {ZoomChange::KEEP_ZOOM, 255, 80, 85});
+        move_cursor_towards_flypoint_and_go_there(env.program_info(), env.console, context, {ZoomChange::ZOOM_IN, 0, 0, 0});
 
         
         move_from_medali_to_glaseado_mountain(env, context);
