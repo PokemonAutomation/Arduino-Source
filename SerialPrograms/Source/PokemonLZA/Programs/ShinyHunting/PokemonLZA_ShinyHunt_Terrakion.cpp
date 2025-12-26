aec3f3c09724900c2517bf8a30f0fc17cb815340
Eric Zhang <ericzklm@hotmail.com>
2025-12-26 14:10:49 -0800

    Catch and Retry
    Added game reset when failure is detected

--- SerialPrograms/Source/PokemonLZA/Programs/ShinyHunting/PokemonLZA_ShinyHunt_Terrakion.cpp
+++ SerialPrograms/Source/PokemonLZA/Programs/ShinyHunting/PokemonLZA_ShinyHunt_Terrakion.cpp
@@ -5,6 +5,7 @@
  */
 
 #include "CommonFramework/Exceptions/OperationFailedException.h"
+#include "CommonFramework/GlobalSettingsPanel.h"
 #include "CommonFramework/ProgramStats/StatsTracking.h"
 #include "CommonFramework/Notifications/ProgramNotifications.h"
 #include "Common/Cpp/PrettyPrint.h"
@@ -68,7 +69,7 @@ std::unique_ptr<StatsTracker> ShinyHunt_Terrakion_Descriptor::make_stats() const
 
 
 ShinyHunt_Terrakion::ShinyHunt_Terrakion()
-    : THRESHOLD("<b>Calorie Threshold:</b><br>Stop resetting and check Terrakion when this many calories remain", LockMode::UNLOCK_WHILE_RUNNING, 800, 200, 4800)
+    : THRESHOLD("<b>Calorie Threshold:</b><br>Stop resetting and check Terrakion when this many calories remain", LockMode::UNLOCK_WHILE_RUNNING, 1500, 200, 4800)
     , SAVE_ON_START("<b>Save on Start:</b><br>Save the game when starting the program. Each cycle will start at the calorie count at the time of the save", LockMode::LOCK_WHILE_RUNNING, true)
     , SHINY_DETECTED("Shiny Detected", "", "1000 ms", ShinySoundDetectedAction::STOP_PROGRAM)
     , NOTIFICATION_STATUS("Status Update", true, false, std::chrono::seconds(3600))
@@ -88,14 +89,14 @@ ShinyHunt_Terrakion::ShinyHunt_Terrakion()
 
 
 bool are_calories_under_threshold(SingleSwitchProgramEnvironment& env, ProControllerContext& context,
-    ShinyHunt_Terrakion_Descriptor::Stats& stats, SimpleIntegerOption<uint16_t>& THRESHOLD) {
+    ShinyHunt_Terrakion_Descriptor::Stats& stats, SimpleIntegerOption<uint16_t>& THRESHOLD){
         
     OverworldPartySelectionWatcher overworld(COLOR_WHITE, &env.console.overlay());
     int ret = wait_until(
         env.console, context, 10s,
         {overworld}
     );
-    if (ret < 0) {
+    if (ret < 0){
         stats.errors++;
         env.update_stats();
         OperationFailedException::fire(
@@ -138,7 +139,7 @@ bool are_calories_under_threshold(SingleSwitchProgramEnvironment& env, ProContro
 
 
 void detect_warp_pad(SingleSwitchProgramEnvironment& env, ProControllerContext& context,
-    ShinyHunt_Terrakion_Descriptor::Stats& stats) {
+    ShinyHunt_Terrakion_Descriptor::Stats& stats){
 
     ButtonWatcher ButtonA(
         COLOR_RED,
@@ -151,7 +152,7 @@ void detect_warp_pad(SingleSwitchProgramEnvironment& env, ProControllerContext& 
         env.console, context, 10s,
         {ButtonA}
     );
-    if (ret < 0) {
+    if (ret < 0){
         stats.errors++;
         env.update_stats();
         OperationFailedException::fire(
@@ -159,7 +160,7 @@ void detect_warp_pad(SingleSwitchProgramEnvironment& env, ProControllerContext& 
             "route_terrakion_reset(): Cannot detect warp pad after 10 seconds",
             env.console
         );
-    } else {
+    }else{
         env.log("Detected warp pad.");
         // env.console.overlay().add_log("Warp Pad Detected");
     }
@@ -193,7 +194,7 @@ void detect_warp_pad(SingleSwitchProgramEnvironment& env, ProControllerContext& 
 
 
 void route_reset_terrakion(SingleSwitchProgramEnvironment& env, ProControllerContext& context,
-    ShinyHunt_Terrakion_Descriptor::Stats& stats, bool do_rolls=true) {
+    ShinyHunt_Terrakion_Descriptor::Stats& stats, bool do_rolls=true){
 
     context.wait_for_all_requests();
 
@@ -207,7 +208,7 @@ void route_reset_terrakion(SingleSwitchProgramEnvironment& env, ProControllerCon
 
     // Roll and roll back on Terrakion's roof to respawn
     detect_warp_pad(env, context, stats);
-    if (do_rolls) {
+    if (do_rolls){
         pbf_press_button(context, BUTTON_Y, 100ms, 900ms);
         pbf_move_left_joystick(context, {0, -1}, 80ms, 160ms);
         pbf_press_button(context, BUTTON_Y, 100ms, 900ms);
@@ -221,7 +222,7 @@ void route_reset_terrakion(SingleSwitchProgramEnvironment& env, ProControllerCon
 
 
 void route_check_terrakion(SingleSwitchProgramEnvironment& env, ProControllerContext& context,
-    ShinyHunt_Terrakion_Descriptor::Stats& stats) {
+    ShinyHunt_Terrakion_Descriptor::Stats& stats){
 
     context.wait_for_all_requests();
 
@@ -244,10 +245,10 @@ void route_check_terrakion(SingleSwitchProgramEnvironment& env, ProControllerCon
     env.update_stats();
 }
 
-void ShinyHunt_Terrakion::program(SingleSwitchProgramEnvironment& env, ProControllerContext& context) {
+void ShinyHunt_Terrakion::program(SingleSwitchProgramEnvironment& env, ProControllerContext& context){
     ShinyHunt_Terrakion_Descriptor::Stats& stats = env.current_stats<ShinyHunt_Terrakion_Descriptor::Stats>();
     ShinySoundHandler shiny_sound_handler(SHINY_DETECTED);
-    PokemonLA::ShinySoundDetector shiny_detector(env.console, [&](float error_coefficient) -> bool {
+    PokemonLA::ShinySoundDetector shiny_detector(env.console, [&](float error_coefficient) -> bool{
         //  Warning: This callback will be run from a different thread than this function.
         stats.shinies++;
         env.update_stats();
@@ -259,11 +260,12 @@ void ShinyHunt_Terrakion::program(SingleSwitchProgramEnvironment& env, ProContro
         );
     });
 
-    if (SAVE_ON_START) {
+    if (SAVE_ON_START){
         save_game_to_menu(env.console, context);
         pbf_mash_button(context, BUTTON_B, 2000ms);
     }
 
+    int consecutive_failures = 0;
     run_until<ProControllerContext>(
         env.console, context,
         [&](ProControllerContext& context){
@@ -271,14 +273,36 @@ void ShinyHunt_Terrakion::program(SingleSwitchProgramEnvironment& env, ProContro
                 context.wait_for_all_requests();
                 shiny_sound_handler.process_pending(context);
 
-                bool stop_resets = are_calories_under_threshold(env, context, stats, THRESHOLD);
-                if (stop_resets) {
-                    // Reset one additional time without rolling to reset position
-                    route_reset_terrakion(env, context, stats, false);
-                    route_check_terrakion(env, context, stats);
+                try{
+                    bool stop_resets = are_calories_under_threshold(env, context, stats, THRESHOLD);
+                    if (stop_resets){
+                        // Reset one additional time without rolling to reset position
+                        route_reset_terrakion(env, context, stats, false);
+                        route_check_terrakion(env, context, stats);
+                        go_home(env.console, context);
+                        reset_game_from_home(env, env.console, context);
+                    }else{
+                        route_reset_terrakion(env, context, stats);
+                    }
+                }catch (OperationFailedException& e){
+                    consecutive_failures++;
+                    env.log("Consecutive failures: " + std::to_string(consecutive_failures), COLOR_RED);
+                    if (consecutive_failures >= 3){
+                        if (PreloadSettings::instance().DEVELOPER_MODE && GlobalSettings::instance().SAVE_DEBUG_VIDEOS_ON_SWITCH){
+                            env.log("Saving debug video on Switch...");
+                            env.console.overlay().add_log("Save Debug Video on Switch");
+                            pbf_press_button(context, BUTTON_CAPTURE, 2000ms, 0ms);
+                            context.wait_for_all_requests();
+                        }
+                        go_home(env.console, context); // go Home to preserve game state for debugging
+                        throw;
+                    }
+                    env.log("Error encountered. Resetting...", COLOR_RED);
+                    stats.errors++;
+                    env.update_stats();
+                    env.console.overlay().add_log("Error Found. Reset Game", COLOR_RED);
+                    go_home(env.console, context);
                     reset_game_from_home(env, env.console, context);
-                } else {
-                    route_reset_terrakion(env, context, stats);
                 }
                 env.update_stats();
                 if (stats.resets.load(std::memory_order_relaxed) % 100 == 0){
