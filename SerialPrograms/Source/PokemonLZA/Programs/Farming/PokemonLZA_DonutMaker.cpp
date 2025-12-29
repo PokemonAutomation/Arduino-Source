c03a1f071b0bc33990a46bf18c19fadfa91a13f9
Gin <>
2025-12-28 18:31:51 -0800

    refactor

--- SerialPrograms/Source/PokemonLZA/Programs/Farming/PokemonLZA_DonutMaker.cpp
+++ SerialPrograms/Source/PokemonLZA/Programs/Farming/PokemonLZA_DonutMaker.cpp
@@ -107,13 +107,7 @@ void DonutMaker::add_berries_in_menu_and_start(SingleSwitchProgramEnvironment& e
     //DonutMaker_Descriptor::Stats& stats = env.current_stats<DonutMaker_Descriptor::Stats>();
 
     env.log("Checking berry count.");
-    size_t num_berries = 0;
     std::vector<std::unique_ptr<DonutBerriesTableRow>> berries_table = BERRIES.copy_snapshot();
-    num_berries = berries_table.size();
-
-    if (num_berries < 3 || num_berries > 8) {
-        throw UserSetupError(env.console, "Must have at least 3 berries and no more than 8 berries.");
-    }
     env.log("Number of berries validated.", COLOR_BLACK);
 
     //Berries map for selection
@@ -188,14 +182,15 @@ void DonutMaker::open_berry_menu_from_ansha(SingleSwitchProgramEnvironment& env,
 }
 
 
-// Return true if it should stop
-// Start the iteration at closest pokemon center
-bool DonutMaker::donut_iteration(SingleSwitchProgramEnvironment& env, ProControllerContext& context){
+// Move from pokecenter to in front of Ansha with button A shown
+void move_from_pokecenter_to_ansha(SingleSwitchProgramEnvironment& env, ProControllerContext& context){
     DonutMaker_Descriptor::Stats& stats = env.current_stats<DonutMaker_Descriptor::Stats>();
 
     bool zoom_to_max = false;
     open_map(env.console, context, zoom_to_max);
-    // Move map cursor upwards a little bit
+    // Move map cursor upwards a little bit so that the cursor locks onto the pokecenter.
+    // This is needed so that in the fast travel location menu the Hotel Z is one row near
+    // the default position on the menu.
     pbf_move_left_joystick_old(context, 128, 64, 100ms, 200ms);
     // Press Y to load fast travel locaiton menu. The cursor should now points to Vert Pokemon Center
     pbf_press_button(context, BUTTON_Y, 125ms, 200ms);
@@ -207,7 +202,7 @@ bool DonutMaker::donut_iteration(SingleSwitchProgramEnvironment& env, ProControl
         env.console, context,
         [&](ProControllerContext& context){
             pbf_mash_button(context, BUTTON_A, Seconds(10));
-            pbf_wait(context, Seconds(30));
+            pbf_wait(context, Seconds(30)); // 30 sec to wait out potential day night change
         },
         {overworld}
     );
@@ -222,7 +217,8 @@ bool DonutMaker::donut_iteration(SingleSwitchProgramEnvironment& env, ProControl
     }
 
     ret = run_towards_gate_with_A_button(env.console, context, 128, 0, Seconds(5));
-    if (ret == 1){
+    if (ret == 1){  // day night change happens during running
+        // As day night change has ended, try running towards door again
         if (run_towards_gate_with_A_button(env.console, context, 128, 0, Seconds(5)) != 0){
             stats.errors++;
             env.update_stats();
@@ -259,6 +255,7 @@ bool DonutMaker::donut_iteration(SingleSwitchProgramEnvironment& env, ProControl
         wait_until_overworld(env.console, context, 50s);
     }
 
+    
     // we are now inside the hotel
 
     // Roll forward twice
@@ -266,11 +263,19 @@ bool DonutMaker::donut_iteration(SingleSwitchProgramEnvironment& env, ProControl
     pbf_press_button(context, BUTTON_Y, 100ms, 1s);
     pbf_move_left_joystick(context, {-1, 0}, 500ms, 100ms);
     context.wait_for_all_requests();
+}
 
+
+// Return true if it should stop
+// Start the iteration at closest pokemon center
+bool DonutMaker::donut_iteration(SingleSwitchProgramEnvironment& env, ProControllerContext& context){
+    DonutMaker_Descriptor::Stats& stats = env.current_stats<DonutMaker_Descriptor::Stats>();
+
+    move_from_pokecenter_to_ansha(env, context);
 
     const ImageFloatBox button_A_box{0.3, 0.2, 0.4, 0.7};
     ButtonWatcher buttonA(COLOR_RED, ButtonType::ButtonA, button_A_box, &env.console.overlay());
-    ret = wait_until(env.console, context, std::chrono::seconds(3), {buttonA});
+    int ret = wait_until(env.console, context, std::chrono::seconds(3), {buttonA});
     if (ret != 0){
         stats.errors++;
         env.update_stats();
@@ -299,6 +304,14 @@ void DonutMaker::program(SingleSwitchProgramEnvironment& env, ProControllerConte
         throw UserSetupError(env.console, "Must set game language option to read berries and flavor powers.");
     }
 
+    {
+        const std::vector<std::unique_ptr<DonutBerriesTableRow>> berries_table = BERRIES.copy_snapshot();
+        const size_t num_berries = berries_table.size();
+        if (num_berries < 3 || num_berries > 8) {
+            throw UserSetupError(env.console, "Must have at least 3 berries and no more than 8 berries.");
+        }
+    }
+
     //Print table to log to check
     std::vector<std::unique_ptr<FlavorPowerTableRow>> wanted_powers_table = FLAVOR_POWERS.copy_snapshot();
     for (const std::unique_ptr<FlavorPowerTableRow>& row : wanted_powers_table){
@@ -310,7 +323,7 @@ void DonutMaker::program(SingleSwitchProgramEnvironment& env, ProControllerConte
     //also TODO: powers table to however we're going to check it once that is written
 
     //  Mash button B to let Switch register the controller
-    pbf_mash_button(context, BUTTON_B, 500ms);
+    pbf_mash_button(context, BUTTON_B, 200ms);
 
     while(true){
         const bool should_stop = donut_iteration(env, context);
