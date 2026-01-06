41574d7d1c83d0ce0b517855626034de9c901bac
Gin <>
2026-01-05 20:36:49 -0800

    refactor

--- SerialPrograms/Source/PokemonLZA/Programs/ShinyHunting/PokemonLZA_ShinyHunt_HyperspaceHunter.cpp
+++ SerialPrograms/Source/PokemonLZA/Programs/ShinyHunting/PokemonLZA_ShinyHunt_HyperspaceHunter.cpp
@@ -142,68 +142,95 @@ void ShinyHunt_HyperspaceHunter::use_fly_spot_reset(
     ProControllerContext& context)
 {
     ShinyHunt_HyperspaceHunter_Descriptor::Stats& stats = env.current_stats<ShinyHunt_HyperspaceHunter_Descriptor::Stats>();
-    while (true){
-        const bool zoom_to_max = false;
-        const bool require_icons = false;
-        if (!open_map(env.console, context, zoom_to_max, require_icons)){
-            stats.errors++;
-            env.update_stats();
-            OperationFailedException::fire(
-                ErrorReport::SEND_ERROR_REPORT,
-                "use_fly_spot_reset(): Cannot fast travel after being chased by wild pokemon.",
-                env.console
-            );
-        }
 
-        // Fly from map to reset spawns
-        std::shared_ptr<const ImageRGB32> overworld_screen;
-        FastTravelState travel_status = fly_from_map(env.console, context, &overworld_screen);
-        if (travel_status != FastTravelState::SUCCESS){
-            stats.errors++;
-            env.update_stats();
-            OperationFailedException::fire(
-                ErrorReport::SEND_ERROR_REPORT,
-                "use_fly_spot_reset(): Cannot fast travel after moving map cursor.",
-                env.console
-            );
-        }
-
-        if (overworld_screen == nullptr){
-            throw InternalProgramError(&env.logger(), PA_CURRENT_FUNCTION, "overworld_screen is nullptr but FastTravelState is successful.");
-        }
-
-        HyperspaceCalorieDetector hyperspace_calorie_detector(env.logger());
-        if (!hyperspace_calorie_detector.detect(*overworld_screen)){
-            stats.errors++;
-            env.update_stats();
-            OperationFailedException::fire(
-                ErrorReport::SEND_ERROR_REPORT,
-                "use_fly_spot_reset(): Cannot read Calorie number on screen.",
-                env.console
-            );
-        }
-
-        const uint16_t calorie_number = hyperspace_calorie_detector.calorie_number();
-        const uint16_t min_calorie = MIN_CALORIE_REMAINING;
-        const std::string log_msg = std::format("Calorie: {}/{}", calorie_number, min_calorie);
-        env.add_overlay_log(log_msg);
-        env.log(log_msg);
-
-        stats.resets++;
+    ShinySoundHandler shiny_sound_handler(SHINY_DETECTED);
+    PokemonLA::ShinySoundDetector shiny_detector(env.console, [&](float error_coefficient) -> bool {
+        //  Warning: This callback will be run from a different thread than this function.
+        stats.shinies++;
         env.update_stats();
-        if (stats.resets % 10 == 0){
-            send_program_status_notification(env, NOTIFICATION_STATUS);
-        }
-        if (calorie_number <= min_calorie){
-            env.log("min calorie reached");
-            env.add_overlay_log("Min Calorie Reached");
-            break;
-        }
-        if (NUM_RESETS > 0 && stats.resets >= NUM_RESETS){
-            env.log(std::format("Reached reset limit {}", static_cast<uint64_t>(NUM_RESETS)));
-            break;
-        }
-    }
+        env.console.overlay().add_log("Shiny Sound Detected!", COLOR_YELLOW);
+
+        return shiny_sound_handler.on_shiny_sound(
+            env, env.console,
+            stats.shinies,
+            error_coefficient
+        );
+    });
+
+    run_until<ProControllerContext>(
+        env.console, context,
+        [&](ProControllerContext& context){
+            
+            // Fly reset loop
+            while (true){
+                const bool zoom_to_max = false;
+                const bool require_icons = false;
+                if (!open_map(env.console, context, zoom_to_max, require_icons)){
+                    stats.errors++;
+                    env.update_stats();
+                    OperationFailedException::fire(
+                        ErrorReport::SEND_ERROR_REPORT,
+                        "use_fly_spot_reset(): Cannot fast travel after being chased by wild pokemon.",
+                        env.console
+                    );
+                }
+
+                // Fly from map to reset spawns
+                std::shared_ptr<const ImageRGB32> overworld_screen;
+                FastTravelState travel_status = fly_from_map(env.console, context, &overworld_screen);
+                if (travel_status != FastTravelState::SUCCESS){
+                    stats.errors++;
+                    env.update_stats();
+                    OperationFailedException::fire(
+                        ErrorReport::SEND_ERROR_REPORT,
+                        "use_fly_spot_reset(): Cannot fast travel after moving map cursor.",
+                        env.console
+                    );
+                }
+                if (overworld_screen == nullptr){
+                    throw InternalProgramError(&env.logger(), PA_CURRENT_FUNCTION, "overworld_screen is nullptr but FastTravelState is successful.");
+                }
+
+                HyperspaceCalorieDetector hyperspace_calorie_detector(env.logger());
+                if (!hyperspace_calorie_detector.detect(*overworld_screen)){
+                    stats.errors++;
+                    env.update_stats();
+                    OperationFailedException::fire(
+                        ErrorReport::SEND_ERROR_REPORT,
+                        "use_fly_spot_reset(): Cannot read Calorie number on screen.",
+                        env.console
+                    );
+                }
+
+                const uint16_t calorie_number = hyperspace_calorie_detector.calorie_number();
+                const uint16_t min_calorie = MIN_CALORIE_REMAINING;
+                const std::string log_msg = std::format("Calorie: {}/{}", calorie_number, min_calorie);
+                env.add_overlay_log(log_msg);
+                env.log(log_msg);
+
+                stats.resets++;
+                env.update_stats();
+                if (stats.resets % 10 == 0){
+                    send_program_status_notification(env, NOTIFICATION_STATUS);
+                }
+
+                shiny_sound_handler.process_pending(context);
+
+                if (calorie_number <= min_calorie){
+                    env.log("min calorie reached");
+                    env.add_overlay_log("Min Calorie Reached");
+                    break;
+                }
+                if (NUM_RESETS > 0 && stats.resets >= NUM_RESETS){
+                    env.log(std::format("Reached reset limit {}", static_cast<uint64_t>(NUM_RESETS)));
+                    break;
+                }
+            } // end fly reset loop
+        },
+        {{shiny_detector}}
+    );
+
+    shiny_sound_handler.process_pending(context);
 }
 
 
@@ -212,57 +239,90 @@ void ShinyHunt_HyperspaceHunter::use_shuttle_run(
     SingleSwitchProgramEnvironment& env,
     ProControllerContext& context)
 {
+
     ShinyHunt_HyperspaceHunter_Descriptor::Stats& stats = env.current_stats<ShinyHunt_HyperspaceHunter_Descriptor::Stats>();
 
-    size_t num_resets = 0;
+    // game reset loop
     while(true){
-        while(true){
-            ssf_press_button(context, BUTTON_B, 0ms, 500ms, 0ms);
-            if (FORWARD_RUN_ROLL_TIME.get() == Milliseconds(0)){
-                pbf_move_left_joystick(context, {0, +1}, FORWARD_RUN_TIME, 0ms);
-            } else{
-                pbf_controller_state(context, BUTTON_NONE, DPAD_NONE, {0, +1}, {0, 0}, FORWARD_RUN_ROLL_TIME);
-                pbf_controller_state(context, BUTTON_Y, DPAD_NONE, {0, +1}, {0, 0}, FORWARD_RUN_TIME.get() - FORWARD_RUN_ROLL_TIME.get());
-            }
-            if (BACKWARD_RUN_ROLL_TIME.get() == Milliseconds(0)){
-                pbf_move_left_joystick(context, {0, -1}, BACKWARD_RUN_TIME, 0ms);
-            } else{
-                pbf_controller_state(context, BUTTON_NONE, DPAD_NONE, {0, -1}, {0, 0}, BACKWARD_RUN_ROLL_TIME);
-                pbf_controller_state(context, BUTTON_Y, DPAD_NONE, {0, -1}, {0, 0}, BACKWARD_RUN_TIME.get() - BACKWARD_RUN_ROLL_TIME.get());
-            }
-            pbf_wait(context, 200ms);
-            context.wait_for_all_requests();
-
-            num_resets++;
-            stats.resets++;
+        size_t num_resets = 0;
+        ShinySoundHandler shiny_sound_handler(SHINY_DETECTED);
+        PokemonLA::ShinySoundDetector shiny_detector(env.console, [&](float error_coefficient) -> bool {
+            //  Warning: This callback will be run from a different thread than this function.
+            stats.shinies++;
             env.update_stats();
-            if (num_resets % 10 == 0){
-                send_program_status_notification(env, NOTIFICATION_STATUS);
-            }
-            if (check_calorie(env.console, context, MIN_CALORIE_REMAINING)){
-                break;
-            }
-            if(NUM_RESETS > 0 && num_resets >= NUM_RESETS){
-                env.log(std::format("Reached reset limit {}", static_cast<uint64_t>(NUM_RESETS)));
-                break;
-            }
-        }
+            env.console.overlay().add_log("Shiny Sound Detected!", COLOR_YELLOW);
 
-        if (SHINY_CHECK_RUN_TIME.get() > Milliseconds(0)){
-            ssf_press_button(context, BUTTON_B, 0ms, 500ms, 0ms);
-            if (FORWARD_RUN_ROLL_TIME.get() == Milliseconds(0)){
-                pbf_move_left_joystick(context, {0, +1}, FORWARD_RUN_TIME.get() + SHINY_CHECK_RUN_TIME.get(), 0ms);
-            } else{
-                pbf_controller_state(context, BUTTON_NONE, DPAD_NONE, {0, +1}, {0, 0}, FORWARD_RUN_ROLL_TIME);
-                pbf_controller_state(context, BUTTON_Y, DPAD_NONE, {0, +1}, {0, 0}, FORWARD_RUN_TIME.get() + SHINY_CHECK_RUN_TIME.get() - FORWARD_RUN_ROLL_TIME.get());
-            }
-            pbf_wait(context, 1000ms); // wait one sec to listen to shiny sound
-            context.wait_for_all_requests();
+            return shiny_sound_handler.on_shiny_sound(
+                env, env.console,
+                stats.shinies,
+                error_coefficient
+            );
+        });
+
+        int ret = run_until<ProControllerContext>(
+            env.console, context,
+            [&](ProControllerContext& context){
+                // Shuttle run loop
+                while(true){
+                    ssf_press_button(context, BUTTON_B, 0ms, 500ms, 0ms);
+                    if (FORWARD_RUN_ROLL_TIME.get() == Milliseconds(0)){
+                        pbf_move_left_joystick(context, {0, +1}, FORWARD_RUN_TIME, 0ms);
+                    } else{
+                        pbf_controller_state(context, BUTTON_NONE, DPAD_NONE, {0, +1}, {0, 0}, FORWARD_RUN_ROLL_TIME);
+                        pbf_controller_state(context, BUTTON_Y, DPAD_NONE, {0, +1}, {0, 0}, FORWARD_RUN_TIME.get() - FORWARD_RUN_ROLL_TIME.get());
+                    }
+                    if (BACKWARD_RUN_ROLL_TIME.get() == Milliseconds(0)){
+                        pbf_move_left_joystick(context, {0, -1}, BACKWARD_RUN_TIME, 0ms);
+                    } else{
+                        pbf_controller_state(context, BUTTON_NONE, DPAD_NONE, {0, -1}, {0, 0}, BACKWARD_RUN_ROLL_TIME);
+                        pbf_controller_state(context, BUTTON_Y, DPAD_NONE, {0, -1}, {0, 0}, BACKWARD_RUN_TIME.get() - BACKWARD_RUN_ROLL_TIME.get());
+                    }
+                    pbf_wait(context, 200ms);
+                    context.wait_for_all_requests();
+
+                    num_resets++;
+                    stats.resets++;
+                    env.update_stats();
+                    if (num_resets % 10 == 0){
+                        send_program_status_notification(env, NOTIFICATION_STATUS);
+                    }
+                    if (check_calorie(env.console, context, MIN_CALORIE_REMAINING)){
+                        break;
+                    }
+                    if(NUM_RESETS > 0 && num_resets >= NUM_RESETS){
+                        env.log(std::format("Reached reset limit {}", static_cast<uint64_t>(NUM_RESETS)));
+                        break;
+                    }
+                } // end shuttle run
+
+                // Run forward to check shiny
+                if (SHINY_CHECK_RUN_TIME.get() > Milliseconds(0)){
+                    ssf_press_button(context, BUTTON_B, 0ms, 500ms, 0ms);
+                    if (FORWARD_RUN_ROLL_TIME.get() == Milliseconds(0)){
+                        pbf_move_left_joystick(context, {0, +1}, FORWARD_RUN_TIME.get() + SHINY_CHECK_RUN_TIME.get(), 0ms);
+                    } else{
+                        pbf_controller_state(context, BUTTON_NONE, DPAD_NONE, {0, +1}, {0, 0}, FORWARD_RUN_ROLL_TIME);
+                        const auto hold_time = FORWARD_RUN_TIME.get() + SHINY_CHECK_RUN_TIME.get() - FORWARD_RUN_ROLL_TIME.get();
+                        pbf_controller_state(context, BUTTON_Y, DPAD_NONE, {0, +1}, {0, 0}, hold_time);
+                    }
+                    // wait some time to listen to shiny sound
+                    pbf_wait(context, 2000ms);
+                    context.wait_for_all_requests();
+                }
+            },
+            {{shiny_detector}}
+        );
+
+        shiny_sound_handler.process_pending(context);
+        if (ret == 0){
+            // there is shiny sound and user requested to stop program when shiny sound:
+            return;
         }
-        // No sound detected so far. Reset game
+        
+        // Reset game
         go_home(env.console, context);
         reset_game_from_home(env, env.console, context);
-    }
+    } // end game reset loop
 }
 
 
@@ -280,38 +340,13 @@ void ShinyHunt_HyperspaceHunter::program(SingleSwitchProgramEnvironment& env, Pr
     // Mash button B to let Switch register the controller
     pbf_mash_button(context, BUTTON_B, 500ms);
 
-    ShinyHunt_HyperspaceHunter_Descriptor::Stats& stats = env.current_stats<ShinyHunt_HyperspaceHunter_Descriptor::Stats>();
-
-    ShinySoundHandler shiny_sound_handler(SHINY_DETECTED);
-
-    PokemonLA::ShinySoundDetector shiny_detector(env.console, [&](float error_coefficient) -> bool {
-        //  Warning: This callback will be run from a different thread than this function.
-        stats.shinies++;
-        env.update_stats();
-        env.console.overlay().add_log("Shiny Sound Detected!", COLOR_YELLOW);
-
-        return shiny_sound_handler.on_shiny_sound(
-            env, env.console,
-            stats.shinies,
-            error_coefficient
-        );
-    });
-
-    run_until<ProControllerContext>(
-        env.console, context,
-        [&](ProControllerContext& context){
-            // Separate logic based on hunt method
-            if (HUNT_METHOD == HuntMethod::FLY_SPOT_RESET){
-                use_fly_spot_reset(env, context);
-            } else if (HUNT_METHOD == HuntMethod::SHUTTLE_RUN){
-                use_shuttle_run(env, context);
-            }
-            shiny_sound_handler.process_pending(context);
-        },
-        {{shiny_detector}}
-    );
+    if (HUNT_METHOD == HuntMethod::FLY_SPOT_RESET){
+        use_fly_spot_reset(env, context);
+    } else if (HUNT_METHOD == HuntMethod::SHUTTLE_RUN){
+        use_shuttle_run(env, context);
+    }
+    
     send_program_finished_notification(env, NOTIFICATION_PROGRAM_FINISH);
-
     go_home(env.console, context);
 }
 
