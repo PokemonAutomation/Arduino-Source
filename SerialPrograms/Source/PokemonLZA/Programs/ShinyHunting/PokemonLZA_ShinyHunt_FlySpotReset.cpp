286cc503c8a1ea9e4ebb0f0d25366ed3eaa62324
Gin <>
2026-01-07 21:50:44 -0800

    add warning on donut requirement

--- SerialPrograms/Source/PokemonLZA/Programs/ShinyHunting/PokemonLZA_ShinyHunt_FlySpotReset.cpp
+++ SerialPrograms/Source/PokemonLZA/Programs/ShinyHunting/PokemonLZA_ShinyHunt_FlySpotReset.cpp
@@ -85,8 +85,9 @@ ShinyHunt_FlySpotReset::ShinyHunt_FlySpotReset()
     , MIN_CALORIE_REMAINING(
         "<b>Minimum Cal. allowed While Resetting in Hyperspace:</b><br>The program will stop if the Calorie number is at or below this value."
         "<br>NOTE: the more star the hyperspace has the faster Calorie burns! Pick a minimum Calorie value that gives you enough time to catch shinies."
-        "<br>Cal. per sec: 1 Star: 1 Cal./s, 2 Star: 1.6 Cal./s, 3 Star: 3.5 Cal./s, 4 Star: 7.5 Cal./s, 5 Star: 10 Cal./s"
-        "Each reset takes between 0.6-1.0 sec of the timer.",
+        "<br>Cal. per sec: 1 Star: 1 Cal./s, 2 Star: 1.6 Cal./s, 3 Star: 3.5 Cal./s, 4 Star: 7.5 Cal./s, 5 Star: 10 Cal./s. "
+        "Each reset takes between 0.6-1.0 sec of the timer."
+        "<br>Use a donut with AT LEAST ONE flavor power for the program to read Calorie number correctly.",
         LockMode::UNLOCK_WHILE_RUNNING,
         120, 0, 9999 // default, min, max
     )
