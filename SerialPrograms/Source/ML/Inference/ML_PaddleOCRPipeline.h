0c03a94b8a057c071ec6c443865eac6f457a3c0f
jw098 <justin.wong098@gmail.com>
2026-01-13 22:32:22 -0800

    change path to model. change function naming convention.

--- SerialPrograms/Source/ML/Inference/ML_PaddleOCRPipeline.h
+++ SerialPrograms/Source/ML/Inference/ML_PaddleOCRPipeline.h
@@ -28,12 +28,12 @@ public:
     PaddleOCRPipeline(Language language);
     PaddleOCRPipeline(Language language, std::string rec_path, std::string dict_path);
 
-    void Run(const std::string& img_path);
+    void run(const std::string& img_path);
 
-    std::string Recognize(const ImageViewRGB32& image);
+    std::string recognize(const ImageViewRGB32& image);
 
 private:
-    void LoadDictionary(const std::string& path);
+    void load_dictionary(const std::string& path);
 
     Ort::Env m_env;
     // Ort::Session det_session;
@@ -47,9 +47,9 @@ private:
 };
 
 // convert HCW (height, width, channels) to NCHW (batch N, channels C, height H, width W)
-std::vector<float> PreprocessNCHW(cv::Mat& img);
+std::vector<float> preprocess_NCHW(cv::Mat& img);
 
-std::string DecodeCTC(float* data, const std::vector<int64_t>& shape, const std::vector<std::string>& dict);
+std::string decode_CTC(float* data, const std::vector<int64_t>& shape, const std::vector<std::string>& dict);
 
 cv::Mat imageviewrgb32_to_cv_mat_rgb(const ImageViewRGB32& image);
 
