80f8e9bb377c569be1f89060fd0a6e7c9a97825e
Alexander Yee <a-yee@u.northwestern.edu>
2026-01-18 18:49:06 -0800

    Revert "PIMPL the file system wrapper."
    This reverts commit a2dae68c2dc88b734d02a639427e28745254a052.

--- Common/Cpp/Filesystem.h
+++ Common/Cpp/Filesystem.h
@@ -17,20 +17,13 @@
  *  Therefore, we ban the direct use of "std::filesystem" in our code base and
  *  wrap them instead.
  *
- *  Since we are wrapping "std::filesytem", we also PIMPL it to reduce
- *  compilation times.
- *
  */
 
 #ifndef PokemonAutomation_Filesystem_H
 #define PokemonAutomation_Filesystem_H
 
-#include <string>
-#include "Common/Cpp/Containers/Pimpl.h"
-
-namespace std::filesystem{
-    class path;
-}
+#include <filesystem>
+#include "Common/Cpp/Strings/Unicode.h"
 
 namespace PokemonAutomation{
 namespace Filesystem{
@@ -38,67 +31,99 @@ namespace Filesystem{
 
 class Path{
 public:
-    ~Path();
-    Path(Path&&);
-    Path& operator=(Path&&);
-    Path(const Path&);
-    Path& operator=(const Path&);
-
-
-public:
-    Path();
-    Path(std::filesystem::path path);
+    Path() = default;
+    Path(std::filesystem::path path)
+        : m_path(std::move(path))
+    {}
 
     //  Construct assuming UTF-8 encoding.
-    Path(const char* path);
-    Path(const std::string& path);
+    Path(const char* path)
+        : m_path(utf8_to_utf8(path))
+    {}
+    Path(const std::string& path)
+        : m_path(utf8_to_utf8(path))
+    {}
 
     //  Construct from UTF-8.
-    Path(std::u8string path);
+    Path(std::u8string path)
+        : m_path(std::move(path))
+    {}
 
-    void clear();
+    void clear(){
+        m_path.clear();
+    }
 
 
 public:
     //  Convert to the C++ path so it can be passed to other library functions.
-    operator const std::filesystem::path&() const;
-    const std::filesystem::path& stdpath() const;
+    operator const std::filesystem::path&() const{
+        return m_path;
+    }
+    const std::filesystem::path& stdpath() const{
+        return m_path;
+    }
 
 
 public:
     //  Return path string as UTF-8.
-    std::string string() const;
-    std::u8string u8string() const;
+    std::string string() const{
+        return utf8_to_str(m_path.u8string());
+    }
+    std::u8string u8string() const{
+        return m_path.u8string();
+    }
 
-    Path filename() const;
-    Path parent_path() const;
-    Path stem() const;
+    Path filename() const{
+        return m_path.filename();
+    }
+    Path parent_path() const{
+        return m_path.parent_path();
+    }
+    Path stem() const{
+        return m_path.stem();
+    }
 
 
 public:
-    Path& replace_extension(const Path& replacement);
+    Path& replace_extension(const Path& replacement){
+        m_path.replace_extension(replacement);
+        return *this;
+    }
 
 
 public:
-    friend bool operator==(const Path& x, const Path& y);
+    friend bool operator==(const Path& x, const Path& y){
+        return x.m_path == y.m_path;
+    }
 
 
 public:
-    friend Path operator/(const Path& x, const Path& y);
-    friend std::ostream& operator<<(std::ostream& stream, const Path& x);
+    friend Path operator/(const Path& x, const Path& y){
+        return x.m_path / y.m_path;
+    }
+    friend std::ostream& operator<<(std::ostream& stream, const Path& x){
+        return stream << x.m_path;
+    }
 
 
 private:
-    struct Data;
-    Pimpl<Data> m_data;
+    std::filesystem::path m_path;
 };
 
 
 
-bool exists(const Path& path);
-bool create_directories(const Path& path);
-uintmax_t remove_all(const Path& path);
-bool copy_file(const Path& from, const Path& to);
+inline bool exists(const Path& path){
+    return std::filesystem::exists(path.stdpath());
+}
+inline bool create_directories(const Path& path){
+    return std::filesystem::create_directories(path.stdpath());
+}
+inline auto remove_all(const Path& path){
+    return std::filesystem::remove_all(path.stdpath());
+}
+inline bool copy_file(const Path& from, const Path& to){
+    return std::filesystem::copy_file(from.stdpath(), to.stdpath());
+}
 
 
 
