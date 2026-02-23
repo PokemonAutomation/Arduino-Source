2f9b6577319fcfccf847ce2f0f9159d7d4420edb
kichithewolf <kichithewolf@gmail.com>
2026-02-23 15:36:30 -0500

    battle dialog detectors

--- SerialPrograms/Source/PokemonFRLG/Inference/Dialogs/PokemonFRLG_DialogDetector.cpp
+++ SerialPrograms/Source/PokemonFRLG/Inference/Dialogs/PokemonFRLG_DialogDetector.cpp
@@ -120,6 +120,101 @@ bool SelectionDialogDetector::detect(const ImageViewRGB32& screen){
     return false;
 }
 
+BattleDialogDetector::BattleDialogDetector(Color color)
+    : m_dialog_top_box(0.152, 0.721, 0.694, 0.008)
+    , m_dialog_right_box(0.842, 0.730, 0.004, 0.171)
+{}
+void BattleDialogDetector::make_overlays(VideoOverlaySet& items) const{
+    items.add(COLOR_RED, m_dialog_top_box);
+    items.add(COLOR_RED, m_dialog_right_box);
+}
+bool BattleDialogDetector::detect(const ImageViewRGB32& screen){
+    //Background dialog is teal
+    ImageViewRGB32 dialog_top_image = extract_box_reference(screen, m_dialog_top_box);
+    ImageViewRGB32 dialog_right_image = extract_box_reference(screen, m_dialog_right_box);
+
+    if (is_solid(dialog_top_image, { 0.176, 0.357, 0.467 })
+        && is_solid(dialog_right_image, { 0.176, 0.357, 0.467 })
+        ){
+        return true;
+    }
+    return false;
+}
+
+
+BattleMenuDetector::BattleMenuDetector(Color color)
+    : m_menu_top_box(0.522, 0.716, 0.331, 0.011)
+    , m_menu_right_box(0.845, 0.727, 0.008, 0.175)
+    , m_dialog_top_box(0.153, 0.720, 0.343, 0.014) //top of the white dialog box
+    , m_dialog_right_box(0.486, 0.722, 0.009, 0.177) //right side, closest to the menu
+{}
+void BattleMenuDetector::make_overlays(VideoOverlaySet& items) const{
+    items.add(COLOR_RED, m_menu_top_box);
+    items.add(COLOR_RED, m_menu_right_box);
+    items.add(COLOR_RED, m_dialog_top_box);
+    items.add(COLOR_RED, m_dialog_right_box);
+}
+bool BattleMenuDetector::detect(const ImageViewRGB32& screen){
+    //Menu is white
+    ImageViewRGB32 menu_top_image = extract_box_reference(screen, m_menu_top_box);
+    ImageViewRGB32 menu_right_image = extract_box_reference(screen, m_menu_right_box);
+
+    //Background dialog is teal
+    ImageViewRGB32 dialog_top_image = extract_box_reference(screen, m_dialog_top_box);
+    ImageViewRGB32 dialog_right_image = extract_box_reference(screen, m_dialog_right_box);
+
+    if (is_solid(menu_top_image, { 0.335, 0.332, 0.335 }) //253, 251, 254 white
+        && is_solid(menu_right_image, { 0.335, 0.332, 0.335 })
+        && is_solid(dialog_top_image, { 0.176, 0.357, 0.467 }) //40, 81, 106 teal
+        && is_solid(dialog_right_image, { 0.176, 0.357, 0.467 })
+        ){
+        return true;
+    }
+    return false;
+}
+
+
+AdvanceBattleDialogDetector::AdvanceBattleDialogDetector(Color color)
+    : m_dialog_box(0.152, 0.721, 0.694, 0.177)
+    , m_dialog_top_box(0.152, 0.721, 0.694, 0.008)
+    , m_dialog_right_box(0.842, 0.730, 0.004, 0.171)
+{}
+void AdvanceBattleDialogDetector::make_overlays(VideoOverlaySet& items) const{
+    items.add(COLOR_RED, m_dialog_box);
+    items.add(COLOR_RED, m_dialog_top_box);
+    items.add(COLOR_RED, m_dialog_right_box);
+}
+bool AdvanceBattleDialogDetector::detect(const ImageViewRGB32& screen){
+    const bool replace_color_within_range = false;
+
+    //Filter out background
+    ImageRGB32 filtered_region = filter_rgb32_range(
+        extract_box_reference(screen, m_dialog_box),
+        combine_rgb(185, 0, 1), combine_rgb(255, 32, 33), Color(0), replace_color_within_range
+    );
+    ImageStats stats = image_stats(filtered_region);
+
+    /*
+    filtered_region.save("./filtered_only.png");
+    cout << stats.average.r << endl;
+    cout << stats.average.g << endl;
+    cout << stats.average.b << endl;
+    */
+
+    ImageViewRGB32 dialog_top_image = extract_box_reference(screen, m_dialog_top_box);
+    ImageViewRGB32 dialog_right_image = extract_box_reference(screen, m_dialog_right_box);
+
+    if (is_solid(dialog_top_image, { 0.176, 0.357, 0.467 })
+        && is_solid(dialog_right_image, { 0.176, 0.357, 0.467 })
+        && (stats.average.r > stats.average.b + 200)
+        && (stats.average.r > stats.average.g + 200)
+        )
+    {
+        return true;
+    }
+    return false;
+}
+
 
 }
 }
--- SerialPrograms/Source/PokemonFRLG/Inference/Dialogs/PokemonFRLG_DialogDetector.h
+++ SerialPrograms/Source/PokemonFRLG/Inference/Dialogs/PokemonFRLG_DialogDetector.h
@@ -95,6 +95,72 @@ public:
     {}
 };
 
+// Battle dialog boxes are teal, similar to r/s/e
+// We look in multiple places for this solid color
+// doing so checks that the battle menu is not up
+class BattleDialogDetector : public StaticScreenDetector{
+public:
+    BattleDialogDetector(Color color);
+
+    virtual void make_overlays(VideoOverlaySet& items) const override;
+    virtual bool detect(const ImageViewRGB32& screen) override;
+
+private:
+    ImageFloatBox m_dialog_top_box;
+    ImageFloatBox m_dialog_right_box;
+};
+class BattleDialogWatcher : public DetectorToFinder<BattleDialogDetector>{
+public:
+    BattleDialogWatcher(Color color)
+        : DetectorToFinder("BattleDialogWatcher", std::chrono::milliseconds(250), color)
+    {}
+};
+
+
+// Battle menu is a white box on the right with FIGHT/POKEMON/BAG/RUN
+// The dialog box with "what will POKEMON do?" is dark teal/navy
+class BattleMenuDetector : public StaticScreenDetector{
+public:
+    BattleMenuDetector(Color color);
+
+    virtual void make_overlays(VideoOverlaySet& items) const override;
+    virtual bool detect(const ImageViewRGB32& screen) override;
+
+private:
+    ImageFloatBox m_menu_top_box;
+    ImageFloatBox m_menu_right_box;
+    ImageFloatBox m_dialog_top_box;
+    ImageFloatBox m_dialog_right_box;
+};
+class BattleMenuWatcher : public DetectorToFinder<BattleMenuDetector>{
+public:
+    BattleMenuWatcher(Color color)
+        : DetectorToFinder("BattleMenuWatcher", std::chrono::milliseconds(250), color)
+    {}
+};
+
+
+// Detect the red advancement arrow by filtering for red.
+// No red colored text in battle?
+// reuse the battledialog checks for the top and right
+class AdvanceBattleDialogDetector : public StaticScreenDetector{
+public:
+    AdvanceBattleDialogDetector(Color color);
+
+    virtual void make_overlays(VideoOverlaySet& items) const override;
+    virtual bool detect(const ImageViewRGB32& screen) override;
+
+private:
+    ImageFloatBox m_dialog_box;
+    ImageFloatBox m_dialog_top_box;
+    ImageFloatBox m_dialog_right_box;
+};
+class AdvanceBattleDialogWatcher : public DetectorToFinder<AdvanceBattleDialogDetector>{
+public:
+    AdvanceBattleDialogWatcher(Color color)
+        : DetectorToFinder("AdvanceBattleDialogWatcher", std::chrono::milliseconds(250), color)
+    {}
+};
 
 
 
