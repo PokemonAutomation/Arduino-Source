5303e533e9f12a209175643eb65f801cf9ab1e47
jw098 <justin.wong098@gmail.com>
2026-02-28 15:40:56 -0800

    update is_safe()

--- SerialPrograms/Source/NintendoSwitch/DevPrograms/TestProgramComputer.cpp
+++ SerialPrograms/Source/NintendoSwitch/DevPrograms/TestProgramComputer.cpp
@@ -375,7 +375,9 @@ void TestProgramComputer::program(ProgramEnvironment& env, CancellableScope& sco
 
 
 #if 1
-    unzip_file(SETTINGS_PATH() + "test2.zip", SETTINGS_PATH() + "testB/");
+    std::string zip_path = SETTINGS_PATH() + "test2.zip";
+    std::string target_dir = SETTINGS_PATH() + "testB/";
+    unzip_file(zip_path.c_str(), target_dir.c_str());
 
 #endif
 
