d40808b64dfd26c245c8df77f84022e3d13d2e3c
Alexander Yee <a-yee@u.northwestern.edu>
2026-03-05 01:58:20 -0800

    Update box draw to support cropping.

--- SerialPrograms/Source/NintendoSwitch/DevPrograms/BoxDraw.cpp
+++ SerialPrograms/Source/NintendoSwitch/DevPrograms/BoxDraw.cpp
@@ -10,12 +10,96 @@
 #include "Common/Cpp/Strings/StringTools.h"
 #include "BoxDraw.h"
 
-// #include <iostream>
-// using std::cout;
-// using std::endl;
+//#include <iostream>
+//using std::cout;
+//using std::endl;
 
 namespace PokemonAutomation{
 namespace NintendoSwitch{
+
+
+
+BoxDrawGroup::~BoxDrawGroup(){
+    BOX_COORDINATES.remove_listener(*this);
+    HEIGHT.remove_listener(*this);
+    WIDTH.remove_listener(*this);
+    Y.remove_listener(*this);
+    X.remove_listener(*this);
+}
+BoxDrawGroup::BoxDrawGroup(
+    std::string label,
+    LockMode lock_while_program_is_running,
+    EnableMode enable_mode,
+    bool show_restore_defaults_button,
+    ImageFloatBox box
+)
+    : GroupOption(
+        std::move(label),
+        lock_while_program_is_running,
+        enable_mode,
+        show_restore_defaults_button
+    )
+    , X("<b>X Coordinate:</b>", LockMode::UNLOCK_WHILE_RUNNING, box.x, 0.0, 1.0)
+    , Y("<b>Y Coordinate:</b>", LockMode::UNLOCK_WHILE_RUNNING, box.y, 0.0, 1.0)
+    , WIDTH("<b>Width:</b>", LockMode::UNLOCK_WHILE_RUNNING, box.width, 0.0, 1.0)
+    , HEIGHT("<b>Height:</b>", LockMode::UNLOCK_WHILE_RUNNING, box.height, 0.0, 1.0)
+    , BOX_COORDINATES(
+        false,
+        "ImageFloatBox coordinates",
+        LockMode::UNLOCK_WHILE_RUNNING,
+        make_full_str(),
+        "0.3, 0.3, 0.4, 0.4"
+    )
+{
+    PA_ADD_OPTION(X);
+    PA_ADD_OPTION(Y);
+    PA_ADD_OPTION(WIDTH);
+    PA_ADD_OPTION(HEIGHT);
+    PA_ADD_OPTION(BOX_COORDINATES);
+
+    BoxDrawGroup::on_config_value_changed(&X);
+    X.add_listener(*this);
+    Y.add_listener(*this);
+    WIDTH.add_listener(*this);
+    HEIGHT.add_listener(*this);
+    BOX_COORDINATES.add_listener(*this);
+}
+
+void BoxDrawGroup::on_config_value_changed(void* object){
+    if (object == &X || object == &Y || object == &WIDTH || object == &HEIGHT){
+        BOX_COORDINATES.set(make_full_str());
+        report_value_changed(this);
+    }else if(object == &BOX_COORDINATES){
+        std::string box_coord_string = BOX_COORDINATES;
+        std::vector<std::string> all_coords = StringTools::split(box_coord_string, ", ");
+
+        std::string x_string = all_coords[0];
+        std::string y_string = all_coords[1];
+        std::string width_string = all_coords[2];
+        std::string height_string = all_coords[3];
+
+        double x_coord = std::stod(x_string);
+        double y_coord = std::stod(y_string);
+        double width_coord = std::stod(width_string);
+        double height_coord = std::stod(height_string);
+
+        // cout << box_coord_string << endl;
+        // cout << std::to_string(x_coord) << endl;
+        // cout << std::to_string(y_coord) << endl;
+        // cout << std::to_string(width_coord) << endl;
+        // cout << std::to_string(height_coord) << endl;
+
+        X.set(x_coord);
+        Y.set(y_coord);
+        WIDTH.set(width_coord);
+        HEIGHT.set(height_coord);
+    }else{
+        report_value_changed(object);
+    }
+}
+
+
+
 
 
 
@@ -32,17 +116,22 @@ BoxDraw_Descriptor::BoxDraw_Descriptor()
 {}
 
 BoxDraw::BoxDraw()
-    : X("<b>X Coordinate:</b>", LockMode::UNLOCK_WHILE_RUNNING, 0.3, 0.0, 1.0)
-    , Y("<b>Y Coordinate:</b>", LockMode::UNLOCK_WHILE_RUNNING, 0.3, 0.0, 1.0)
-    , WIDTH("<b>Width:</b>", LockMode::UNLOCK_WHILE_RUNNING, 0.4, 0.0, 1.0)
-    , HEIGHT("<b>Height:</b>", LockMode::UNLOCK_WHILE_RUNNING, 0.4, 0.0, 1.0)
-    , BOX_COORDINATES(false, "ImageFloatBox coordinates", LockMode::UNLOCK_WHILE_RUNNING, "0.3, 0.3, 0.4, 0.4", "0.3, 0.3, 0.4, 0.4")
+    : INFERENCE_BOX(
+        "Inference Box",
+        LockMode::UNLOCK_WHILE_RUNNING,
+        GroupOption::EnableMode::ALWAYS_ENABLED,
+        false
+    )
+    , CONTENT_BOX(
+        "Content Box",
+        LockMode::UNLOCK_WHILE_RUNNING,
+        GroupOption::EnableMode::DEFAULT_DISABLED,
+        true,
+        {0.1, 0.1, .8, .8}
+    )
 {
-    PA_ADD_OPTION(X);
-    PA_ADD_OPTION(Y);
-    PA_ADD_OPTION(WIDTH);
-    PA_ADD_OPTION(HEIGHT);
-    PA_ADD_OPTION(BOX_COORDINATES);
+    PA_ADD_OPTION(INFERENCE_BOX);
+    PA_ADD_OPTION(CONTENT_BOX);
 }
 
 class BoxDraw::DrawnBox : public ConfigOption::Listener, public VideoOverlay::MouseListener{
@@ -58,11 +147,8 @@ public:
         // DrawnBox listens to changes in the config option (X, Y, WIDTH, HEIGHT)
         // and mouse events on the video overlay layer.
         try{
-            m_parent.X.add_listener(*this);
-            m_parent.Y.add_listener(*this);
-            m_parent.WIDTH.add_listener(*this);
-            m_parent.HEIGHT.add_listener(*this);
-            m_parent.BOX_COORDINATES.add_listener(*this);
+            m_parent.INFERENCE_BOX.add_listener(*this);
+            m_parent.CONTENT_BOX.add_listener(*this);
             overlay.add_mouse_listener(*this);
         }catch (...){
             detach();
@@ -73,17 +159,28 @@ public:
         {
             std::lock_guard<Mutex> lg(m_lock);
             m_overlay_set.clear();
-            m_overlay_set.add(COLOR_RED, {m_parent.X, m_parent.Y, m_parent.WIDTH, m_parent.HEIGHT});
+            double ix = m_parent.INFERENCE_BOX.X;
+            double iy = m_parent.INFERENCE_BOX.Y;
+            double iw = m_parent.INFERENCE_BOX.WIDTH;
+            double ih = m_parent.INFERENCE_BOX.HEIGHT;
+            if (m_parent.CONTENT_BOX.enabled()){
+                double cx = m_parent.CONTENT_BOX.X;
+                double cy = m_parent.CONTENT_BOX.Y;
+                double cw = m_parent.CONTENT_BOX.WIDTH;
+                double ch = m_parent.CONTENT_BOX.HEIGHT;
+                m_overlay_set.add(COLOR_GREEN, {cx, cy, cw, ch});
+                ix *= cw;
+                iy *= ch;
+                iw *= cw;
+                ih *= ch;
+                ix += cx;
+                iy += cy;
+            }
+            m_overlay_set.add(COLOR_RED, {ix, iy, iw, ih});
         }
-        
-        if (object == &m_parent.X || object == &m_parent.Y || object == &m_parent.WIDTH || object == &m_parent.HEIGHT){
-            m_parent.update_box_coordinates();
-        }
-        else if(object == &m_parent.BOX_COORDINATES){
-            m_parent.update_individual_coordinates();    
-        }
-
-        
+//        if (object != &m_parent.INFERENCE_BOX){
+//            m_parent.INFERENCE_BOX.on_config_value_changed(object);
+//        }
     }
     virtual void on_mouse_press(double x, double y) override{
         // m_parent.WIDTH.set(0);
@@ -107,6 +204,29 @@ public:
         double yl = m_mouse_start->second;
         double yh = y;
 
+        if (m_parent.CONTENT_BOX.enabled()){
+            double cx = m_parent.CONTENT_BOX.X;
+            double cy = m_parent.CONTENT_BOX.Y;
+            double cw = m_parent.CONTENT_BOX.WIDTH;
+            double ch = m_parent.CONTENT_BOX.HEIGHT;
+
+            xl = (xl - cx) / cw;
+            xl = std::max(xl, 0.);
+            xl = std::min(xl, 1.);
+
+            yl = (yl - cy) / ch;
+            yl = std::max(yl, 0.);
+            yl = std::min(yl, 1.);
+
+            xh = (xh - cx) / cw;
+            xh = std::max(xh, 0.);
+            xh = std::min(xh, 1.);
+
+            yh = (yh - cy) / ch;
+            yh = std::max(yh, 0.);
+            yh = std::min(yh, 1.);
+        }
+
         if (xl > xh){
             std::swap(xl, xh);
         }
@@ -114,21 +234,18 @@ public:
             std::swap(yl, yh);
         }
 
-        m_parent.X.set(xl);
-        m_parent.Y.set(yl);
-        m_parent.WIDTH.set(xh - xl);
-        m_parent.HEIGHT.set(yh - yl);
+        m_parent.INFERENCE_BOX.X.set(xl);
+        m_parent.INFERENCE_BOX.Y.set(yl);
+        m_parent.INFERENCE_BOX.WIDTH.set(xh - xl);
+        m_parent.INFERENCE_BOX.HEIGHT.set(yh - yl);
         // m_parent.update_box_coordinates();
     }
 
 private:
     void detach(){
         m_overlay.remove_mouse_listener(*this);
-        m_parent.X.remove_listener(*this);
-        m_parent.Y.remove_listener(*this);
-        m_parent.WIDTH.remove_listener(*this);
-        m_parent.HEIGHT.remove_listener(*this);
-        m_parent.BOX_COORDINATES.remove_listener(*this);
+        m_parent.CONTENT_BOX.remove_listener(*this);
+        m_parent.INFERENCE_BOX.remove_listener(*this);
     }
 
 private:
@@ -140,41 +257,10 @@ private:
     std::optional<std::pair<double, double>> m_mouse_start;
 };
 
-void BoxDraw::update_box_coordinates(){
-    std::string box_coord_string = std::to_string(X) + ", " + std::to_string(Y) + ", " + std::to_string(WIDTH) + ", " + std::to_string(HEIGHT);
-    BOX_COORDINATES.set(box_coord_string);
-}
 
-
-
-void BoxDraw::update_individual_coordinates(){
-    std::string box_coord_string = BOX_COORDINATES;
-    std::vector<std::string> all_coords = StringTools::split(box_coord_string, ", ");
-
-    std::string x_string = all_coords[0];
-    std::string y_string = all_coords[1];
-    std::string width_string = all_coords[2];
-    std::string height_string = all_coords[3];
-
-    double x_coord = std::stod(x_string);
-    double y_coord = std::stod(y_string);
-    double width_coord = std::stod(width_string);
-    double height_coord = std::stod(height_string);
-
-    // cout << box_coord_string << endl;
-    // cout << std::to_string(x_coord) << endl;
-    // cout << std::to_string(y_coord) << endl;
-    // cout << std::to_string(width_coord) << endl;
-    // cout << std::to_string(height_coord) << endl;
-
-    X.set(x_coord);
-    Y.set(y_coord);
-    WIDTH.set(width_coord);
-    HEIGHT.set(height_coord);
-}
 
 void BoxDraw::program(SingleSwitchProgramEnvironment& env, ProControllerContext& context){
-    update_individual_coordinates();
+//    update_individual_coordinates();
     DrawnBox drawn_box(*this, env.console.overlay());
     drawn_box.on_config_value_changed(this);
     context.wait_until_cancel();
--- SerialPrograms/Source/NintendoSwitch/DevPrograms/BoxDraw.h
+++ SerialPrograms/Source/NintendoSwitch/DevPrograms/BoxDraw.h
@@ -9,10 +9,43 @@
 
 #include "Common/Cpp/Options/FloatingPointOption.h"
 #include "Common/Cpp/Options/StringOption.h"
+#include "Common/Cpp/Options/GroupOption.h"
+#include "CommonFramework/ImageTools/ImageBoxes.h"
 #include "NintendoSwitch/NintendoSwitch_SingleSwitchProgram.h"
 
 namespace PokemonAutomation{
 namespace NintendoSwitch{
+
+
+class BoxDrawGroup : public GroupOption, public ConfigOption::Listener{
+public:
+    ~BoxDrawGroup();
+    BoxDrawGroup(
+        std::string label,
+        LockMode lock_while_program_is_running,
+        EnableMode enable_mode = EnableMode::ALWAYS_ENABLED,
+        bool show_restore_defaults_button = false,
+        ImageFloatBox box = {0.3, 0.3, 0.4, 0.4}
+    );
+
+    virtual void on_config_value_changed(void* object) override;
+
+private:
+    std::string make_full_str() const{
+        return
+            std::to_string(X) + ", " +
+            std::to_string(Y) + ", " +
+            std::to_string(WIDTH) + ", " +
+            std::to_string(HEIGHT);
+    }
+
+public:
+    FloatingPointOption X;
+    FloatingPointOption Y;
+    FloatingPointOption WIDTH;
+    FloatingPointOption HEIGHT;
+    StringOption BOX_COORDINATES;
+};
 
 
 
@@ -27,20 +60,14 @@ class BoxDraw : public SingleSwitchProgramInstance{
 public:
     BoxDraw();
 
-    void update_box_coordinates();
-    void update_individual_coordinates();
-
     virtual void program(SingleSwitchProgramEnvironment& env, ProControllerContext& context) override;
 
 private:
     class DrawnBox;
 
 private:
-    FloatingPointOption X;
-    FloatingPointOption Y;
-    FloatingPointOption WIDTH;
-    FloatingPointOption HEIGHT;
-    StringOption BOX_COORDINATES;
+    BoxDrawGroup INFERENCE_BOX;
+    BoxDrawGroup CONTENT_BOX;
 };
 
 
