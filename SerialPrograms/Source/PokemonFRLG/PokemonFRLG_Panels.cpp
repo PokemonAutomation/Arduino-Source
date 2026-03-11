e031d895d02b9051dbd2bd371c56a2b6636bd22c
miniumknight <32173804+miniumknight@users.noreply.github.com>
2026-03-11 10:06:33 +0000

    Disabled DEV mode FRLG panels

--- SerialPrograms/Source/PokemonFRLG/PokemonFRLG_Panels.cpp
+++ SerialPrograms/Source/PokemonFRLG/PokemonFRLG_Panels.cpp
@@ -4,9 +4,9 @@
  *
  */
 
-#include "PokemonFRLG_Panels.h"
 #include "CommonFramework/GlobalSettingsPanel.h"
 #include "Pokemon/Pokemon_Strings.h"
+#include "PokemonFRLG_Panels.h"
 
 #include "PokemonFRLG_Settings.h"
 
@@ -16,54 +16,55 @@
 #include "Programs/ShinyHunting/PokemonFRLG_LegendaryRunAway.h"
 #include "Programs/ShinyHunting/PokemonFRLG_PrizeCornerReset.h"
 #include "Programs/ShinyHunting/PokemonFRLG_ShinyHunt-Overworld.h"
-#include "Programs/TestPrograms/PokemonFRLG_ReadStats.h"
 #include "Programs/TestPrograms/PokemonFRLG_SoundListener.h"
+#include "Programs/TestPrograms/PokemonFRLG_ReadStats.h"
 
 namespace PokemonAutomation {
-namespace NintendoSwitch {
-namespace PokemonFRLG {
-
-PanelListFactory::PanelListFactory()
-    : PanelListDescriptor(Pokemon::STRING_POKEMON + " FireRed and LeafGreen") {}
-
-std::vector<PanelEntry> PanelListFactory::make_panels() const {
-  std::vector<PanelEntry> ret;
-
-  ret.emplace_back("---- Settings ----");
-  ret.emplace_back(make_settings<GameSettings_Descriptor, GameSettingsPanel>());
-
-  ret.emplace_back("---- Farming ----");
-  if (PreloadSettings::instance().DEVELOPER_MODE) {
-      ret.emplace_back(make_single_switch_program<NuggetBridgeFarmer_Descriptor, NuggetBridgeFarmer>());
-  }
-
-  // ret.emplace_back("---- General ----");
-
-  ret.emplace_back("---- Shiny Hunting  ----");
-  ret.emplace_back(
-      make_single_switch_program<GiftReset_Descriptor, GiftReset>());
-  ret.emplace_back(
-      make_single_switch_program<LegendaryReset_Descriptor, LegendaryReset>());
-  ret.emplace_back(make_single_switch_program<ShinyHuntOverworld_Descriptor,
-                                              ShinyHuntOverworld>());
-  if (PreloadSettings::instance().DEVELOPER_MODE) {
-    ret.emplace_back(make_single_switch_program<LegendaryRunAway_Descriptor,
-                                                LegendaryRunAway>());
-    ret.emplace_back(make_single_switch_program<PrizeCornerReset_Descriptor,
-                                                PrizeCornerReset>());
-  }
+    namespace NintendoSwitch {
+        namespace PokemonFRLG {
 
 
-    ret.emplace_back("---- Developer Tools ----");
-    ret.emplace_back(
-        make_single_switch_program<SoundListener_Descriptor, SoundListener>());
-    ret.emplace_back(
-        make_single_switch_program<ReadStats_Descriptor, ReadStats>());
-  
 
-  return ret;
+            PanelListFactory::PanelListFactory()
+                : PanelListDescriptor(Pokemon::STRING_POKEMON + " FireRed and LeafGreen")
+            {
+            }
+
+            std::vector<PanelEntry> PanelListFactory::make_panels() const {
+                std::vector<PanelEntry> ret;
+
+                ret.emplace_back("---- Settings ----");
+                ret.emplace_back(make_settings<GameSettings_Descriptor, GameSettingsPanel>());
+
+                ret.emplace_back("---- Farming ----");
+                if (PreloadSettings::instance().DEVELOPER_MODE) {
+                    ret.emplace_back(make_single_switch_program<NuggetBridgeFarmer_Descriptor, NuggetBridgeFarmer>());
+                }
+
+                //ret.emplace_back("---- General ----");
+
+                ret.emplace_back("---- Shiny Hunting  ----");
+                ret.emplace_back(make_single_switch_program<GiftReset_Descriptor, GiftReset>());
+                ret.emplace_back(make_single_switch_program<LegendaryReset_Descriptor, LegendaryReset>());
+                ret.emplace_back(make_single_switch_program<ShinyHuntOverworld_Descriptor, ShinyHuntOverworld>());
+                if (PreloadSettings::instance().DEVELOPER_MODE) {
+                    ret.emplace_back(make_single_switch_program<LegendaryRunAway_Descriptor, LegendaryRunAway>());
+                    ret.emplace_back(make_single_switch_program<PrizeCornerReset_Descriptor, PrizeCornerReset>());
+                }
+
+
+                if (PreloadSettings::instance().DEVELOPER_MODE) {
+                    ret.emplace_back("---- Developer Tools ----");
+                    ret.emplace_back(make_single_switch_program<SoundListener_Descriptor, SoundListener>());
+                    ret.emplace_back(make_single_switch_program<ReadStats_Descriptor, ReadStats>());
+                }
+
+                return ret;
+            }
+
+
+
+
+        }
+    }
 }
-
-} // namespace PokemonFRLG
-} // namespace NintendoSwitch
-} // namespace PokemonAutomation
