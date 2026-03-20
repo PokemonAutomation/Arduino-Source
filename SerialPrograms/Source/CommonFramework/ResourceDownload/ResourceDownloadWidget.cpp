767d1485d8f384c00071a43b0dcfa489481e3a17
jw098 <justin.wong098@gmail.com>
2026-03-20 13:03:38 -0700

    clicking Download button disables it until action is done

--- SerialPrograms/Source/CommonFramework/ResourceDownload/ResourceDownloadWidget.cpp
+++ SerialPrograms/Source/CommonFramework/ResourceDownload/ResourceDownloadWidget.cpp
@@ -5,8 +5,11 @@
  */
 
 #include <QFont>
+#include <QObject>
 #include <QPushButton>
+#include <QPointer>
 #include "CommonFramework/Logging/Logger.h"
+
 #include "CommonFramework/Notifications/ProgramNotifications.h"
 #include "ResourceDownloadWidget.h"
 
@@ -19,9 +22,12 @@ namespace PokemonAutomation{
 
 template class RegisterConfigWidget<DownloadButtonWidget>;
 
-
+DownloadButtonWidget::~DownloadButtonWidget(){
+}
 DownloadButtonWidget::DownloadButtonWidget(QWidget& parent, ResourceDownloadButton& value)
-    : ConfigWidget(value)
+    : QWidget(&parent)
+    , ConfigWidget(value)
+    , m_value(value)
 {
     QPushButton* button = new QPushButton(&parent);
     m_widget = button;
@@ -29,15 +35,42 @@ DownloadButtonWidget::DownloadButtonWidget(QWidget& parent, ResourceDownloadButt
     QFont font;
     font.setBold(true);
     button->setFont(font);
-    button->setText("Download");
 
-    button->connect(
+    // Button should be disabled when in the middle of downloading
+    // this status is stored within ResourceDownloadButton::m_enabled
+    // when the button is clicked, m_enabled is set to false
+    // when te download is done, m_enabled is set back to true
+    // the UI is updated to reflect the status of m_enabled, by using update_enabled_status
+    auto update_enabled_status = [this, button](){
+        if (m_value.get_enabled()){
+            button->setEnabled(true);
+            button->setText("Download");
+        }else{
+            button->setEnabled(false);
+            button->setText("Downloading...");
+        }
+    };
+
+    // update the UI based on m_enabled when the button is constructed
+    update_enabled_status();
+
+    // when the button is clicked, m_enabled is set to false, the UI is updated, and we run_download()
+    connect(
         button, &QPushButton::clicked,
-        button, [&](bool){
-            cout << "Clicked Download Button" << endl;
+        this, [this, update_enabled_status](){
+            m_value.set_enabled(false);
+            update_enabled_status();
+            m_value.run_download();
         }
     );
+
+    // when the download is finished, update the UI
+    connect(
+        &m_value, &ResourceDownloadButton::download_finished,
+        this, update_enabled_status
+    );
 }
+
 
 template class RegisterConfigWidget<DeleteButtonWidget>;
 
--- SerialPrograms/Source/CommonFramework/ResourceDownload/ResourceDownloadWidget.h
+++ SerialPrograms/Source/CommonFramework/ResourceDownload/ResourceDownloadWidget.h
@@ -7,18 +7,25 @@
 #ifndef PokemonAutomation_ResourceDownloadWidget_H
 #define PokemonAutomation_ResourceDownloadWidget_H
 
+#include <QWidget>
 #include "Common/Qt/Options/ConfigWidget.h"
 #include "ResourceDownloadTable.h"
 
 namespace PokemonAutomation{
 
+class ResourceDownloadButton;
 
-class DownloadButtonWidget : public ConfigWidget{
+class DownloadButtonWidget : public QWidget, public ConfigWidget{
 public:
     using ParentOption = ResourceDownloadButton;
 
 public:
+    ~DownloadButtonWidget();
     DownloadButtonWidget(QWidget& parent, ResourceDownloadButton& value);
+
+private:
+    ResourceDownloadButton& m_value;
+    
 };
 
 
