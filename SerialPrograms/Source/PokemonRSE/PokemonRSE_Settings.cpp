7791dbcb08e5f5a2e9c553518bf6daddba2ae440
kichithewolf <kichithewolf@gmail.com>
2026-03-20 10:40:14 -0400

    device selection for RSE

--- SerialPrograms/Source/PokemonRSE/PokemonRSE_Settings.cpp
+++ SerialPrograms/Source/PokemonRSE/PokemonRSE_Settings.cpp
@@ -20,11 +20,38 @@ GameSettings& GameSettings::instance(){
     static GameSettings settings;
     return settings;
 }
+GameSettings::~GameSettings(){
+    DEVICE.remove_listener(*this);
+    GAME_BOX.remove_listener(*this);
+    GAME_BOX.X.remove_listener(*this);
+    GAME_BOX.Y.remove_listener(*this);
+    GAME_BOX.WIDTH.remove_listener(*this);
+    GAME_BOX.HEIGHT.remove_listener(*this);
+}
 GameSettings::GameSettings()
     : BatchOption(LockMode::LOCK_WHILE_RUNNING)
+    , m_game_device_settings("<font size=4><b>Game Device settings:</b></font>")
+    , DEVICE(
+        "<b>Device:</b><br>Select the device the game is running on. "
+        "Refer to the documentation for specific setups.",
+        {
+            {Device::switch_1_2,            "switch_1_2",           "Nintendo Switch 1 and 2"},
+            {Device::rg35xx,                "rg35xx",               "RG35XX"},
+            //{Device::custom,                "custom",               "Custom"},
+        },
+        LockMode::LOCK_WHILE_RUNNING,
+        Device::switch_1_2
+    )
+    , GAME_BOX(
+        "Game Box: The part of the screen containing the actual video feed.",
+        LockMode::LOCK_WHILE_RUNNING,
+        GroupOption::EnableMode::ALWAYS_ENABLED,
+        true,
+        {0.09375, 0.00462963, 0.8125, 0.962963}
+    )
     , m_soft_reset_timings("<font size=4><b>Soft Reset Timings:</b></font>")
-    , START_BUTTON_MASH0(
-        "<b>Start Button Mash:</b><br>Mash Start for this long after a soft reset to get to the main menu.",
+    , SELECT_BUTTON_MASH0(
+        "<b>Select Button Mash:</b><br>Mash select for this long after a soft reset to get to Press Start.",
         LockMode::LOCK_WHILE_RUNNING,
         "5000 ms"
     )
@@ -37,23 +64,61 @@ GameSettings::GameSettings()
     , SHINY_SOUND_THRESHOLD(
         "<b>Shiny Sound Threshold:</b><br>Maximum error coefficient to trigger a shiny detection.",
         LockMode::LOCK_WHILE_RUNNING,
-        0.97, 0, 1.0
+        0.80, 0, 1.0
     )
     , SHINY_SOUND_LOW_FREQUENCY(
         "<b>Shiny Sound Low Frequency (Hz):</b><br>High pass filter frequency for shiny sound.",
         LockMode::LOCK_WHILE_RUNNING,
-        5000, 0, 48000
+        1000, 0, 48000
     )
 {
+    PA_ADD_STATIC(m_game_device_settings);
+    PA_ADD_OPTION(DEVICE);
+    PA_ADD_STATIC(GAME_BOX);
     PA_ADD_STATIC(m_soft_reset_timings);
-    PA_ADD_OPTION(START_BUTTON_MASH0);
+    PA_ADD_OPTION(SELECT_BUTTON_MASH0);
     PA_ADD_OPTION(ENTER_GAME_WAIT0);
     PA_ADD_STATIC(m_shiny_audio_settings);
     PA_ADD_OPTION(SHINY_SOUND_THRESHOLD);
     PA_ADD_OPTION(SHINY_SOUND_LOW_FREQUENCY);
+
+    GameSettings::on_config_value_changed(this);
+    DEVICE.add_listener(*this);
+    GAME_BOX.add_listener(*this);
+    GAME_BOX.X.add_listener(*this);
+    GAME_BOX.Y.add_listener(*this);
+    GAME_BOX.WIDTH.add_listener(*this);
+    GAME_BOX.HEIGHT.add_listener(*this);
 }
 
-
+void GameSettings::on_config_value_changed(void* object){
+    switch (DEVICE){
+    case Device::switch_1_2:
+        GAME_BOX.X.set(0.09375);
+        GAME_BOX.Y.set(0.00462963);
+        GAME_BOX.WIDTH.set(0.8125);
+        GAME_BOX.HEIGHT.set(0.962963);
+        GAME_BOX.set_visibility(ConfigOptionState::DISABLED);
+        break;
+    case Device::rg35xx:
+        GAME_BOX.X.set(0.125);
+        GAME_BOX.Y.set(0.0564814814814815);
+        GAME_BOX.WIDTH.set(0.7494791666666667);
+        GAME_BOX.HEIGHT.set(0.8861111111111111);
+        GAME_BOX.set_visibility(ConfigOptionState::DISABLED);
+        break;
+    case Device::custom:
+        GAME_BOX.set_visibility(ConfigOptionState::ENABLED);
+        break;
+    default:
+        GAME_BOX.X.set(0.09375);
+        GAME_BOX.Y.set(0.00462963);
+        GAME_BOX.WIDTH.set(0.8125);
+        GAME_BOX.HEIGHT.set(0.962963);
+        GAME_BOX.set_visibility(ConfigOptionState::ENABLED);
+        break;
+    }
+}
 
 
 
