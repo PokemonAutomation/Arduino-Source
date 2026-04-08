17970a448c16d6069e90e1c90b2f4d4bd38354ee
kichithewolf <kichithewolf@gmail.com>
2026-04-08 02:13:13 -0400

    min time for throw no sound (#1153)

--- SerialPrograms/Source/PokemonRSE/Programs/ShinyHunting/PokemonRSE_AudioStarterReset.cpp
+++ SerialPrograms/Source/PokemonRSE/Programs/ShinyHunting/PokemonRSE_AudioStarterReset.cpp
@@ -224,7 +224,7 @@ void AudioStarterReset::program(SingleSwitchProgramEnvironment& env, ProControll
             },
             {{pooch_detector}}
         );
-        pooch_detector.throw_if_no_sound();
+        pooch_detector.throw_if_no_sound(std::chrono::milliseconds(1000));
         if (res == 0){
             env.log("Shiny Poochyena detected!");
             stats.poochyena++;
@@ -277,7 +277,7 @@ void AudioStarterReset::program(SingleSwitchProgramEnvironment& env, ProControll
             },
             {{starter_detector}}
         );
-        starter_detector.throw_if_no_sound();
+        starter_detector.throw_if_no_sound(std::chrono::milliseconds(1000));
         context.wait_for_all_requests();
         if (res2 == 0){
             env.log("Shiny starter detected!");
