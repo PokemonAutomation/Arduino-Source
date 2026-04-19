21873222b3779c9334f3a26108dba88cec24a9a3
jw098 <justin.wong098@gmail.com>
2026-04-19 12:07:07 -0700

    FileUnzip (#1198)

--- SerialPrograms/Source/CommonFramework/Tools/FileUnzip.h
+++ SerialPrograms/Source/CommonFramework/Tools/FileUnzip.h
@@ -8,10 +8,22 @@
 #define PokemonAutomation_FileUnzip_H
 
 #include <string>
+#include <functional>
+#include "Common/Cpp/CancellableScope.h"
+
 
 namespace PokemonAutomation{
 
-void unzip_file(const char* zip_path, const char* target_dir);
+// unzips the zip file located in zip_path, to target_dir
+// if target_dir doesn't already exist, it will create it.
+// throw OperationCancelledException if the CancellableScope is cancelled
+// throw InternalProgramError if unzipping fails.
+void unzip_file(
+    CancellableScope& scope,
+    const char* zip_path, 
+    const char* target_dir, 
+    std::function<void(uint64_t bytes_done, uint64_t total_bytes)> progress_callback
+);
 
 }
 #endif
