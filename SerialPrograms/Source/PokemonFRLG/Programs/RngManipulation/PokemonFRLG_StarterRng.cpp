3da14a8a97fc171638b4937d6d68433e00c8b11d
theAstrogoth <theastrogoth@gmail.com>
2026-04-22 13:46:15 -0500

    add missing hits display update

--- SerialPrograms/Source/PokemonFRLG/Programs/RngManipulation/PokemonFRLG_StarterRng.cpp
+++ SerialPrograms/Source/PokemonFRLG/Programs/RngManipulation/PokemonFRLG_StarterRng.cpp
@@ -1102,6 +1102,7 @@ void StarterRng::program(SingleSwitchProgramEnvironment& env, ProControllerConte
         }
         if (pokemon.level.size() > 1){
             searcher.refine_search(search_hits, filters, 0, 30);
+            POSSIBLE_HITS.set(search_hits);
             env.log("Number of search hits: " + std::to_string(search_hits.size()));
             finished = update_history(env, ADVANCE_HISTORY, CALIBRATION_HISTORY, MAX_HISTORY_LENGTH, SEED_CALIBRATION_FRAMES, ADVANCES_CALIBRATION, CONTINUE_SCREEN_ADJUSTMENT, search_hits);
             if (finished){
@@ -1157,6 +1158,7 @@ void StarterRng::program(SingleSwitchProgramEnvironment& env, ProControllerConte
             if (pokemon.level.size() > num_levels){
                 num_levels = pokemon.level.size();
                 searcher.refine_search(search_hits, filters, 0, 30);
+                POSSIBLE_HITS.set(search_hits);
                 env.log("Number of search hits: " + std::to_string(search_hits.size()));
                 finished = update_history(env, ADVANCE_HISTORY, CALIBRATION_HISTORY, MAX_HISTORY_LENGTH, SEED_CALIBRATION_FRAMES, ADVANCES_CALIBRATION, CONTINUE_SCREEN_ADJUSTMENT, search_hits);
                 if (finished){
