c3c775ae0bb6f08e1cbff78bef7510383b60b2a7
theAstroGoth <52220914+theastrogoth@users.noreply.github.com>
2026-05-25 20:49:03 -0500

    tweak StartMenuDetector (#1270)

--- SerialPrograms/Source/PokemonFRLG/Inference/Menus/PokemonFRLG_StartMenuDetector.cpp
+++ SerialPrograms/Source/PokemonFRLG/Inference/Menus/PokemonFRLG_StartMenuDetector.cpp
@@ -5,6 +5,7 @@
  */
 
 #include "Common/Cpp/Color.h"
+#include "CommonTools/Images/SolidColorTest.h"
 #include "CommonFramework/ImageTypes/ImageViewRGB32.h"
 #include "CommonFramework/VideoPipeline/VideoOverlayScopes.h"
 #include "PokemonFRLG/PokemonFRLG_Settings.h"
@@ -17,13 +18,24 @@ namespace PokemonFRLG{
 
 StartMenuDetector::StartMenuDetector(Color color)
     : m_selection_arrow(color, nullptr, MENU_ARROW_BOX)
+    , m_menu_top_box(0.728365, 0.043509, 0.238462, 0.006490)
+    , m_menu_left_box(0.728365, 0.050000, 0.003365, 0.427644)
 {}
 void StartMenuDetector::make_overlays(VideoOverlaySet& items) const{
     const BoxOption& GAME_BOX = GameSettings::instance().GAME_BOX;
     items.add(COLOR_RED, GAME_BOX.inner_to_outer(MENU_ARROW_BOX));
+    items.add(COLOR_RED, GAME_BOX.inner_to_outer(m_menu_top_box));
+    items.add(COLOR_RED, GAME_BOX.inner_to_outer(m_menu_left_box));
 }
 bool StartMenuDetector::detect(const ImageViewRGB32& screen){
-    return m_selection_arrow.detect(screen);
+    ImageViewRGB32 game_screen = extract_box_reference(screen, GameSettings::instance().GAME_BOX);
+    ImageViewRGB32 menu_top_image = extract_box_reference(game_screen, m_menu_top_box);
+    ImageViewRGB32 menu_left_image = extract_box_reference(game_screen, m_menu_left_box);
+    return (
+        m_selection_arrow.detect(screen)
+        && is_white(menu_top_image)
+        && is_white(menu_left_image)
+    );
 }
 
 
