3d6ce18744e3ef1fe625aaefa8fe45803f96ee94
Dalton-V <dalton216@gmail.com>
2026-07-12 10:11:24 -0500

    Move auto catch logic to navigation. Use optimal actions for safari encounters.

--- SerialPrograms/Source/PokemonFRLG/Inference/PokemonFRLG_PokedexRegisteredDetector.cpp
+++ SerialPrograms/Source/PokemonFRLG/Inference/PokemonFRLG_PokedexRegisteredDetector.cpp
@@ -6,6 +6,7 @@
 
 #include "Kernels/Waterfill/Kernels_Waterfill_Types.h"
 #include "CommonTools/ImageMatch/WaterfillTemplateMatcher.h"
+#include "CommonTools/Images/SolidColorTest.h"
 #include "CommonTools/Images/WaterfillUtilities.h"
 #include "PokemonFRLG/PokemonFRLG_Settings.h"
 #include "PokemonFRLG_PokedexRegisteredDetector.h"
@@ -38,17 +39,26 @@ public:
 
 PokedexRegisteredDetector::PokedexRegisteredDetector(
     Color color,
-    VideoOverlay* overlay,
-    const ImageFloatBox& box
+    VideoOverlay* overlay
 )
     : m_color(color)
     , m_overlay(overlay)
-    , m_box(box)
+    , m_box(0.85, 0.923, 0.082, 0.070)
+    , m_top_box(0.940865, 0.013942, 0.049519, 0.071394)
+    , m_header_box(0.037019, 0.147355, 0.928365, 0.015865)
+    , m_divider_box(0.016827, 0.560577, 0.961827, 0.002885)
+    , m_body_box(0.016827, 0.582577, 0.961827, 0.002885)
+    , m_bottom_box(0.007692, 0.912500, 0.688942, 0.075721)
 {}
 
 void PokedexRegisteredDetector::make_overlays(VideoOverlaySet& items) const{
     const BoxOption& GAME_BOX = GameSettings::instance().GAME_BOX;
-    items.add(m_color, GAME_BOX.inner_to_outer(m_box));
+    items.add(m_color, GAME_BOX.inner_to_outer(m_box));           // White "A" button prompt
+    items.add(COLOR_RED, GAME_BOX.inner_to_outer(m_top_box));     // Green RGB(189, 205,   0)
+    items.add(COLOR_RED, GAME_BOX.inner_to_outer(m_header_box));  // White
+    items.add(COLOR_RED, GAME_BOX.inner_to_outer(m_divider_box)); // Brown RGB(136, 100,  38)
+    items.add(COLOR_RED, GAME_BOX.inner_to_outer(m_body_box));    // Beige RGB(255, 245, 204)
+    items.add(COLOR_RED, GAME_BOX.inner_to_outer(m_bottom_box));  // Green
 }
 
 bool PokedexRegisteredDetector::detect(const ImageViewRGB32& screen){
@@ -85,6 +95,30 @@ bool PokedexRegisteredDetector::detect(const ImageViewRGB32& screen){
             return true;
         }
     );
+
+    if (!found){
+        m_last_detected_box.reset();
+
+        ImageViewRGB32 top_image = extract_box_reference(game_screen, m_top_box);
+        ImageViewRGB32 header_image = extract_box_reference(game_screen, m_header_box);
+        ImageViewRGB32 divider_image = extract_box_reference(game_screen, m_divider_box);
+        ImageViewRGB32 body_image = extract_box_reference(game_screen, m_body_box);
+        ImageViewRGB32 bottom_image = extract_box_reference(game_screen, m_bottom_box);
+
+        if (is_solid(top_image, { 0.480, 0.520, 0.000 }, 0.25, 20)
+            && is_white(header_image)
+            && is_solid(divider_image, { 0.496, 0.365, 0.139 }, 0.25, 20)
+            && is_solid(body_image, { 0.362, 0.348, 0.290 }, 0.25, 20)
+            && is_solid(bottom_image, { 0.480, 0.520, 0.000 }, 0.25, 20)
+            && !is_white(top_image)
+            && !is_white(divider_image)
+            && !is_white(bottom_image)
+        ){
+            return true;
+        }
+
+        return false;
+    }
 
     if (m_overlay){
         if (found){
