5c82dd1b80e4c62954bc4256a22db9472c4ed5f2
jw098 <justin.wong098@gmail.com>
2026-07-26 10:44:22 -0700

    update SandwichRecipeNumberDetector

--- SerialPrograms/Source/PokemonSV/Inference/Picnics/PokemonSV_SandwichRecipeDetector.cpp
+++ SerialPrograms/Source/PokemonSV/Inference/Picnics/PokemonSV_SandwichRecipeDetector.cpp
@@ -7,6 +7,7 @@
 #include <opencv2/imgproc.hpp>
 #include "Common/Cpp/Logging/AbstractLogger.h"
 #include "Common/Cpp/Containers/FixedLimitVector.tpp"
+#include "CommonFramework/GlobalSettingsPanel.h"
 #include "CommonFramework/ImageTypes/ImageRGB32.h"
 #include "CommonFramework/ImageTypes/ImageViewRGB32.h"
 #include "CommonFramework/VideoPipeline/VideoOverlayScopes.h"
@@ -56,7 +57,7 @@ void SandwichRecipeNumberDetector::detect_recipes(const ImageViewRGB32& screen, 
 
         ImageRGB32 dilated_image(filterd_image.width(), filterd_image.height());
 
-        if (screen.width() >= 1280){
+        if (screen.width() >= 1280 && GlobalSettings::instance().OCR_LIBRARY == OcrLibrary::TESSERACT){
             const int dilation_type = cv::MORPH_ELLIPSE;
             const int dilation_size = 1;
 
