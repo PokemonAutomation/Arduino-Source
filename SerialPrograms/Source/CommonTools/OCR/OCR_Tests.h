bdb466a03dc8e75917148d8236337ff344a64f1a
jw098 <justin.wong098@gmail.com>
2026-07-26 23:42:27 -0700

    raw OCR tests

---
+++ SerialPrograms/Source/CommonTools/OCR/OCR_Tests.h
@@ -0,0 +1,25 @@
+/*  OCR Tests
+ *
+ *  From: https://github.com/PokemonAutomation/
+ *
+ */
+
+#ifndef PokemonAutomation_OCR_Tests_H
+#define PokemonAutomation_OCR_Tests_H
+
+#include "Common/Cpp/TestRunners/UnitTest.h"
+
+namespace PokemonAutomation{
+namespace OCR{
+
+
+
+void add_tests(UnitTestDatabase& database);
+
+void add_tests_raw_OCR(UnitTestDatabase& database);
+
+
+
+}
+}
+#endif
