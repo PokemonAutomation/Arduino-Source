18bc5db1e38ff5aa8d87e57867a38796d54ab7f5
kichithewolf <kichithewolf@gmail.com>
2026-08-01 12:34:19 -0400

    Pokenav detection

--- SerialPrograms/Source/PokemonRSE/Inference/Dialogs/PokemonRSE_DialogDetector.cpp
+++ SerialPrograms/Source/PokemonRSE/Inference/Dialogs/PokemonRSE_DialogDetector.cpp
@@ -175,6 +175,32 @@ bool AdvanceBattleDialogDetector::detect(const ImageViewRGB32& screen){
 
 
 
+PokenavDialogDetector::PokenavDialogDetector(Color color)
+    : m_dialog_border_box(0.04, 0.717, 0.926425, 0.002033) //Very narrow, not many places to detect
+    , m_dialog_main_box(0.039784, 0.759311, 0.039008, 0.010672)
+{}
+void PokenavDialogDetector::make_overlays(VideoOverlaySet& items) const{
+    const BoxOption& GAME_BOX = GameSettings::instance().GAME_BOX;
+    items.add(COLOR_RED, GAME_BOX.inner_to_outer(m_dialog_border_box));
+    items.add(COLOR_RED, GAME_BOX.inner_to_outer(m_dialog_main_box));
+}
+bool PokenavDialogDetector::detect(const ImageViewRGB32& screen){
+    ImageViewRGB32 game_screen = extract_box_reference(screen, GameSettings::instance().GAME_BOX);
+
+    ImageViewRGB32 dialog_border_image = extract_box_reference(game_screen, m_dialog_border_box);
+    ImageViewRGB32 dialog_main_image = extract_box_reference(game_screen, m_dialog_main_box);
+
+    if (is_solid(dialog_border_image, { 0.20498, 0.45, 0.3448 }, 0.25, 20) //mint green 107, 235, 180
+        && is_white(dialog_main_image)
+    ){
+        return true;
+    }
+    return false;
+}
+
+
+
+
 }
 }
 }
--- SerialPrograms/Source/PokemonRSE/Inference/Dialogs/PokemonRSE_DialogDetector.h
+++ SerialPrograms/Source/PokemonRSE/Inference/Dialogs/PokemonRSE_DialogDetector.h
@@ -104,6 +104,30 @@ public:
     {}
 };
 
+
+// Detect the Pokenav dialog box that appears when being called on the overworld
+// This does not work for calls from the Pokenav menu itself, as the position is different
+// Emerald only, Ruby and Sapphire don't have to worry about randomly being interrupted.
+// Advance arrow appears on all but the last box of dialog. No detection for this since
+// most interaction will be to mash B to get rid of the call.
+class PokenavDialogDetector : public StaticScreenDetector{
+public:
+    PokenavDialogDetector(Color color);
+
+    virtual void make_overlays(VideoOverlaySet& items) const override;
+    virtual bool detect(const ImageViewRGB32& screen) override;
+
+private:
+    ImageFloatBox m_dialog_border_box;
+    ImageFloatBox m_dialog_main_box;
+};
+class PokenavDialogWatcher : public DetectorToFinder<PokenavDialogDetector>{
+public:
+    PokenavDialogWatcher(Color color)
+        : DetectorToFinder("PokenavDialogWatcher", std::chrono::milliseconds(250), color)
+    {}
+};
+
 // Future note: when given a choice popup, there is no advance arrow.
 
 
