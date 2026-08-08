03a66b402d035be434a476445e3fac755fac29c2
jw098 <justin.wong098@gmail.com>
2026-08-08 00:14:18 -0700

    Hid_Keyboard: remove unused header. move SuperControlSession instantiation. (#1361)

--- SerialPrograms/Source/CommonTools/Async/SuperControlSession.cpp
+++ SerialPrograms/Source/CommonTools/Async/SuperControlSession.cpp
@@ -8,12 +8,14 @@
 
 #include "NintendoSwitch/Controllers/Procon/NintendoSwitch_ProController.h"
 #include "NintendoSwitch/Controllers/Joycon/NintendoSwitch_Joycon.h"
+#include "Controllers/StandardHid/StandardHid_Keyboard.h"
 
 namespace PokemonAutomation{
 
 
 template class SuperControlSession<NintendoSwitch::ProController>;
 template class SuperControlSession<NintendoSwitch::JoyconController>;
+template class SuperControlSession<StandardHid::Keyboard>;
 
 
 }
--- SerialPrograms/Source/Controllers/StandardHid/StandardHid_Keyboard.cpp
+++ SerialPrograms/Source/Controllers/StandardHid/StandardHid_Keyboard.cpp
@@ -6,10 +6,8 @@
 
 #include "Common/Cpp/Containers/Pimpl.tpp"
 #include "CommonTools/Async/InterruptableCommands.tpp"
-#include "CommonTools/Async/SuperControlSession.tpp"
 #include "ControllerInput/ControllerInput.h"
 #include "ControllerInput/Keyboard/KeyboardInput_State.h"
-#include "ControllerInput/Keyboard/KeyboardInput_KeyMappings.h"
 #include "Controllers/ControllerState.h"
 #include "StandardHid_Keyboard.h"
 
@@ -21,7 +19,6 @@ namespace PokemonAutomation{
 
 //  Instantiate some template helper classes.
 template class AsyncCommandSession<StandardHid::Keyboard>;
-template class SuperControlSession<StandardHid::Keyboard>;
 
 namespace StandardHid{
 
