f43405eafc07f070dab5ddc60568c6c07e644313
Alexander Yee <a-yee@u.northwestern.edu>
2026-08-15 12:21:17 -0700

    Disable the Qt6.9 workaround for Mutex and ConditionVariable.

--- Common/Cpp/Concurrency/ConditionVariable.h
+++ Common/Cpp/Concurrency/ConditionVariable.h
@@ -7,11 +7,14 @@
 #ifndef PokemonAutomation_Concurrency_ConditionVariable_H
 #define PokemonAutomation_Concurrency_ConditionVariable_H
 
-#include <mutex>
 #include "Qt6.9ThreadBugWorkaround.h"
 
-#ifdef PA_ENABLE_QT_ADOPTION_WORKAROUND
+//  Note from Kuro (2026/08/15): I think it's ok to disable this workaround for
+//  mutex and conditional_variable since we're completely leaking the thread.
+//  So the parasitic adoption should dangle these sync classes.
+#if 0 //def PA_ENABLE_QT_ADOPTION_WORKAROUND
 
+#include <mutex>
 #include <QWaitCondition>
 
 //#include <iostream>
--- Common/Cpp/Concurrency/Mutex.h
+++ Common/Cpp/Concurrency/Mutex.h
@@ -9,7 +9,10 @@
 
 #include "Qt6.9ThreadBugWorkaround.h"
 
-#ifdef PA_ENABLE_QT_ADOPTION_WORKAROUND
+//  Note from Kuro (2026/08/15): I think it's ok to disable this workaround for
+//  mutex and conditional_variable since we're completely leaking the thread.
+//  So the parasitic adoption should dangle these sync classes.
+#if 0 //def PA_ENABLE_QT_ADOPTION_WORKAROUND
 
 #include <mutex>
 #include <QMutex>
