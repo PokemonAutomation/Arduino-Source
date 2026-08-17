7eea12460471f658bad2c8056373c8b3ddeca803
jw098 <justin.wong098@gmail.com>
2026-08-17 12:52:53 -0700

    send error report if failing 10 times in early tutorial

--- SerialPrograms/Source/PokemonSV/Programs/AutoStory/PokemonSV_AutoStoryTools.cpp
+++ SerialPrograms/Source/PokemonSV/Programs/AutoStory/PokemonSV_AutoStoryTools.cpp
@@ -1496,6 +1496,15 @@ void checkpoint_reattempt_loop_tutorial(
 
         break;  
     }catch (OperationFailedException& e){
+        if (i == 10){  // send an error report for debugging if 10 failed attempts for a given checkpoint.
+            OperationFailedException exception(
+                ErrorReport::SEND_ERROR_REPORT,
+                "10 failed attempts. " + checkpoint_text,
+                env.console
+            );
+            exception.send_recoverable_notification(env);
+        }
+
         if (i > max_attempts){
             OperationFailedException::fire(
                 ErrorReport::SEND_ERROR_REPORT,
