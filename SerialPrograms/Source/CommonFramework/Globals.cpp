13e8d598aa626aff15f8ef92c568e98a38723057
Alexander Yee <a-yee@u.northwestern.edu>
2026-09-24 22:28:54 -0700

    Version #

--- SerialPrograms/Source/CommonFramework/Globals.cpp
+++ SerialPrograms/Source/CommonFramework/Globals.cpp
@@ -27,11 +27,11 @@ namespace PokemonAutomation{
 #endif
 
 #ifndef PA_VERSION_MINOR
-#define PA_VERSION_MINOR 70
+#define PA_VERSION_MINOR 71
 #endif
 
 #ifndef PA_VERSION_PATCH
-#define PA_VERSION_PATCH 8
+#define PA_VERSION_PATCH 1
 #endif
 
 const bool IS_BETA_VERSION = PA_IS_BETA;
